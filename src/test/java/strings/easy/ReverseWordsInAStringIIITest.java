bbc1cb960ba8d038c663bf78f7851980b74a5b1e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-03-24 11:09:01 -0700

    Solution for the 557. Reverse Words in a String III.

---
+++ src/test/java/strings/easy/ReverseWordsInAStringIIITest.java
@@ -0,0 +1,16 @@
+package strings.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ */
+public class ReverseWordsInAStringIIITest {
+
+    @Test
+    public void reverseWords() {
+        assertEquals("s'teL ekat edoCteeL tsetnoc", new ReverseWordsInAStringIII().reverseWords("Let's take LeetCode contest"));
+    }
+}
