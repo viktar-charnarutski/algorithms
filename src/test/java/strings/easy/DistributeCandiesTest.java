068509ebb1a43b0a3bcef44099f3103f7b5aaa24
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-02 14:31:06 -0700

    Solution for the 575. Distribute Candies. Improving performance.

--- src/test/java/strings/easy/DistributeCandiesTest.java
+++ src/test/java/strings/easy/DistributeCandiesTest.java
@@ -13,5 +13,6 @@ public class DistributeCandiesTest {
         assertTrue(3 == ins.distributeCandies(new int[]{1, 1, 2, 2, 3, 3}));
         assertTrue(2 == ins.distributeCandies(new int[]{1, 1, 2, 3}));
         assertTrue(3 == ins.distributeCandies(new int[]{1, 1, 2, 3, 4, 5}));
+        assertTrue(2 == ins.distributeCandies(new int[]{100000, 0, 100000, 0, 100000, 0, 100000, 0, 100000, 0, 100000, 0}));
     }
 }
