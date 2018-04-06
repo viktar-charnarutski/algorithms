ec93a79d57478dbe13acaa2dae3b52c8114c32f0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-05 21:06:52 -0700

    Solution for the 653. Two Sum IV - Input is a BST.

--- README.md
+++ README.md
@@ -61,6 +61,7 @@ Solutions for algorithm questions which could be asked on a technical interview.
 | 46 | 5-Apr-2018 | [226. Invert Binary Tree][46] | Easy | Y |
 | 47 | 5-Apr-2018 | [349. Intersection of Two Arrays][47] | Easy |  |
 | 48 | 5-Apr-2018 | [690. Employee Importance][48] | Easy |  |
+| 49 | 5-Apr-2018 | [653. Two Sum IV - Input is a BST][49] | Easy |  |
 
 [1]: https://leetcode.com/problems/judge-route-circle/
 [2]: https://leetcode.com/problems/reverse-string/
@@ -110,3 +111,4 @@ Solutions for algorithm questions which could be asked on a technical interview.
 [46]: https://leetcode.com/problems/invert-binary-tree/
 [47]: https://leetcode.com/problems/intersection-of-two-arrays/
 [48]: https://leetcode.com/problems/employee-importance/
+[49]: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
---
+++ src/main/java/leetcode/easy/TwoSumIV.java
@@ -0,0 +1,53 @@
+package leetcode.easy;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ * 653. Two Sum IV - Input is a BST
+ * <p>
+ * Given a Binary Search Tree and a target number, return true if there exist two elements
+ * in the BST such that their sum is equal to the given target.
+ */
+public class TwoSumIV {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> wanted = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
+
+        LinkedList<TreeNode> toCheck = new LinkedList<>();
+        toCheck.add(root);
+
+        while (!toCheck.isEmpty()) {
+            TreeNode currNode = toCheck.remove();
+
+            if (currNode == null || seen.contains(currNode.val)) {
+                continue;
+            }
+            seen.add(currNode.val);
+
+            if (wanted.contains(currNode.val)) {
+                return true;
+            }
+            wanted.add(k - currNode.val);
+
+            if (currNode.left != null) {
+                toCheck.add(currNode.left);
+            }
+            if (currNode.right != null) {
+                toCheck.add(currNode.right);
+            }
+        }
+        return false;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
