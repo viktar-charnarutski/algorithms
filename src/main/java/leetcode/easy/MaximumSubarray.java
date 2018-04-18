0220436a34e3f31fdcc7c22cf11c1940ec3db3f2
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-18 15:14:50 -0700

    Solution for the 53. Maximum Subarray. Improving performance.v

--- src/main/java/leetcode/easy/MaximumSubarray.java
+++ src/main/java/leetcode/easy/MaximumSubarray.java
@@ -9,7 +9,16 @@ package leetcode.easy;
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        return maxSubArray(nums, 0, Long.MIN_VALUE);
+
+        int maxSum = nums[0];
+        int currSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        return maxSum;
     }
 
     private int maxSubArray(int[] nums, int i, long maxSum) {
