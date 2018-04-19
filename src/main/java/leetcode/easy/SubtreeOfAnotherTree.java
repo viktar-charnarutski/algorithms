2450b625d7efe6b45ba28d1a013b7343968d08dd
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-19 13:34:35 -0700

    Solution for the 572. Subtree of Another Tree. Improving performance.

--- src/main/java/leetcode/easy/SubtreeOfAnotherTree.java
+++ src/main/java/leetcode/easy/SubtreeOfAnotherTree.java
@@ -11,6 +11,20 @@ import java.util.LinkedList;
  */
 public class SubtreeOfAnotherTree {
     public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSameTree(s, t)) return true;
+        return isSameTree(s.left, t) || isSameTree(s.right, t);
+    }
+
+    private boolean isSameTree(TreeNode t, TreeNode s) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+
+    public boolean isSubtreeDFS(TreeNode s, TreeNode t) {
         if (s == null || t == null) return false;
         boolean result = false;
 
