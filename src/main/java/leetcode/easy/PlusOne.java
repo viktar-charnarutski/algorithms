1190b20baa04801af802164296284098062eba2f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-20 14:52:06 -0700

    Solution for the 66. Plus One. Improving performance.

--- src/main/java/leetcode/easy/PlusOne.java
+++ src/main/java/leetcode/easy/PlusOne.java
@@ -14,6 +14,21 @@ public class PlusOne {
     public int[] plusOne(int[] digits) {
         if (digits == null || digits.length == 0) return digits;
 
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public int[] plusOne1(int[] digits) {
+        if (digits == null || digits.length == 0) return digits;
+
         int ind = digits.length - 1;
         boolean remained = true;
         while (remained && ind >= 0) {
