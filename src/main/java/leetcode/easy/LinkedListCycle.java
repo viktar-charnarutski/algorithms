6662dad154b996ede245e283bb34262d610e5e61
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-23 11:37:34 -0700

    Solution for the 141. Linked List Cycle. Solving without using extra space.

--- src/main/java/leetcode/easy/LinkedListCycle.java
+++ src/main/java/leetcode/easy/LinkedListCycle.java
@@ -1,22 +1,25 @@
 package leetcode.easy;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * 141. Linked List Cycle
  * <p>
  * Given a linked list, determine if it has a cycle in it.
+ * <p>
+ * Follow up:
+ * Can you solve it without using extra space?
  */
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
         if (head == null) return false;
 
-        Set<ListNode> nodes = new HashSet<>();
-        ListNode curr = head;
-        while (curr != null) {
-            if (!nodes.add(curr)) return true;
-            curr = curr.next;
+        ListNode slowRunner = head;
+        ListNode fastRunner = head.next;
+
+        while (fastRunner != null && slowRunner != null) {
+            if (slowRunner == fastRunner) return true;
+
+            slowRunner = slowRunner.next;
+            fastRunner = fastRunner.next != null ? fastRunner.next.next : null;
         }
         return false;
     }
