3e405e63cc7a1ad43b59c9e863de08c4f2c83136
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-24 10:28:49 -0700

    Solution for the 290. Word Pattern. Improving the algorithm.

--- src/main/java/leetcode/easy/WordPattern.java
+++ src/main/java/leetcode/easy/WordPattern.java
@@ -1,6 +1,7 @@
 package leetcode.easy;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * 290. Word Pattern
@@ -11,7 +12,22 @@ import java.util.HashMap;
  * a letter in pattern and a non-empty word in str.
  */
 public class WordPattern {
+
     public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length()) return false;
+
+        HashMap<Object, Integer> patternMapping = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (!Objects.equals(patternMapping.put(words[i], i), patternMapping.put(pattern.charAt(i), i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean wordPatternOrig(String pattern, String str) {
         if (pattern == null || str == null) return false;
         else if (pattern.isEmpty() && str.isEmpty()) return true;
 
