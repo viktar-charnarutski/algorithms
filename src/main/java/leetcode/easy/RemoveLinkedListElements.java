248b5d3fda081f9bf328298618af59c2c0b5c2fe
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-24 14:10:37 -0700

    Solution for the 203. Remove Linked List Elements. Improving the algorithm.

--- src/main/java/leetcode/easy/RemoveLinkedListElements.java
+++ src/main/java/leetcode/easy/RemoveLinkedListElements.java
@@ -8,23 +8,7 @@ package leetcode.easy;
 public class RemoveLinkedListElements {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
-
-        while (head != null && head.val == val) {
-            head = head.next;
-        }
-
-        if (head != null) removeElements(head.next, head, val);
-        return head;
-    }
-
-    private static void removeElements(ListNode node, ListNode prev, int val) {
-        if (node == null) return;
-
-        if (node.val == val) {
-            prev.next = node.next;
-            node = prev;
-        }
-
-        removeElements(node.next, node, val);
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
     }
 }
