d0a0f031e217cb8f98f3b946d5370521c275ad19
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-24 21:09:47 -0700

    Solution for the 234. Palindrome Linked List. Optimizing the solution in O(n) time and O(1) space.

--- src/main/java/leetcode/easy/PalindromeLinkedList.java
+++ src/main/java/leetcode/easy/PalindromeLinkedList.java
@@ -4,12 +4,56 @@ import java.util.Stack;
 
 /**
  * 234. Palindrome Linked List
- *
+ * <p>
  * Given a singly linked list, determine if it is a palindrome.
- *
+ * <p>
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
  */
 public class PalindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        fast = head;
+        slow = reverse(slow);
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public boolean isPalindromeOrig(ListNode head) {
         if (head == null || head.next == null) return true;
 
         Stack<ListNode> nodes = new Stack<>();
