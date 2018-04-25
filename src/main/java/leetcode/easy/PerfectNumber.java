67476e5229b30d66fbab400299e56d70ee417bfb
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-25 12:28:34 -0700

    Solution for the 507. Perfect Number. Improving performance.

--- src/main/java/leetcode/easy/PerfectNumber.java
+++ src/main/java/leetcode/easy/PerfectNumber.java
@@ -2,9 +2,9 @@ package leetcode.easy;
 
 /**
  * 507. Perfect Number
- *
+ * <p>
  * We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
- *
+ * <p>
  * Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
  */
 public class PerfectNumber {
@@ -12,12 +12,14 @@ public class PerfectNumber {
     public boolean checkPerfectNumber(int num) {
 
         int sum = 0;
-        for (int i = 1; i < num; i++) {
+        for (int i = 1; i * i <= num; i++) {
             if (num % i == 0) {
                 sum += i;
-                if (sum > num) return false;
+                if (i * i != num) {
+                    sum += num / i;
+                }
             }
         }
-        return sum == num;
+        return sum - num == num;
     }
 }
