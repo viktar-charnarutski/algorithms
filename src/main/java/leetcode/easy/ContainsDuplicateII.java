2b502ad2bc72189ddbcc3fa9bc24054ec0dc1c2e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-25 10:44:23 -0700

    Solution for the 219. Contains Duplicate II. Fixing the solution.

--- src/main/java/leetcode/easy/ContainsDuplicateII.java
+++ src/main/java/leetcode/easy/ContainsDuplicateII.java
@@ -1,17 +1,23 @@
 package leetcode.easy;
 
+import java.util.HashMap;
+
 /**
  * 219. Contains Duplicate II
- *
+ * <p>
  * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array
  * such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
  */
 public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if (nums == null || nums.length < k) return false;
+        if (nums == null) return false;
 
-        for (int i = 0; i < nums.length - k; i++) {
-            if (nums[i] == nums[i + k]) return true;
+        HashMap<Integer, Integer> pos = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer ind = pos.get(nums[i]);
+            if (ind != null && (i - ind) <= k) return true;
+
+            pos.put(nums[i], i);
         }
 
         return false;
