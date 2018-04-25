513fea01e2c657f1b481034a9cc07bfce3229d13
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-25 11:48:02 -0700

    Solution for the 507. Perfect Number. Optimizing the solution.

--- src/main/java/leetcode/easy/PerfectNumber.java
+++ src/main/java/leetcode/easy/PerfectNumber.java
@@ -15,6 +15,7 @@ public class PerfectNumber {
         for (int i = 1; i < num; i++) {
             if (num % i == 0) {
                 sum += i;
+                if (sum > num) return false;
             }
         }
         return sum == num;
