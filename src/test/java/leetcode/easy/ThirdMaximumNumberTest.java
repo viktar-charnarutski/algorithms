127d12314fe1f67c6e4f399e61d60287ef1a5bdd
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-29 14:05:01 -0700

    Solution for the 414. Third Maximum Number.

---
+++ src/test/java/leetcode/easy/ThirdMaximumNumberTest.java
@@ -0,0 +1,18 @@
+package leetcode.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+
+public class ThirdMaximumNumberTest {
+
+    @Test
+    public void thirdMax() {
+        ThirdMaximumNumber ins = new ThirdMaximumNumber();
+        assertSame(1, ins.thirdMax(new int[]{3, 2, 1}));
+        assertSame(2, ins.thirdMax(new int[]{1, 2}));
+        assertSame(1, ins.thirdMax(new int[]{2, 2, 3, 1}));
+        assertSame(-2147483648, ins.thirdMax(new int[]{1,2,-2147483648}));
+
+    }
+}
