2a058191a26523e14f75cf8fd465dfd0db4d9aa5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-04-29 19:30:46 -0700

    Solution for the 414. Third Maximum Number. Optimizing solution.

--- src/main/java/leetcode/easy/ThirdMaximumNumber.java
+++ src/main/java/leetcode/easy/ThirdMaximumNumber.java
@@ -13,19 +13,19 @@ public class ThirdMaximumNumber {
         int max3 = Integer.MIN_VALUE;
         boolean flag = false;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max1) {
+        for (int num : nums) {
+            if (num > max1) {
                 max3 = max2;
                 max2 = max1;
-                max1 = nums[i];
-            } else if (nums[i] > max2 && max1 != nums[i]) {
+                max1 = num;
+            } else if (num > max2 && max1 != num) {
                 max3 = max2;
-                max2 = nums[i];
-            } else if (nums[i] >= max3 && max2 != nums[i] && max1 != nums[i]) {
-                max3 = nums[i];
+                max2 = num;
+            } else if (num >= max3 && max2 != num && max1 != num) {
+                max3 = num;
             }
 
-            if (nums[i] == Integer.MIN_VALUE) {
+            if (num == Integer.MIN_VALUE) {
                 flag = true;
             }
         }
