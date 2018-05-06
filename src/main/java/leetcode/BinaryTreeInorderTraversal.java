2b50cb030ef23c52787c21b41811ebdcf001e454
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-05 18:34:10 -0700

    Solution for the 94. Binary Tree Inorder Traversal. Iterating method using Stack

--- src/main/java/leetcode/BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/BinaryTreeInorderTraversal.java
@@ -1,16 +1,38 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 94. Binary Tree Inorder Traversal
- *
+ * <p>
  * Given a binary tree, return the inorder traversal of its nodes' values.
- *
  */
 public class BinaryTreeInorderTraversal {
+
     public List<Integer> inorderTraversal(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            list.add(curr.val);
+            curr = curr.right;
+        }
+
+        return list;
+    }
+
+    public List<Integer> inorderTraversalRecursive(TreeNode root) {
         ArrayList<Integer> list = new ArrayList<>();
         inorder(root, list);
         return list;
