f382a2b46b3743a7d649f3492e783ebb1ab4879f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-08 14:43:36 -0700

    Solution for the 769. Max Chunks To Make Sorted. Improving the solution.

--- src/main/java/leetcode/MaxChunksToMakeSorted.java
+++ src/main/java/leetcode/MaxChunksToMakeSorted.java
@@ -13,6 +13,17 @@ package leetcode;
 public class MaxChunksToMakeSorted {
 
     public int maxChunksToSorted(int[] arr) {
+        int max = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+            if (max == i) count++;
+        }
+        return count;
+    }
+
+    public int maxChunksToSorted1(int[] arr) {
         int[] max = new int[arr.length];
         int count = 0;
 
