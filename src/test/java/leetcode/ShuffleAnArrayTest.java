cd9b3e3b860d5b823560a767692fb3377971e58d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-11 16:44:32 -0700

    384. Shuffle an Array. Test for the shuffle method.

--- src/test/java/leetcode/ShuffleAnArrayTest.java
+++ src/test/java/leetcode/ShuffleAnArrayTest.java
@@ -3,6 +3,7 @@ package leetcode;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ShuffleAnArrayTest {
 
@@ -15,5 +16,8 @@ public class ShuffleAnArrayTest {
 
     @Test
     public void shuffle() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7};
+        ShuffleAnArray ins = new ShuffleAnArray(input);
+        assertNotEquals(input, ins.shuffle());
     }
 }
