e9281a90ba5d691510713639e2b4d9df4e5df827
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-11 16:44:53 -0700

    Solution for the 384. Shuffle an Array.

--- src/main/java/leetcode/ShuffleAnArray.java
+++ src/main/java/leetcode/ShuffleAnArray.java
@@ -1,14 +1,20 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * 384. Shuffle an Array
  */
 public class ShuffleAnArray {
 
     private int[] nums;
+    private Random rand;
 
     public ShuffleAnArray(int[] nums) {
         this.nums = nums;
+        this.rand = new Random();
     }
 
     /** Resets the array to its original configuration and return it. */
@@ -18,6 +24,22 @@ public class ShuffleAnArray {
 
     /** Returns a random shuffling of the array. */
     public int[] shuffle() {
-        return null;
+        List<Integer> numsList = arrayAsList();
+        int[] res = new int[nums.length];
+
+        for (int i = 0; i < res.length; i++) {
+            int randInx = this.rand.nextInt(numsList.size());
+            res[i] = numsList.get(randInx);
+            numsList.remove(randInx);
+        }
+        return res;
+    }
+
+    private List<Integer> arrayAsList() {
+        ArrayList<Integer> numsList = new ArrayList<>();
+        for (int num : this.nums) {
+            numsList.add(num);
+        }
+        return numsList;
     }
 }
