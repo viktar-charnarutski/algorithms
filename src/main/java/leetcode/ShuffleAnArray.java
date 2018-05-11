4c59ab2482279dae62569c5c8a65f4678c230ffd
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-11 11:28:32 -0700

    Initial commit for the 384. Shuffle an Array.

---
+++ src/main/java/leetcode/ShuffleAnArray.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 384. Shuffle an Array
+ */
+public class ShuffleAnArray {
+    public ShuffleAnArray(int[] nums) {
+
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+
+    }
+
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+
+    }
+}
