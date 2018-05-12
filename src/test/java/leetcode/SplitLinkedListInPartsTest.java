77aba66aab2b11701b248f4d99f8a80b80b26bb7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-12 10:00:52 -0700

    725. Split Linked List in Parts. Tests.

--- src/test/java/leetcode/SplitLinkedListInPartsTest.java
+++ src/test/java/leetcode/SplitLinkedListInPartsTest.java
@@ -11,8 +11,11 @@ public class SplitLinkedListInPartsTest {
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(2);
         ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
         n1.next = n2;
         n2.next = n3;
+        n3.next = n4;
+
         SplitLinkedListInParts ins = new SplitLinkedListInParts();
         ListNode[] expected = new ListNode[5];
         expected[0] = n1;
