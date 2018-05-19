e2eca2cf043669f38e96f6fc348c9bac881ed29d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-19 11:37:45 -0700

    Initial commit for the 398. Random Pick Index.

---
+++ src/main/java/leetcode/RandomPickIndex.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 398. Random Pick Index
+ * <p>
+ * Given an array of integers with possible duplicates, randomly output the index of a given target number.
+ * <p>
+ * You can assume that the given target number must exist in the array.
+ */
+public class RandomPickIndex {
+
+    public RandomPickIndex(int[] nums) {
+
+    }
+
+    public int pick(int target) {
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode/RandomPickIndexTest.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RandomPickIndexTest {
+
+    @Test
+    public void pick1() {
+        RandomPickIndex ins = new RandomPickIndex(new int[]{1, 2, 3, 3, 3});
+        assertEquals(0, ins.pick(1));
+    }
+    @Test
+    public void pick2() {
+        RandomPickIndex ins = new RandomPickIndex(new int[]{1, 2, 3, 3, 3});
+        assertEquals(1, ins.pick(2));
+    }
+
+    @Test
+    public void pick3() {
+        RandomPickIndex ins = new RandomPickIndex(new int[]{1, 2, 3, 3, 3});
+        assertEquals(2, ins.pick(3));
+    }
+}
