3e36936bfd414eb1e1e9276fe2aaa17c20d22621
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-18 17:25:01 -0700

    Solution for the 78. Subsets.

--- README.md
+++ README.md
@@ -195,6 +195,7 @@ Solutions for algorithm questions which could be asked on a technical interview.
 | 178 | 17-May-2018 | [554. Brick Wall][178] | Medium | Y+ |
 | 179 | 18-May-2018 | [230. Kth Smallest Element in a BST][179] | Medium | Y |
 | 180 | 18-May-2018 | [378. Kth Smallest Element in a Sorted Matrix][180] | Medium | Y |
+| 181 | 18-May-2018 | [78. Subsets][181] | Medium | Y |
 
 [1]: https://leetcode.com/problems/judge-route-circle/
 [2]: https://leetcode.com/problems/reverse-string/
@@ -376,3 +377,4 @@ Solutions for algorithm questions which could be asked on a technical interview.
 [178]: https://leetcode.com/problems/brick-wall/
 [179]: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 [180]: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+[181]: https://leetcode.com/problems/subsets/
---
+++ src/test/java/leetcode/SubsetsTest.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubsetsTest {
+
+    @Test
+    public void subsets() {
+        List<List<Integer>> expected = new ArrayList<>();
+        List<Integer> subset4 = new ArrayList<>();
+        subset4.add(1);
+        subset4.add(2);
+        subset4.add(3);
+        expected.add(subset4);
+        List<Integer> subset7 = new ArrayList<>();
+        subset7.add(1);
+        subset7.add(2);
+        expected.add(subset7);
+        List<Integer> subset5 = new ArrayList<>();
+        subset5.add(1);
+        subset5.add(3);
+        expected.add(subset5);
+        List<Integer> subset2 = new ArrayList<>();
+        subset2.add(1);
+        expected.add(subset2);
+        List<Integer> subset6 = new ArrayList<>();
+        subset6.add(2);
+        subset6.add(3);
+        expected.add(subset6);
+        List<Integer> subset3 = new ArrayList<>();
+        subset3.add(2);
+        expected.add(subset3);
+        List<Integer> subset1 = new ArrayList<>();
+        subset1.add(3);
+        expected.add(subset1);
+        List<Integer> subset8 = new ArrayList<>();
+        expected.add(subset8);
+        assertEquals(expected, new Subsets().subsets(new int[]{1, 2, 3}));
+    }
+}
