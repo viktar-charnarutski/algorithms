22c86d81de182ff76f89c3ef2be199752e769037
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-20 21:37:20 -0700

    Solution for the 398. Random Pick Index. Refactoring.

--- src/main/java/leetcode/RandomPickIndex.java
+++ src/main/java/leetcode/RandomPickIndex.java
@@ -20,14 +20,12 @@ public class RandomPickIndex {
     }
 
     public int pick(int target) {
-        int res = 0;
         int count = 0;
         for (int i = 0; i < this.nums.length; i++) {
-            if (this.nums[i] == target) {
-                count++;
-                res = rand.nextInt(count) == 0 ? i : res;
+            if (this.nums[i] == target && rand.nextInt(++count) == 0) {
+                return i;
             }
         }
-        return res;
+        return -1;
     }
 }
