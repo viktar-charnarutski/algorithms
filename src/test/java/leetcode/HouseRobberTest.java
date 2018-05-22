497c930eb50cd2996ce7801ebb936c9b95a69a7c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-22 11:34:42 -0700

    198. House Robber. Fixing the test.

--- src/test/java/leetcode/HouseRobberTest.java
+++ src/test/java/leetcode/HouseRobberTest.java
@@ -10,6 +10,6 @@ public class HouseRobberTest {
     public void rob() {
         HouseRobber ins = new HouseRobber();
         int[] input = {20, 10, 5, 100, 50, 40, 80, 90};
-        assertSame(240, ins.rob(input));
+        assertSame(250, ins.rob(input));
     }
 }
