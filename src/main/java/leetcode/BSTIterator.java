9e9b8d17219cd00061d026a82c6f9e4e48de1312
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-22 22:08:23 -0700

    Solution for the 173. Binary Search Tree Iterator.

--- src/main/java/leetcode/BSTIterator.java
+++ src/main/java/leetcode/BSTIterator.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * 173. Binary Search Tree Iterator
  * <p>
@@ -10,21 +14,39 @@ package leetcode;
  * Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
  */
 public class BSTIterator {
-    public BSTIterator(TreeNode root) {
 
+    private Queue<Integer> vals;
+
+    public BSTIterator(TreeNode root) {
+        this.vals = new LinkedList<>();
+        inorderTraversal(root);
+    }
+
+    private void inorderTraversal(TreeNode node) {
+        TreeNode curr = node;
+        Stack<TreeNode> stack = new Stack<>();
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            vals.offer(curr.val);
+            curr = curr.right;
+        }
     }
 
     /**
      * @return whether we have a next smallest number
      */
     public boolean hasNext() {
-        return false;
+        return !vals.isEmpty();
     }
 
     /**
      * @return the next smallest number
      */
     public int next() {
-        return -1;
+        return this.vals.poll();
     }
 }
--- src/test/java/leetcode/BSTIteratorTest.java
+++ src/test/java/leetcode/BSTIteratorTest.java
@@ -2,34 +2,65 @@ package leetcode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
 public class BSTIteratorTest {
 
     @Test
     public void iterator() {
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
+        TreeNode n1 = new TreeNode(8);
+        TreeNode n2 = new TreeNode(3);
+        TreeNode n3 = new TreeNode(10);
         n1.left = n2;
         n1.right = n3;
+        TreeNode n4 = new TreeNode(1);
+        TreeNode n5 = new TreeNode(6);
         n2.left = n4;
         n2.right = n5;
+        TreeNode n6 = new TreeNode(4);
+        TreeNode n7 = new TreeNode(7);
+        n5.left = n6;
+        n5.right = n7;
+        TreeNode n8 = new TreeNode(14);
+        n3.right = n8;
+        TreeNode n9 = new TreeNode(13);
+        n8.left = n9;
 
         BSTIterator i = new BSTIterator(n1);
-        int[] res = new int[2];
+        int[] res = new int[9];
         int ind = 0;
-        while (i.hasNext()) res[ind++] = i.next();
-        assertArrayEquals(res, new int[]{2, 4});
+        while (i.hasNext()) {
+            res[ind++] = i.next();
+        }
+
+        assertArrayEquals(res, new int[]{1, 3, 4, 6, 7, 8, 10, 13, 14});
     }
 
     @Test
-    public void hasNext() {
+    public void hasNext1() {
+        assertTrue(new BSTIterator(new TreeNode(1)).hasNext());
     }
 
     @Test
-    public void next() {
+    public void hasNext2() {
+        BSTIterator itr = new BSTIterator(new TreeNode(1));
+        itr.next();
+        assertFalse(itr.hasNext());
+    }
+
+    @Test
+    public void next1() {
+        TreeNode n1 = new TreeNode(5);
+        TreeNode n2 = new TreeNode(4);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(2);
+        TreeNode n5 = new TreeNode(1);
+        n1.left = n2;
+        n1.right = n3;
+        n2.right = n4;
+        n2.left = n5;
+
+        BSTIterator itr = new BSTIterator(n1);
+        assertEquals(1, itr.next());
     }
 }
