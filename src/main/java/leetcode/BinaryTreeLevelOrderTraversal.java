31bca2d4fefbc8794a34267cd0108dd987c28917
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-22 22:23:46 -0700

    Initial commit for the 102. Binary Tree Level Order Traversal.

---
+++ src/main/java/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * 102. Binary Tree Level Order Traversal
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ */
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}
---
+++ src/test/java/leetcode/BinaryTreeLevelOrderTraversalTest.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class BinaryTreeLevelOrderTraversalTest {
+
+    @Test
+    public void levelOrder() {
+    }
+}
