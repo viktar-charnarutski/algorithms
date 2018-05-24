161b2f364d05fb820d4c8a0f63b7399badc290bc
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-24 16:35:13 -0700

    Initial commit for the 524. Longest Word in Dictionary through Deleting.

---
+++ src/main/java/leetcode/LongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * 524. Longest Word in Dictionary through Deleting
+ */
+public class LongestWordInDictionaryThroughDeleting {
+    public String findLongestWord(String s, List<String> d) {
+        return null;
+    }
+}
