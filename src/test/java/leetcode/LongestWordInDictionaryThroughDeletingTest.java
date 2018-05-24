8dcd9b3010f035cd93d5cbf154cb5c91988793d8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-24 16:35:28 -0700

    Tests for the 524. Longest Word in Dictionary through Deleting.

---
+++ src/test/java/leetcode/LongestWordInDictionaryThroughDeletingTest.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestWordInDictionaryThroughDeletingTest {
+
+    @Test
+    public void findLongestWord1() {
+        List<String> list = new ArrayList<>();
+        list.add("ale");
+        list.add("apple");
+        list.add("monkey");
+        list.add("plea");
+        assertEquals("apple", new LongestWordInDictionaryThroughDeleting().findLongestWord("abpcplea", list));
+    }
+
+    @Test
+    public void findLongestWord2() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        assertEquals("a", new LongestWordInDictionaryThroughDeleting().findLongestWord("abpcplea", list);
+    }
+}
