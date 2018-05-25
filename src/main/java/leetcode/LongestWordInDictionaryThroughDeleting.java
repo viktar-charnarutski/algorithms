830d653dd8b142e26f28c3236f7b4c680de1460e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-25 14:13:40 -0700

    CharsCount method for the 524. Longest Word in Dictionary through Deleting.

--- src/main/java/leetcode/LongestWordInDictionaryThroughDeleting.java
+++ src/main/java/leetcode/LongestWordInDictionaryThroughDeleting.java
@@ -6,7 +6,22 @@ import java.util.List;
  * 524. Longest Word in Dictionary through Deleting
  */
 public class LongestWordInDictionaryThroughDeleting {
+    private static final int CHARS_COUNT = 26;
+
     public String findLongestWord(String s, List<String> d) {
         return null;
+    }
+
+    private boolean charMatch(String word, int[] target) {
+        int[] chars = new int[CHARS_COUNT];
+        for (char c : word.toCharArray()) {
+            chars[c - 'a']++;
+        }
+
+        for (int i = 0; i < CHARS_COUNT; i++) {
+            if (chars[i] < target[i]) return false;
+        }
+
+        return true;
     }
 }
