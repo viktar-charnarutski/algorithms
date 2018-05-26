568982fb5354dcaf702cc0d9426b3f9b9f1f36af
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-26 08:19:10 -0700

    Solution for the 524. Longest Word in Dictionary through Deleting.

--- src/main/java/leetcode/LongestWordInDictionaryThroughDeleting.java
+++ src/main/java/leetcode/LongestWordInDictionaryThroughDeleting.java
@@ -4,24 +4,40 @@ import java.util.List;
 
 /**
  * 524. Longest Word in Dictionary through Deleting
+ *
+ * Given a string and a string dictionary, find the longest string in the dictionary that can be formed
+ * by deleting some characters of the given string. If there are more than one possible results,
+ * return the longest word with the smallest lexicographical order. If there is no possible result,
+ * return the empty string.
  */
 public class LongestWordInDictionaryThroughDeleting {
+
     private static final int CHARS_COUNT = 26;
 
     public String findLongestWord(String s, List<String> d) {
-        return null;
+        String res = "";
+        int[] target = occurrence(s);
+        for (String str : d) {
+            if (check(str, target)) {
+                res = str.length() > res.length() ? str : res;
+            }
+        }
+        return res;
     }
 
-    private boolean charMatch(String word, int[] target) {
+    private int[] occurrence(String str) {
         int[] chars = new int[CHARS_COUNT];
-        for (char c : word.toCharArray()) {
+        for (char c : str.toCharArray()) {
             chars[c - 'a']++;
         }
+        return chars;
+    }
 
+    private boolean check(String word, int[] target) {
+        int[] chars = occurrence(word);
         for (int i = 0; i < CHARS_COUNT; i++) {
             if (chars[i] < target[i]) return false;
         }
-
         return true;
     }
 }
