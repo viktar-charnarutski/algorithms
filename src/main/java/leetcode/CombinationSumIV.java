6dcc3ab2818c0f0b18fadeaffc9d50e2c8bd2a66
viktar-charnarutski <v.charnarutski@gmail.com>
2018-05-30 14:18:03 -0700

    Solution for the 377. Combination Sum IV. Introducing memorization.

--- src/main/java/leetcode/CombinationSumIV.java
+++ src/main/java/leetcode/CombinationSumIV.java
@@ -8,11 +8,15 @@ package leetcode;
  */
 public class CombinationSumIV {
     public int combinationSum4(int[] nums, int target) {
+        return combinationSum4(nums, target, new Integer[target + 1][nums.length + 1]);
+    }
+
+    public int combinationSum4(int[] nums, int target, Integer[][] memo) {
         if (target == 0) return 1;
         int count = 0;
         for (int num : nums) {
             if (num <= target)
-                count += combinationSum4(nums, target - num);
+                count += memo[target][num] != null ? memo[target][num] : combinationSum4(nums, target - num);
         }
         return count;
     }
