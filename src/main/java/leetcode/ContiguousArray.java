b49e8e533f8f7afb9f29de1ce6ac9d511e6ef219
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-03 12:19:57 -0700

    Initial commit for the 525. Contiguous Array.

---
+++ src/main/java/leetcode/ContiguousArray.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * 525. Contiguous Array
+ *
+ * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+ */
+public class ContiguousArray {
+    public int findMaxLength(int[] nums) {
+        return -1;
+    }
+}
---
+++ src/test/java/leetcode/ContiguousArrayTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ContiguousArrayTest {
+
+    @Test
+    public void findMaxLength1() {
+        assertEquals(2, new ContiguousArray().findMaxLength(new int[]{0, 1}));
+    }
+
+    @Test
+    public void findMaxLength2() {
+        assertEquals(2, new ContiguousArray().findMaxLength(new int[]{0, 1, 0}));
+    }
+}
