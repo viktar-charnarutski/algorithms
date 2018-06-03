df793d5cb6359460a47d35c04c3a98c959ed5f16
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-03 11:32:04 -0700

    Solution for the 718. Maximum Length of Repeated Subarray.

--- src/main/java/leetcode/MaximumLengthOfRepeatedSubarray.java
+++ src/main/java/leetcode/MaximumLengthOfRepeatedSubarray.java
@@ -2,19 +2,36 @@ package leetcode;
 
 /**
  * 718. Maximum Length of Repeated Subarray
- *
+ * <p>
  * Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
- *
+ * <p>
  * 1 <= len(A), len(B) <= 1000
  * 0 <= A[i], B[i] < 100
- *
+ * <p>
  * A: [1,2,3,2,1]
  * B: [3,2,1,4,7]
  * 3: 3, 2, 1
- *
  */
 public class MaximumLengthOfRepeatedSubarray {
     public int findLength(int[] A, int[] B) {
-        return -1;
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] == B[j]) {
+                    max = Math.max(count(A, B, i, j), max);
+                }
+            }
+        }
+        return max;
+    }
+
+    private static int count(int[] A, int[] B, int i, int j) {
+        int count = 0;
+        while (i < A.length && j < B.length && A[i] == B[j]) {
+            count++;
+            i++;
+            j++;
+        }
+        return count;
     }
 }
--- src/test/java/leetcode/MaximumLengthOfRepeatedSubarrayTest.java
+++ src/test/java/leetcode/MaximumLengthOfRepeatedSubarrayTest.java
@@ -7,7 +7,17 @@ import static org.junit.Assert.assertEquals;
 public class MaximumLengthOfRepeatedSubarrayTest {
 
     @Test
-    public void findLength() {
+    public void findLength1() {
         assertEquals(3, new MaximumLengthOfRepeatedSubarray().findLength(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7}));
+    }
+
+    @Test
+    public void findLength2() {
+        assertEquals(1, new MaximumLengthOfRepeatedSubarray().findLength(new int[]{1, 2, 3, 4, 5}, new int[]{3, 2, 1, 4, 7}));
+    }
+
+    @Test
+    public void findLength3() {
+        assertEquals(0, new MaximumLengthOfRepeatedSubarray().findLength(new int[]{1, 2, 3, 4, 5}, new int[]{6, 7, 8, 9, 0}));
     }
 }
