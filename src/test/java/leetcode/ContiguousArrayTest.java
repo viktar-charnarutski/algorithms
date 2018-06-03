ba93d036f0ac4b562e79699e66203ecb52078c94
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-03 12:39:57 -0700

    Adding a test for the 525. Contiguous Array.

--- src/test/java/leetcode/ContiguousArrayTest.java
+++ src/test/java/leetcode/ContiguousArrayTest.java
@@ -15,4 +15,9 @@ public class ContiguousArrayTest {
     public void findMaxLength2() {
         assertEquals(2, new ContiguousArray().findMaxLength(new int[]{0, 1, 0}));
     }
+
+    @Test
+    public void findMaxLength3() {
+        assertEquals(2, new ContiguousArray().findMaxLength(new int[]{1, 1, 1, 0, 1, 1, 1, 0}));
+    }
 }
