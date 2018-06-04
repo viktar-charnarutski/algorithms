c094c9c6d0c938458e9090c8f67b22a367da82da
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-03 18:35:08 -0700

    Solution for the 692. Top K Frequent Words.

--- src/main/java/leetcode/TopKFrequentWords.java
+++ src/main/java/leetcode/TopKFrequentWords.java
@@ -1,17 +1,27 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 692. Top K Frequent Words
- *
+ * <p>
  * Given a non-empty list of words, return the k most frequent elements.
- *
+ * <p>
  * Your answer should be sorted by frequency from highest to lowest.
  * If two words have the same frequency, then the word with the lower alphabetical order comes first.
  */
 public class TopKFrequentWords {
     public List<String> topKFrequent(String[] words, int k) {
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
+        }
+
+        List<String> res = new ArrayList<>(map.keySet());
+        res.sort((w1, w2) -> map.get(w1).equals(map.get(w2)) ? w1.compareTo(w2) : map.get(w2) - map.get(w1));
+        return res.subList(0, k);
     }
 }
