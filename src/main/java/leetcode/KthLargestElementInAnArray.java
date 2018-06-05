60a8d51eb92e8b5310a66622fd4db2e0c9dd99b6
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-05 08:55:17 -0700

    Solution for the 215. Kth Largest Element in an Array. PriorityQueue variant.

--- src/main/java/leetcode/KthLargestElementInAnArray.java
+++ src/main/java/leetcode/KthLargestElementInAnArray.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * 215. Kth Largest Element in an Array
@@ -9,7 +10,21 @@ import java.util.Arrays;
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
  */
 public class KthLargestElementInAnArray {
+
+    // n lg(k) - time
+    // n(k) - memory
     public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int n : nums) {
+            q.offer(n);
+            if (q.size() > k) q.poll();
+        }
+        return q.peek();
+    }
+
+    // n log(n) - time
+    // n(1) - memory
+    public int findKthLargest1(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
