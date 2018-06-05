0d920aa719956db5993382fc2b75dd5e331c1933
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-05 09:27:02 -0700

    Solution for the 795. Number of Subarrays with Bounded Maximum. Adding tests.

--- src/test/java/leetcode/NumberOfSubarraysWithBoundedMaximumTest.java
+++ src/test/java/leetcode/NumberOfSubarraysWithBoundedMaximumTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class NumberOfSubarraysWithBoundedMaximumTest {
 
     @Test
-    public void numSubarrayBoundedMax() {
+    public void numSubarrayBoundedMax1() {
         assertEquals(3, new NumberOfSubarraysWithBoundedMaximum().numSubarrayBoundedMax(new int[]{2, 1, 4, 3}, 2, 3));
+    }
+
+    @Test
+    public void numSubarrayBoundedMax2() {
+        assertEquals(7, new NumberOfSubarraysWithBoundedMaximum().numSubarrayBoundedMax(new int[]{2, 9, 2, 5, 6}, 2, 8));
     }
 }
