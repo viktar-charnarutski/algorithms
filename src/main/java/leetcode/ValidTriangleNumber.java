b99aeafb47589fab7c18f714c6c6198927556240
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-06 16:17:12 -0700

    Solution for the 611. Valid Triangle Number. Linear Scan solution.

--- src/main/java/leetcode/ValidTriangleNumber.java
+++ src/main/java/leetcode/ValidTriangleNumber.java
@@ -17,6 +17,20 @@ public class ValidTriangleNumber {
         for (int i = 0; i < nums.length - 2; i++) {
             int k = i + 2;
             for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
+                while (k < nums.length && nums[i] + nums[j] > nums[k]) k++;
+                count += k - j - 1;
+            }
+        }
+        return count;
+    }
+
+    public int triangleNumberBinarySearch(int[] nums) {
+        if (nums == null || nums.length < 3) return 0;
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int k = i + 2;
+            for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
                 k = binarySearch(nums, k, nums.length - 1, nums[i] + nums[j]);
                 count += k - j - 1;
             }
