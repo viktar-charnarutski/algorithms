c315a71eeb2b70bbad78090012f6f4ebcf218b38
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-06 07:23:18 -0700

    Solution for the 611. Valid Triangle Number. Adding tests.

--- src/test/java/leetcode/ValidTriangleNumberTest.java
+++ src/test/java/leetcode/ValidTriangleNumberTest.java
@@ -7,6 +7,13 @@ import static org.junit.Assert.assertEquals;
 public class ValidTriangleNumberTest {
 
     @Test
+    public void triangleNumber0() {
+        assertEquals(0, new ValidTriangleNumber().triangleNumber(null));
+        assertEquals(0, new ValidTriangleNumber().triangleNumber(new int[]{2}));
+        assertEquals(0, new ValidTriangleNumber().triangleNumber(new int[]{2, 2}));
+    }
+
+    @Test
     public void triangleNumber1() {
         assertEquals(3, new ValidTriangleNumber().triangleNumber(new int[]{2, 2, 3, 4}));
     }
