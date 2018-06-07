72f5557c123469004fd333c8d94e7b98da226cd6
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-06 17:12:37 -0700

    738. Monotone Increasing Digits. Initial commit.

---
+++ src/main/java/leetcode/MonotoneIncreasingDigits.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 738. Monotone Increasing Digits
+ * <p>
+ * Given a non-negative integer N, find the largest number
+ * that is less than or equal to N with monotone increasing digits.
+ * <p>
+ * (Recall that an integer has monotone increasing digits if and only if
+ * each pair of adjacent digits x and y satisfy x <= y.)
+ */
+public class MonotoneIncreasingDigits {
+    public int monotoneIncreasingDigits(int N) {
+        return -1;
+    }
+}
