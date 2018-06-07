49cfdc4d51fc7ea88bac42e01a3631dfe15c19e8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-07 07:22:00 -0700

    813. Largest Sum of Averages. Initial commit.

---
+++ src/main/java/leetcode/LargestSumOfAverages.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * 813. Largest Sum of Averages
+ * <p>
+ * We partition a row of numbers A into at most K adjacent (non-empty) groups,
+ * then our score is the sum of the average of each group. What is the largest score we can achieve?
+ * <p>
+ * Note that our partition must use every number in A, and that scores are not necessarily integers.
+ */
+public class LargestSumOfAverages {
+    public double largestSumOfAverages(int[] A, int K) {
+        return -1;
+    }
+}
