f3ee64836e9cb7a5b8a8b7a5d198584254c508c0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-08 07:40:06 -0700

    380. Insert Delete GetRandom O(1). Initial commit.

---
+++ src/main/java/leetcode/RandomizedSet.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * 380. Insert Delete GetRandom O(1)
+ * <p>
+ * Design a data structure that supports all following operations in average O(1) time.
+ * <p>
+ * insert(val): Inserts an item val to the set if not already present.
+ * remove(val): Removes an item val from the set if present.
+ * getRandom: Returns a random element from current set of elements.
+ * Each element must have the same probability of being returned.
+ */
+public class RandomizedSet {
+
+    public void insert(int val) {
+        return;
+    }
+
+    public void remove(int val) {
+        return;
+    }
+
+    public int getRandom() {
+        return -1;
+    }
+
+
+}
