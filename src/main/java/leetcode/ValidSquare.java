9199ca5fca44cc1f8dab2a555f2cdbaf39dffe22
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-09 13:01:48 -0700

    593. Valid Square. Solution.

--- src/main/java/leetcode/ValidSquare.java
+++ src/main/java/leetcode/ValidSquare.java
@@ -11,20 +11,12 @@ import java.util.Arrays;
  */
 public class ValidSquare {
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-        int[][] arr = {p1, p2, p3, p4};
-        Arrays.sort(arr, (e1, e2) -> (e1[0] == e2[0] ? e1[1] - e2[1] : e1[0] - e2[0]));
-        int a = getTopLength(p1, p2);
-        int b = getSideLength(p2, p3);
-        int c = getTopLength(p3, p4);
-        int d = getSideLength(p1, p4);
-        return a == b && b == c && c == d;
+        int[][] p = {p1, p2, p3, p4};
+        Arrays.sort(p, (e1, e2) -> (e1[0] == e2[0] ? e1[1] - e2[1] : e1[0] - e2[0]));
+        return dist(p[0], p[1]) != 0 && dist(p[0], p[1]) == dist(p[1], p[3]) && dist(p[1], p[3]) == dist(p[3], p[2]) && dist(p[3], p[2]) == dist(p[2], p[0]) && dist(p[0], p[3]) == dist(p[1], p[2]);
     }
 
-    private int getTopLength(int[] p1, int[] p2) {
-        return Math.abs(p1[0] - p2[0]);
-    }
-
-    private int getSideLength(int[] p1, int[] p2) {
-        return Math.abs(p1[1] - p2[1]);
+    private int dist(int[] p1, int[] p2) {
+        return (p2[1] - p1[1]) * (p2[1] - p1[1]) + (p2[0] - p1[0]) * (p2[0] - p1[0]);
     }
 }
