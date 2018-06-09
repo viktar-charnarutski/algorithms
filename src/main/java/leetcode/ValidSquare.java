9daac5c72ddd7ca91000aaf776894ed97e935cd1
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-08 17:15:00 -0700

    593. Valid Square. Initial commit.

---
+++ src/main/java/leetcode/ValidSquare.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 593. Valid Square
+ *
+ * Given the coordinates of four points in 2D space, return whether the four points could construct a square.
+ *
+ * The coordinate (x,y) of a point is represented by an integer array with two integers.
+ */
+public class ValidSquare {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return false;
+    }
+}
