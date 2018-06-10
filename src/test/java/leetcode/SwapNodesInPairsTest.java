3003788158db35fed8883401e6a76e2c49bc2bc0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-10 11:01:50 -0700

    24. Swap Nodes in Pairs. Tests.

---
+++ src/test/java/leetcode/SwapNodesInPairsTest.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SwapNodesInPairsTest {
+
+    @Test
+    public void swapPairs() {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        ListNode o1 = new ListNode(2);
+        ListNode o2 = new ListNode(1);
+        ListNode o3 = new ListNode(4);
+        ListNode o4 = new ListNode(3);
+        o1.next = o2;
+        o2.next = o3;
+        o3.next = o4;
+
+        assertEquals(o1, new SwapNodesInPairs().swapPairs(n1));
+    }
+}
