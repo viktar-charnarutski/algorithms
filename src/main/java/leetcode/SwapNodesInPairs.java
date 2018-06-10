5bb5192ec2d0f8e82c5ce0289bf7b507c11c09c4
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-10 10:58:51 -0700

    24. Swap Nodes in Pairs. Initial commit.

---
+++ src/main/java/leetcode/SwapNodesInPairs.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 24. Swap Nodes in Pairs
+ * <p>
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * <p>
+ * Example:
+ * <p>
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * <p>
+ * Note:
+ * <p>
+ * * Your algorithm should use only constant extra space.
+ * * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ */
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        return null;
+    }
+}
