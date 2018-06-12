dbbc60b91f20853efa552556c836db8528be149b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-12 16:32:21 -0700

    452. Minimum Number of Arrows to Burst Balloons. Tests.

--- src/test/java/leetcode/MinimumNumberOfArrowsToBurstBalloonsTest.java
+++ src/test/java/leetcode/MinimumNumberOfArrowsToBurstBalloonsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertSame;
 public class MinimumNumberOfArrowsToBurstBalloonsTest {
 
     @Test
-    public void findMinArrowShots() {
+    public void findMinArrowShots1() {
         int[][] input = new int[4][2];
         input[0][0] = 10;
         input[0][1] = 16;
@@ -17,6 +17,48 @@ public class MinimumNumberOfArrowsToBurstBalloonsTest {
         input[2][1] = 6;
         input[3][0] = 7;
         input[3][1] = 12;
+        assertSame(2, new MinimumNumberOfArrowsToBurstBalloons().findMinArrowShots(input));
+    }
+
+    @Test
+    public void findMinArrowShots2() {
+        int[][] input = new int[4][2];
+        input[0][0] = 10;
+        input[0][1] = 16;
+        input[1][0] = 2;
+        input[1][1] = 8;
+        input[2][0] = 1;
+        input[2][1] = 6;
+        input[3][0] = 7;
+        input[3][1] = 10;
+        assertSame(2, new MinimumNumberOfArrowsToBurstBalloons().findMinArrowShots(input));
+    }
+
+    @Test
+    public void findMinArrowShots3() {
+        int[][] input = new int[4][2];
+        input[0][0] = 10;
+        input[0][1] = 16;
+        input[1][0] = 7;
+        input[1][1] = 8;
+        input[2][0] = 1;
+        input[2][1] = 6;
+        input[3][0] = 9;
+        input[3][1] = 10;
+        assertSame(3, new MinimumNumberOfArrowsToBurstBalloons().findMinArrowShots(input));
+    }
+
+    @Test
+    public void findMinArrowShots4() {
+        int[][] input = new int[4][2];
+        input[0][0] = 4;
+        input[0][1] = 5;
+        input[1][0] = 2;
+        input[1][1] = 3;
+        input[2][0] = 1;
+        input[2][1] = 2;
+        input[3][0] = 3;
+        input[3][1] = 4;
         assertSame(2, new MinimumNumberOfArrowsToBurstBalloons().findMinArrowShots(input));
     }
 }
