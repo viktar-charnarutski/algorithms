e9489543b092f918ebf7b4044dfb8874034308b8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-11 21:18:58 -0700

    75. Sort Colors. Tests.

--- src/test/java/leetcode/SortColorsTest.java
+++ src/test/java/leetcode/SortColorsTest.java
@@ -7,9 +7,16 @@ import static org.junit.Assert.assertArrayEquals;
 public class SortColorsTest {
 
     @Test
-    public void sortColors() {
+    public void sortColors1() {
         int[] arr = {2, 0, 2, 1, 1, 0};
         new SortColors().sortColors(arr);
         assertArrayEquals(new int[]{0, 0, 1, 1, 2, 2}, arr);
+    }
+
+    @Test
+    public void sortColors2() {
+        int[] arr = {2, 0, 1};
+        new SortColors().sortColors(arr);
+        assertArrayEquals(new int[]{0, 1, 2}, arr);
     }
 }
