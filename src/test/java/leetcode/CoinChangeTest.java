05e3151e7f4db0a5cdf28f9dd69960216971e9e9
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-13 08:04:04 -0700

    322. Coin Change. Tests.

---
+++ src/test/java/leetcode/CoinChangeTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CoinChangeTest {
+
+    @Test
+    public void coinChange1() {
+        assertEquals(3, new CoinChange().coinChange(new int[]{1, 2, 5}, 11));
+    }
+
+    @Test
+    public void coinChange2() {
+        assertEquals(-1, new CoinChange().coinChange(new int[]{2}, 3));
+    }
+}
