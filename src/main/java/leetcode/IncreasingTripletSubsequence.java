e506aa91755092a6d95bcd723af1a5cac443a6d9
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-12 17:09:03 -0700

    334. Increasing Triplet Subsequence. Initial commit.

---
+++ src/main/java/leetcode/IncreasingTripletSubsequence.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * 334. Increasing Triplet Subsequence
+ * <p>
+ * Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+ * <p>
+ * Formally the function should:
+ * <p>Return true if there exists i, j, k
+ * such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+ * <p>
+ * Your algorithm should run in O(n) time complexity and O(1) space complexity.
+ */
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        return false;
+    }
+}
