4a82c973e0a8ecfd27458bf2d49c4b4b0b15cd86
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-13 21:44:24 -0700

    Fibonacci Numbers. Bottom-up solution.

--- src/main/java/dp/FibonacciNumbers.java
+++ src/main/java/dp/FibonacciNumbers.java
@@ -9,14 +9,26 @@ package dp;
  * The Rule is Xn = Xn-1 + Xn-2
  */
 public class FibonacciNumbers {
-    public int fib(int n) {
-        return fib(n, new int[n]);
+    public int fibBottomUp(int n) {
+        int[] fibs = new int[n + 1];
+        for (int i = 0; i < fibs.length; i++) {
+            if (i <= 2) {
+                fibs[i] = 1;
+            } else {
+                fibs[i] = fibs[i - 1] + fibs[i - 2];
+            }
+        }
+        return fibs[n];
     }
 
-    private int fib(int n, int[] memo) {
+    public int fibMemoization(int n) {
+        return fibMemoization(n, new int[n]);
+    }
+
+    private int fibMemoization(int n, int[] memo) {
         if (n <= 2) return 1;
         if (memo[n - 1] == 0) {
-            memo[n - 1] = fib(n - 1, memo) + fib(n - 2, memo);
+            memo[n - 1] = fibMemoization(n - 1, memo) + fibMemoization(n - 2, memo);
         }
         return memo[n - 1];
     }
--- src/test/java/dp/FibonacciNumbersTest.java
+++ src/test/java/dp/FibonacciNumbersTest.java
@@ -7,27 +7,52 @@ import static org.junit.Assert.assertEquals;
 public class FibonacciNumbersTest {
 
     @Test
-    public void fib1() {
-        assertEquals(1, new FibonacciNumbers().fib(1));
+    public void fib1_memoization() {
+        assertEquals(1, new FibonacciNumbers().fibMemoization(1));
     }
 
     @Test
-    public void fib2() {
-        assertEquals(1, new FibonacciNumbers().fib(2));
+    public void fib2_memoization() {
+        assertEquals(1, new FibonacciNumbers().fibMemoization(2));
     }
 
     @Test
-    public void fib3() {
-        assertEquals(2, new FibonacciNumbers().fib(3));
+    public void fib3_memoization() {
+        assertEquals(2, new FibonacciNumbers().fibMemoization(3));
     }
 
     @Test
-    public void fib7() {
-        assertEquals(13, new FibonacciNumbers().fib(7));
+    public void fib7_memoization() {
+        assertEquals(13, new FibonacciNumbers().fibMemoization(7));
     }
 
     @Test
-    public void fib13() {
-        assertEquals(233, new FibonacciNumbers().fib(13));
+    public void fib13_memoization() {
+        assertEquals(233, new FibonacciNumbers().fibMemoization(13));
+    }
+
+    @Test
+    public void fib1_fibBottomUp() {
+        assertEquals(1, new FibonacciNumbers().fibBottomUp(1));
+    }
+
+    @Test
+    public void fib2_fibBottomUp() {
+        assertEquals(1, new FibonacciNumbers().fibBottomUp(2));
+    }
+
+    @Test
+    public void fib3_fibBottomUp() {
+        assertEquals(2, new FibonacciNumbers().fibBottomUp(3));
+    }
+
+    @Test
+    public void fib7_fibBottomUp() {
+        assertEquals(13, new FibonacciNumbers().fibBottomUp(7));
+    }
+
+    @Test
+    public void fib13_fibBottomUp() {
+        assertEquals(233, new FibonacciNumbers().fibBottomUp(13));
     }
 }
