29e5f4d256c3bf3664d00a39e26802d3b81353cf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-14 07:49:26 -0700

    300. Longest Increasing Subsequence. Initial commit.

---
+++ src/main/java/leetcode/LongestIncreasingSubsequence.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * 300. Longest Increasing Subsequence
+ *
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ */
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        return 0;
+    }
+}
