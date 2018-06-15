d1f376ef69840c4f4def66d38929e6b890f0b41d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-14 22:16:05 -0700

    532. K-diff Pairs in an Array. Initial commit.

---
+++ src/main/java/leetcode/KDiffPairsInArray.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 532. K-diff Pairs in an Array
+ *
+ * Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array.
+ * Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their
+ * absolute difference is k.
+ */
+public class KDiffPairsInArray {
+    public int findPairs(int[] nums, int k) {
+        return 0;
+    }
+}
