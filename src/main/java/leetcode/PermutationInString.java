2d591381a2f3371b93e875e2b84ac2ba15e04fda
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-15 07:45:46 -0700

    567. Permutation in String. Initial commit.

---
+++ src/main/java/leetcode/PermutationInString.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * 567. Permutation in String
+ *
+ * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1.
+ * In other words, one of the first string's permutations is the substring of the second string.
+ */
+public class PermutationInString {
+    public boolean checkInclusion(String s1, String s2) {
+        return false;
+    }
+}
