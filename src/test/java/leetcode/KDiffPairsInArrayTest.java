3e2aaf44520bb89f32a3513e18c2fbfd341ace02
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-14 22:18:29 -0700

    532. K-diff Pairs in an Array. Tests.

---
+++ src/test/java/leetcode/KDiffPairsInArrayTest.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KDiffPairsInArrayTest {
+
+    @Test
+    public void findPairs1() {
+        assertEquals(2, new KDiffPairsInArray().findPairs(new int[]{3, 1, 4, 1, 5}, 2));
+    }
+
+    @Test
+    public void findPairs2() {
+        assertEquals(4, new KDiffPairsInArray().findPairs(new int[]{1, 2, 3, 4, 5}, 1));
+    }
+
+    @Test
+    public void findPairs3() {
+        assertEquals(1, new KDiffPairsInArray().findPairs(new int[]{1, 3, 1, 5, 4}, 0));
+    }
+}
