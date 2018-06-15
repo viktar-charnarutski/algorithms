cfb366d32b29e3b371eb4ff87f312eead44058de
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-14 17:08:40 -0700

    300. Longest Increasing Subsequence. Tests.

--- src/test/java/leetcode/LongestIncreasingSubsequenceTest.java
+++ src/test/java/leetcode/LongestIncreasingSubsequenceTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class LongestIncreasingSubsequenceTest {
 
     @Test
-    public void lengthOfLIS() {
+    public void lengthOfLIS0() {
+        assertEquals(0, new LongestIncreasingSubsequence().lengthOfLIS(new int[0]));
+    }
+
+    @Test
+    public void lengthOfLIS1() {
         assertEquals(4, new LongestIncreasingSubsequence().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
     }
 }
