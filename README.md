4aed2b61b46535dad1c88ab4669e432e3baccfbb
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-14 22:55:07 -0700

    532. K-diff Pairs in an Array. Solution.

--- README.md
+++ README.md
@@ -274,6 +274,7 @@ Solutions for algorithm questions which could be asked on a technical interview.
 | 218 | 13-Jun-2018 | [334. Increasing Triplet Subsequence][218] | Medium | Y |
 | 219 | 14-Jun-2018 | [322. Coin Change][219] | Medium | Y |
 | 220 | 15-Jun-2018 | [300. Longest Increasing Subsequence][220] | Medium | Y |
+| 221 | 15-Jun-2018 | [532. K-diff Pairs in an Array][221] | Easy | Y |
 
 [1]: https://leetcode.com/problems/judge-route-circle/
 [2]: https://leetcode.com/problems/reverse-string/
@@ -495,3 +496,4 @@ Solutions for algorithm questions which could be asked on a technical interview.
 [218]: https://leetcode.com/problems/increasing-triplet-subsequence/
 [219]: https://leetcode.com/problems/coin-change/
 [220]: https://leetcode.com/problems/longest-increasing-subsequence/
+[221]: https://leetcode.com/problems/k-diff-pairs-in-an-array/
--- src/main/java/leetcode/KDiffPairsInArray.java
+++ src/main/java/leetcode/KDiffPairsInArray.java
@@ -1,14 +1,44 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 532. K-diff Pairs in an Array
- *
+ * <p>
  * Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array.
  * Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their
  * absolute difference is k.
  */
 public class KDiffPairsInArray {
     public int findPairs(int[] nums, int k) {
-        return 0;
+        if (nums == null || nums.length < 2 || k < 0) return 0;
+        int count = 0;
+
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> indexes = map.get(nums[i]);
+            if (indexes == null) {
+                indexes = new HashSet<>();
+            }
+            indexes.add(i);
+            map.put(nums[i], indexes);
+        }
+
+        for (int firstNum : map.keySet()) {
+            int secondNum = firstNum - k;
+            if (k == 0) {
+                Set<Integer> indexes = map.get(secondNum);
+                if (indexes.size() > 1) count++;
+            } else {
+                if (map.containsKey(secondNum)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
