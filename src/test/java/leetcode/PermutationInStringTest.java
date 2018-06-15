6839aef7851ec4dcdb6328b82873d8e3afbb3ca2
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-15 08:06:22 -0700

    567. Permutation in String. Tests.

--- src/test/java/leetcode/PermutationInStringTest.java
+++ src/test/java/leetcode/PermutationInStringTest.java
@@ -9,11 +9,24 @@ public class PermutationInStringTest {
 
     @Test
     public void checkInclusion1() {
-        assertTrue(new PermutationInString().checkInclusion("eidbaooo", "ab"));
+        assertTrue(new PermutationInString().checkInclusion("ab", "eidbaooo"));
     }
 
     @Test
     public void checkInclusion2() {
-        assertFalse(new PermutationInString().checkInclusion("eidboaoo", "ab"));
+        assertFalse(new PermutationInString().checkInclusion("ab", "eidboaoo"));
+    }
+    @Test
+    public void checkInclusion3() {
+        assertTrue(new PermutationInString().checkInclusion("", "eidboaoo"));
+    }
+    @Test
+    public void checkInclusion4() {
+        assertFalse(new PermutationInString().checkInclusion("ab", ""));
+    }
+
+    @Test
+    public void checkInclusion5() {
+        assertTrue(new PermutationInString().checkInclusion("adc", "dcda"));
     }
 }
