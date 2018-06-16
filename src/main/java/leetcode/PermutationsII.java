48bd0eb97b22a566b4b4f19f7008ac030ad04434
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-16 10:59:20 -0700

    47. Permutations II. Initial commit.

---
+++ src/main/java/leetcode/PermutationsII.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 47. Permutations II
+ * <p>
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+ */
+public class PermutationsII {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        return Collections.emptyList();
+    }
+}
