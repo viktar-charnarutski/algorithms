254f7b0258cd725e1d8cfb0861dc6a6be65256f0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-15 17:08:14 -0700

    46. Permutations. Initial commit.

---
+++ src/main/java/leetcode/Permutations.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 46. Permutations
+ *
+ * Given a collection of distinct integers, return all possible permutations.
+ */
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        return Collections.emptyList();
+    }
+}
