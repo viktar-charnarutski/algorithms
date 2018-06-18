5d2186f183a72b341d417d55bf7c076ab1435fd7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-17 22:13:53 -0700

    47. Permutations II. Tests.

--- src/main/java/leetcode/PermutationsII.java
+++ src/main/java/leetcode/PermutationsII.java
@@ -1,6 +1,7 @@
 package leetcode;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,6 +11,26 @@ import java.util.List;
  */
 public class PermutationsII {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        return Collections.emptyList();
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, used, res, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<List<Integer>> res, ArrayList<Integer> curr) {
+        if (curr.size() == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            curr.add(nums[i]);
+            backtrack(nums, used, res, curr);
+            used[i] = false;
+            curr.remove(curr.size() - 1);
+        }
+
     }
 }
