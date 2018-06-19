c41719037d00a921406e1ab97b96ca9092e3c48b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-19 08:17:47 -0700

    Palindrome Permutation. Tests.

---
+++ src/test/java/leetcode/PalindromePermutationTest.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PalindromePermutationTest {
+
+    @Test
+    public void isPalindrome() {
+        assertTrue(new PalindromePermutation().isPalindrome("tact coa"));
+    }
+}
