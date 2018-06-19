6eecfdc50daf5d8418ccf809cb2f5acfa3c0eb65
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-18 21:51:25 -0700

    316. Remove Duplicate Letters. Initial commit.

---
+++ src/main/java/leetcode/RemoveDuplicateLetters.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * 316. Remove Duplicate Letters
+ *
+ * Given a string which contains only lowercase letters, remove duplicate letters so that every letter
+ * appear once and only once.
+ *
+ * You must make sure your result is the smallest in lexicographical order among all possible results.
+ *
+ * bcabc - abc
+ */
+public class RemoveDuplicateLetters {
+    public String removeDuplicateLetters(String s) {
+        return null;
+    }
+}
