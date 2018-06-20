499f1597d749219ef273a488af5b537fcce04599
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-19 18:00:32 -0700

    Palindrome Permutation. Solution.

--- src/main/java/leetcode/PalindromePermutation.java
+++ src/main/java/leetcode/PalindromePermutation.java
@@ -1,14 +1,23 @@
 package leetcode;
 
 /**
- * Palindrome Permutation.
- *
+ * 266. Palindrome Permutation
+ * <p>
  * Given a string, write a function to check if it is a permutation of a palindrome.
  * A palindrome is a word or phrase that is the same forwards and backwards. A permutation
  * is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
  */
 public class PalindromePermutation {
     public boolean isPalindrome(String s) {
-        return false;
+        if (s == null || s.length() == 0) return false;
+        int[] charsCount = new int[26];
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c)) charsCount[c - 'a']++;
+        }
+        int count = 0;
+        for (int i : charsCount) {
+            count += i % 2;
+        }
+        return count <= 1;
     }
 }
