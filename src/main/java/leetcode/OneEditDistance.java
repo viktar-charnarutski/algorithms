c904a9363036dd5b3f4df2a1f89b715e15c56316
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-19 20:15:38 -0700

    161. One Edit Distance. Solution.

--- src/main/java/leetcode/OneEditDistance.java
+++ src/main/java/leetcode/OneEditDistance.java
@@ -9,6 +9,25 @@ package leetcode;
  */
 public class OneEditDistance {
     public boolean isOneEditDistance(String s1, String s2) {
-        return false;
+        if (Math.abs(s1.length() - s2.length()) > 1) return false;
+
+        int i1 = 0, i2 = 0, count = 0;
+        while (i1 < s1.length() && i2 < s2.length()) {
+            if (s1.charAt(i1) == s2.charAt(i2)) {
+                i1++;
+                i2++;
+            } else {
+                if (++count > 1) return false;
+                if (s1.length() > s2.length()) {
+                    i1++;
+                } else if (s2.length() > s1.length()) {
+                    i2++;
+                } else {
+                    i1++;
+                    i2++;
+                }
+            }
+        }
+        return true;
     }
 }
