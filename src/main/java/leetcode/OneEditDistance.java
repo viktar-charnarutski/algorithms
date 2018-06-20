a245b49f498d9616d4e62a59ad3aa38f6f708ec1
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-19 18:22:08 -0700

    161. One Edit Distance. Initial commit.

---
+++ src/main/java/leetcode/OneEditDistance.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * 161. One Edit Distance
+ * <p>
+ * There are three types of edits that can be performed on strings: insert a character,
+ * remove a character, or replace a character. Given two strings, write a function to check if they are
+ * one edit (or zero edits) away.
+ */
+public class OneEditDistance {
+    public boolean isOneEditDistance(String s1, String s2) {
+        return false;
+    }
+}
