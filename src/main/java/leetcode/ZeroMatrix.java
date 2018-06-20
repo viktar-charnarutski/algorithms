801ef69a3b864262fc111fc15a6b237a687ca64a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-20 16:31:35 -0700

    Zero Matrix. Solution.

--- src/main/java/leetcode/ZeroMatrix.java
+++ src/main/java/leetcode/ZeroMatrix.java
@@ -8,6 +8,29 @@ package leetcode;
  */
 public class ZeroMatrix {
     public void zeroMatrix(int[][] m) {
-        return;
+        int[][] n = copyMatrix(m);
+
+        for (int c = 0; c < m.length; c++) {
+            for (int r = 0; r < m[0].length; r++) {
+                if (n[c][r] == 0) mark(m, c, r);
+            }
+        }
+    }
+
+    private int[][] copyMatrix(int[][] m) {
+        int[][] n = new int[m.length][m[0].length];
+        for (int c = 0; c < m.length; c++) {
+            System.arraycopy(m[c], 0, n[c], 0, m[0].length);
+        }
+        return n;
+    }
+
+    private void mark(int[][] m, int c, int r) {
+        for (int i = 0; i < m.length; i++) {
+            m[c][i] = 0;
+        }
+        for (int i = 0; i < m[0].length; i++) {
+            m[i][r] = 0;
+        }
     }
 }
