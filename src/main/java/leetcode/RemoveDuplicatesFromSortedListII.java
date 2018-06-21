9b891921894e479989ee047125ccf952189f1f82
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-20 17:22:17 -0700

    82. Remove Duplicates from Sorted List II. Initial commit.

---
+++ src/main/java/leetcode/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * 82. Remove Duplicates from Sorted List II
+ *
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ */
+public class RemoveDuplicatesFromSortedListII {
+    public ListNode deleteDuplicates(ListNode head) {
+        return head;
+    }
+}
