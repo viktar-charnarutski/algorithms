4508034832722fd55f46f0f308ff1ab418c97be0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-20 22:57:54 -0700

    82. Remove Duplicates from Sorted List II. Tests.

--- src/test/java/leetcode/ZeroMatrixTest.java
+++ src/test/java/leetcode/ZeroMatrixTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertArrayEquals;
 public class ZeroMatrixTest {
 
     @Test
-    public void zeroMatrix() {
+    public void zeroMatrix0() {
         int[][] m = new int[5][5];
         m[0][0] = 1;
         m[0][1] = 1;
@@ -61,7 +61,41 @@ public class ZeroMatrixTest {
         n[4][2] = 0;
         n[4][3] = 0;
         n[4][4] = 0;
-        new ZeroMatrix().zeroMatrix(m);
+        new ZeroMatrix().setZeroes(m);
+        assertArrayEquals(n, m);
+    }
+
+    @Test
+    public void zeroMatrix1() {
+        int[][] m = new int[3][4];
+        m[0][0] = 0;
+        m[0][1] = 1;
+        m[0][2] = 2;
+        m[0][3] = 0;
+        m[1][0] = 3;
+        m[1][1] = 4;
+        m[1][2] = 5;
+        m[1][3] = 2;
+        m[2][0] = 1;
+        m[2][1] = 3;
+        m[2][2] = 1;
+        m[2][3] = 5;
+
+        int[][] n = new int[3][4];
+        n[0][0] = 0;
+        n[0][1] = 0;
+        n[0][2] = 0;
+        n[0][3] = 0;
+        n[1][0] = 0;
+        n[1][1] = 4;
+        n[1][2] = 5;
+        n[1][3] = 0;
+        n[2][0] = 0;
+        n[2][1] = 3;
+        n[2][2] = 1;
+        n[2][3] = 0;
+
+        new ZeroMatrix().setZeroes(m);
         assertArrayEquals(n, m);
     }
 }
