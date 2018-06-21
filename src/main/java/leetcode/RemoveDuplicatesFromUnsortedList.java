24b13ea25625bd8727c54af6eb1a4394b061fc7d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-21 07:57:36 -0700

    Remove Duplicates from Unsorted List. Initial commit.

---
+++ src/main/java/leetcode/RemoveDuplicatesFromUnsortedList.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * Remove Duplicates from Unsorted List
+ *
+ * Write code to remove duplicates from an unsorted linked list.
+ */
+public class RemoveDuplicatesFromUnsortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        return head;
+    }
+}
