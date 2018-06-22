a73dcd10aef5f758c0161bdd953b629efc713e67
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-22 08:03:48 -0700

    86. Partition List. Solution.

--- src/main/java/leetcode/PartitionList.java
+++ src/main/java/leetcode/PartitionList.java
@@ -10,6 +10,27 @@ package leetcode;
  */
 public class PartitionList {
     public ListNode partition(ListNode head, int x) {
-        return head;
+        ListNode dummyHead1 = new ListNode(0);
+        ListNode dummyHead2 = new ListNode(0);
+        dummyHead1.next = head;
+        dummyHead2.next = head;
+
+        ListNode curr1 = dummyHead1;
+        ListNode curr2 = dummyHead2;
+
+        while (head != null) {
+            if (head.val < x) {
+                curr1.next = head;
+                curr1 = head;
+            } else {
+                curr2.next = head;
+                curr2 = head;
+            }
+            head = head.next;
+        }
+
+        curr2.next = null;
+        curr1.next = dummyHead2.next;
+        return dummyHead1.next;
     }
 }
