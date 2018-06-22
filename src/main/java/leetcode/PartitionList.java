06997cbcb4481f2e999d7fc161110625bbe4ce68
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-22 07:31:10 -0700

    86. Partition List. Initial commit.

---
+++ src/main/java/leetcode/PartitionList.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * 86. Partition List
+ * <p>
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater
+ * than or equal to x.
+ * <p>
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ */
+public class PartitionList {
+    public ListNode partition(ListNode head, int x) {
+        return head;
+    }
+}
