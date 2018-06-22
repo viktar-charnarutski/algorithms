7ce50844cc2325881446e6f16d51f2805d5f2d77
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-21 17:21:01 -0700

    19. Remove Nth Node From End of List. Tests.

--- src/test/java/leetcode/RemoveNthNodeFromEndOfListTest.java
+++ src/test/java/leetcode/RemoveNthNodeFromEndOfListTest.java
@@ -1,14 +1,22 @@
 package leetcode;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class RemoveNthNodeFromEndOfListTest {
 
-    @Test
-    public void removeNthFromEnd() {
-        ListNode n1 = new ListNode(1);
+    private ListNode n1;
+    private ListNode k1;
+    private ListNode m1;
+    private ListNode l1;
+    private RemoveNthNodeFromEndOfList ins;
+
+    @Before
+    public void init() {
+        n1 = new ListNode(1);
         ListNode n2 = new ListNode(2);
         ListNode n3 = new ListNode(3);
         ListNode n4 = new ListNode(4);
@@ -18,14 +26,45 @@ public class RemoveNthNodeFromEndOfListTest {
         n3.next = n4;
         n4.next = n5;
 
-        ListNode n21 = new ListNode(1);
-        ListNode n22 = new ListNode(2);
-        ListNode n24 = new ListNode(4);
-        ListNode n25 = new ListNode(5);
-        n21.next = n22;
-        n22.next = n24;
-        n24.next = n25;
+        k1 = new ListNode(1);
+        ListNode k2 = new ListNode(2);
+        ListNode k3 = new ListNode(3);
+        ListNode k4 = new ListNode(4);
+        ListNode k5 = new ListNode(5);
+        k1.next = k2;
+        k2.next = k3;
+        k3.next = k4;
+        k4.next = k5;
 
-        assertEquals(n21, new RemoveNthNodeFromEndOfList().removeNthFromEnd(n1, 2));
+        m1 = new ListNode(1);
+        ListNode m2 = new ListNode(2);
+        ListNode m3 = new ListNode(3);
+        ListNode m5 = new ListNode(5);
+        m1.next = m2;
+        m2.next = m3;
+        m3.next = m5;
+
+        l1 = new ListNode(1);
+        ins = new RemoveNthNodeFromEndOfList();
+    }
+
+    @Test
+    public void removeNthFromEnd() {
+        assertEquals(m1, ins.removeNthFromEnd(n1, 2));
+    }
+
+    @Test
+    public void removeNthFromEnd_target_is_zero() {
+        assertEquals(k1, ins.removeNthFromEnd(n1, 0));
+    }
+
+    @Test
+    public void removeNthFromEnd_one_node_target_is_one() {
+        assertNull(ins.removeNthFromEnd(l1, 1));
+    }
+
+    @Test
+    public void count() {
+        assertEquals(5, ins.countNodes(n1));
     }
 }
