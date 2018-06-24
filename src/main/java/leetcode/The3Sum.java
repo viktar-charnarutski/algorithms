36c1b3c6189d2ad42ebfd4dcb107a6498121ffad
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-24 11:49:04 -0700

    15. 3Sum. Initial commit.

---
+++ src/main/java/leetcode/The3Sum.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 15. 3Sum
+ * <p>
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ * <p>
+ * Note:
+ * <p>
+ * The solution set must not contain duplicate triplets.
+ */
+public class The3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        return Collections.emptyList();
+    }
+}
