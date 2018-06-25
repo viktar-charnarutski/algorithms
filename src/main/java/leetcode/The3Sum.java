6b6889e8a9be3c772886cad802271b5b548372c8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-25 07:33:47 -0700

    15. 3Sum. Solution.

--- src/main/java/leetcode/The3Sum.java
+++ src/main/java/leetcode/The3Sum.java
@@ -1,6 +1,7 @@
 package leetcode;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,6 +16,25 @@ import java.util.List;
  */
 public class The3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
-        return Collections.emptyList();
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1, k = nums.length - 1; j < k; ) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
     }
 }
