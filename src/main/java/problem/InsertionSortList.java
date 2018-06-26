108981799250adbb432aa78579ed1e8fe57e9e39
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-26 16:41:32 -0700

    147. Insertion Sort List. Initial commit.

---
+++ src/main/java/problem/InsertionSortList.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * 147. Insertion Sort List
+ *
+ * Sort a linked list using insertion sort.
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        return head;
+    }
+}
