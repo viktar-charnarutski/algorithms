3597ded1802fc493248913fe31a00b4f3edb9e6e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-25 18:58:17 -0700

    109. Convert Sorted List to Binary Search Tree. Fixing the solution.

--- src/main/java/leetcode/ConvertSortedListToBinarySearchTree.java
+++ src/main/java/leetcode/ConvertSortedListToBinarySearchTree.java
@@ -9,58 +9,28 @@ package leetcode;
  * subtrees of every node never differ by more than 1.
  */
 public class ConvertSortedListToBinarySearchTree {
+
     public TreeNode sortedListToBST(ListNode head) {
         if (head == null) return null;
 
-        ListNode reversed = reverseList(head);
-        ListNode mid = findMiddleNode(reversed);
-
-        TreeNode treeNodeHead = new TreeNode(mid.val);
-
-        ListNode currListNode = reversed;
-        TreeNode currTreeNode = treeNodeHead;
-        while (currListNode != mid) {
-            currTreeNode.right = new TreeNode(currListNode.val);
-
-            currTreeNode = treeNodeHead.right;
-            currListNode = currListNode.next;
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
         }
 
-        currListNode = currListNode.next;
-        currTreeNode = treeNodeHead;
-        while (currListNode != null) {
-            currTreeNode.left = new TreeNode(currListNode.val);
+        TreeNode root = new TreeNode(slow.val);
 
-            currTreeNode = treeNodeHead.left;
-            currListNode = currListNode.next;
-        }
-        return treeNodeHead;
-    }
-
-    ListNode reverseList(ListNode head) {
-        ListNode curr = head, prev = null;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
-    ListNode findMiddleNode(ListNode head) {
-        int count = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            curr = curr.next;
-            count++;
+        if (prev != null) {
+            prev.next = null;
+        } else {
+            head = null;
         }
 
-        curr = head;
-        count /= 2;
-        while (count-- > 0) {
-            curr = curr.next;
-        }
-        return curr;
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+
+        return root;
     }
 }
--- src/test/java/leetcode/ConvertSortedListToBinarySearchTreeTest.java
+++ src/test/java/leetcode/ConvertSortedListToBinarySearchTreeTest.java
@@ -27,47 +27,8 @@ public class ConvertSortedListToBinarySearchTreeTest {
         t3.left = t2;
         t2.left = t1;
         t3.right = t5;
-        t5.right = t4;
+        t5.left = t4;
 
         assertEquals(t3, new ConvertSortedListToBinarySearchTree().sortedListToBST(n1));
-    }
-
-    @Test
-    public void findMiddle() {
-        ListNode n1 = new ListNode(-10);
-        ListNode n2 = new ListNode(-3);
-        ListNode n3 = new ListNode(0);
-        ListNode n4 = new ListNode(5);
-        ListNode n5 = new ListNode(9);
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
-
-        assertEquals(n3, new ConvertSortedListToBinarySearchTree().findMiddleNode(n1));
-    }
-    @Test
-    public void reverseLinkedList() {
-        ListNode n1 = new ListNode(-10);
-        ListNode n2 = new ListNode(-3);
-        ListNode n3 = new ListNode(0);
-        ListNode n4 = new ListNode(5);
-        ListNode n5 = new ListNode(9);
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
-
-        ListNode m1 = new ListNode(9);
-        ListNode m2 = new ListNode(5);
-        ListNode m3 = new ListNode(0);
-        ListNode m4 = new ListNode(-3);
-        ListNode m5 = new ListNode(-10);
-        m1.next = m2;
-        m2.next = m3;
-        m3.next = m4;
-        m4.next = m5;
-
-        assertEquals(m1, new ConvertSortedListToBinarySearchTree().reverseList(n1));
     }
 }
