9c6133a6788c4e831871f93b1ce993fa03173c20
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-25 21:44:10 -0700

    203. Remove Linked List Elements. Refactoring.

--- src/main/java/leetcode/RemoveLinkedListElements.java
+++ src/main/java/leetcode/RemoveLinkedListElements.java
@@ -8,7 +8,21 @@ package leetcode;
 public class RemoveLinkedListElements {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
-        head.next = removeElements(head.next, val);
-        return head.val == val ? head.next : head;
+
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        ListNode curr = head;
+        ListNode prev = fakeHead;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+
+        return fakeHead.next;
     }
 }
