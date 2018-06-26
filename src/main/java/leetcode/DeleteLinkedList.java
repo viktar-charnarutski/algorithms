e2e8bfaffb940d7368da7b2947735b9524d50d71
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-26 08:15:38 -0700

    Delete a Linked List. Solution.

---
+++ src/main/java/leetcode/DeleteLinkedList.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * Delete a Linked List
+ * <p>
+ * Write a function to delete a Linked List.
+ */
+public class DeleteLinkedList {
+    public void deleteLinkedList(ListNode head) {
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = null;
+            head = next;
+        }
+    }
+}
