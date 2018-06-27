603d854c7ea77b57f848285d6559bec5ed8e1446
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-26 22:09:41 -0700

    147. Insertion Sort List. Tests.

--- src/test/java/problem/InsertionSortListTest.java
+++ src/test/java/problem/InsertionSortListTest.java
@@ -8,15 +8,15 @@ public class InsertionSortListTest {
 
     @Test
     public void insertionSortList() {
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-        ListNode n5 = new ListNode(5);
-        n4.next = n2;
-        n2.next = n5;
-        n5.next = n1;
-        n1.next = n3;
+        ListNode n1 = new ListNode(3);
+        ListNode n2 = new ListNode(1);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(5);
+        ListNode n5 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
 
         ListNode m1 = new ListNode(1);
         ListNode m2 = new ListNode(2);
@@ -28,6 +28,6 @@ public class InsertionSortListTest {
         m3.next = m4;
         m4.next = m5;
 
-        assertEquals(m1, new InsertionSortList().insertionSortList(m1));
+        assertEquals(m1, new InsertionSortList().insertionSortList(n1));
     }
 }
