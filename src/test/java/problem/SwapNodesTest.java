115d0607e0f65c268e0f10f94e53e48662331eb8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-27 17:37:46 -0700

    Swap Nodes. Tests.

---
+++ src/test/java/problem/SwapNodesTest.java
@@ -0,0 +1,38 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SwapNodesTest {
+
+    @Test
+    public void swap() {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        ListNode m1 = new ListNode(1);
+        ListNode m2 = new ListNode(2);
+        ListNode m3 = new ListNode(5);
+        ListNode m4 = new ListNode(4);
+        ListNode m5 = new ListNode(3);
+        ListNode m6 = new ListNode(6);
+
+        m1.next = m2;
+        m2.next = m3;
+        m3.next = m4;
+        m4.next = m5;
+        m5.next = m6;
+
+        assertEquals(m1, new SwapNodes().swap(n1, 3, 5));
+    }
+}
