7739802ede0ad0bf165290d001b5ec1d1c5e5e82
viktar-charnarutski <v.charnarutski@gmail.com>
2018-06-30 21:25:11 -0700

    Queue Via Stacks. Initial commit.

--- src/main/java/problem/QueueViaStacks.java
+++ src/main/java/problem/QueueViaStacks.java
@@ -2,9 +2,18 @@ package problem;
 
 /**
  * Queue via Stacks
- *
+ * <p>
  * Implement a MyQueue class which implements a queue using two stacks.
  */
-public class QueueViaStacks {
+public class QueueViaStacks<T> {
+    public void add(T value) {
+    }
 
+    public T peek() {
+        return null;
+    }
+
+    public T remove() {
+        return null;
+    }
 }
