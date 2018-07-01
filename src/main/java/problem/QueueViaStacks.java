3cadb6fe697f4abce658e9236ceb57426e86b334
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-01 14:18:11 -0700

    Queue Via Stacks. Implementation.

--- src/main/java/problem/QueueViaStacks.java
+++ src/main/java/problem/QueueViaStacks.java
@@ -1,19 +1,47 @@
 package problem;
 
+import java.util.Stack;
+
 /**
  * Queue via Stacks
  * <p>
- * Implement a MyQueue class which implements a queue using two stacks.
+ * Implement a QueueViaStacks class which implements a queue using two stacks.
+ * 1 2 3 4 5
+ *
  */
 public class QueueViaStacks<T> {
+
+    private Stack<T> stackNew;
+    private Stack<T> stackOld;
+
+    public QueueViaStacks() {
+        stackNew = new Stack<>();
+        stackOld = new Stack<>();
+    }
+
     public void add(T value) {
+        stackNew.push(value);
     }
 
     public T peek() {
-        return null;
+        shiftStacks();
+        return stackOld.peek();
+    }
+
+    private void shiftStacks() {
+        if (stackOld.isEmpty()) {
+            while (!stackNew.isEmpty()) {
+                stackOld.push(stackNew.pop());
+            }
+        }
     }
 
     public T remove() {
-        return null;
+        shiftStacks();
+        return stackOld.pop();
+    }
+
+    public int size() {
+        return stackNew.size() + stackOld.size();
     }
 }
