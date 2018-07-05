1526f08179bd495e41b0893d2d0a34263b3f4f9d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-05 08:31:29 -0700

    List of Depths. Initial commit.

---
+++ src/main/java/problem/ListOfDepths.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * List of Depths
+ *
+ * Given a binary tree, design an algorithm which creates a linked list of all the nodes
+ * at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+ */
+public class ListOfDepths {
+
+    public List<Integer> listOfDepths(TreeNode node) {
+        return Collections.emptyList();
+    }
+}
