9020437a805fec6ab231e7bdc905c2fa468b0e47
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-06 09:29:24 -0700

    List of Depths. Fixing the tests.

--- src/test/java/problem/ListOfDepthsTest.java
+++ src/test/java/problem/ListOfDepthsTest.java
@@ -12,27 +12,6 @@ public class ListOfDepthsTest {
 
     @Test
     public void listOfDepths() {
-        List<LinkedList<TreeNode>> expected = new ArrayList<>();
-        LinkedList<TreeNode> list1 = new LinkedList<>();
-        list1.add(new TreeNode(1));
-        list1.add(new TreeNode(2));
-        list1.add(new TreeNode(4));
-        expected.add(list1);
-        LinkedList<TreeNode> list2 = new LinkedList<>();
-        list2.add(new TreeNode(1));
-        list2.add(new TreeNode(2));
-        list2.add(new TreeNode(5));
-        expected.add(list2);
-        LinkedList<TreeNode> list3 = new LinkedList<>();
-        list3.add(new TreeNode(1));
-        list3.add(new TreeNode(3));
-        list3.add(new TreeNode(6));
-        expected.add(list3);
-        LinkedList<TreeNode> list4 = new LinkedList<>();
-        list4.add(new TreeNode(1));
-        list4.add(new TreeNode(3));
-        list4.add(new TreeNode(7));
-        expected.add(list4);
 
         TreeNode n1 = new TreeNode(1);
         TreeNode n2 = new TreeNode(2);
@@ -48,6 +27,20 @@ public class ListOfDepthsTest {
         n3.left = n6;
         n3.right = n7;
 
+        List<LinkedList<TreeNode>> expected = new ArrayList<>();
+        LinkedList<TreeNode> list1 = new LinkedList<>();
+        list1.add(n1);
+        expected.add(list1);
+        LinkedList<TreeNode> list2 = new LinkedList<>();
+        list2.add(n2);
+        list2.add(n3);
+        expected.add(list2);
+        LinkedList<TreeNode> list3 = new LinkedList<>();
+        list3.add(n4);
+        list3.add(n5);
+        list3.add(n6);
+        list3.add(n7);
+        expected.add(list3);
         assertEquals(expected, new ListOfDepths().listOfDepths(n1));
     }
 }
