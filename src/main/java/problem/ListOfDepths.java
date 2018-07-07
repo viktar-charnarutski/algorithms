00d3ad793e4ea40c420cfb9c86b5d991fbf76d62
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-07 09:04:11 -0700

    List of Depths. Solution.

--- src/main/java/problem/ListOfDepths.java
+++ src/main/java/problem/ListOfDepths.java
@@ -1,7 +1,9 @@
 package problem;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * List of Depths
@@ -11,7 +13,25 @@ import java.util.List;
  */
 public class ListOfDepths {
 
-    public List<Integer> listOfDepths(TreeNode node) {
-        return Collections.emptyList();
+    public List<LinkedList<TreeNode>> listOfDepths(TreeNode node) {
+        List<LinkedList<TreeNode>> listOfDepths = new ArrayList<>();
+
+        Queue<TreeNode> levels = new LinkedList<>();
+        levels.offer(node);
+
+        while (!levels.isEmpty()) {
+            LinkedList<TreeNode> currentDepth = new LinkedList<>();
+
+            int size = levels.size();
+            while (size-- > 0) {
+                TreeNode curr = levels.poll();
+                if (curr.left != null) levels.offer(curr.left);
+                if (curr.right != null) levels.offer(curr.right);
+                currentDepth.add(curr);
+            }
+            listOfDepths.add(currentDepth);
+        }
+
+        return listOfDepths;
     }
 }
