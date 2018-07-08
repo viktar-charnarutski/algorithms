a9f3e962566e1733ae8dc5a2d4470c30d3f2d2d7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-08 15:47:25 -0700

    Validate BST. Initial commit.

---
+++ src/main/java/problem/ValidateBst.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * Validate BST
+ * <p>
+ * Implement a function to check if a binary tree is a binary search tree.
+ */
+public class ValidateBst {
+    public boolean isBst(TreeNode node) {
+        return false;
+    }
+}
