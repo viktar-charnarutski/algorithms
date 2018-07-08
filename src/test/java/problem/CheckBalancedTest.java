7f6ab7e7b49e26236c19f58b3a22677f8e5750fb
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-07 21:38:01 -0700

    Check Balanced. Tests.

---
+++ src/test/java/problem/CheckBalancedTest.java
@@ -0,0 +1,56 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CheckBalancedTest {
+
+    @Test
+    public void isBalanced_true() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+
+        n4.left = n2;
+        n4.right = n6;
+
+        n2.left = n1;
+        n2.right = n3;
+
+        n6.left = n5;
+        n6.right = n7;
+
+        assertTrue(new CheckBalanced().isBalanced(n1));
+    }
+
+    @Test
+    public void isBalanced_false() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+
+        n4.left = n2;
+        n4.right = n6;
+
+        n2.left = n1;
+        n2.right = n3;
+
+        n6.left = n5;
+        n6.right = n7;
+
+        n7.left = n8;
+
+        assertFalse(new CheckBalanced().isBalanced(n1));
+    }
+}
