3c947839d769e171c82cc25c636c93308e806e3d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-08 16:00:55 -0700

    Validate BST. Solution.

--- src/main/java/problem/ValidateBst.java
+++ src/main/java/problem/ValidateBst.java
@@ -7,6 +7,29 @@ package problem;
  */
 public class ValidateBst {
     public boolean isBst(TreeNode node) {
-        return false;
+        if (node == null) return true;
+
+        TreeNode left = node.left;
+        TreeNode right = node.right;
+
+        boolean isBst = false;
+
+        if (left != null && right != null) {
+
+            if (left.val < node.val && right.val > node.val) {
+                isBst = isBst(left) && isBst(right);
+            }
+
+        } else if (left != null && left.val < node.val) {
+            isBst = isBst(left);
+
+        } else if (right != null && right.val > node.val) {
+            isBst = isBst(right);
+
+        } else {
+            isBst = true;
+        }
+
+        return isBst;
     }
 }
