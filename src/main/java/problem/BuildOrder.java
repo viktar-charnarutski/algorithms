49d0d00ae959d9d82fd114b43977616d313c83ac
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-09 07:41:38 -0700

    Build Order. Initial commit.

---
+++ src/main/java/problem/BuildOrder.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Build Order
+ * <p>
+ * You are given a list of projects and a list of dependencies (which is a list of pairs of
+ * projects, where the second project is dependent on the first project). All of a project's dependencies
+ * must be built before the project is. Find a build order that will allow the projects to be built. If there
+ * is no valid build order, return an error.
+ */
+public class BuildOrder {
+
+    public char[] buildOrder(char[] projets, char[][] dependencies) {
+        return null;
+    }
+}
