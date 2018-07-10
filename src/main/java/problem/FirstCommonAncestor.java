fc402a74dc9c72629f25fd103608980bfa9e66b4
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-09 20:06:26 -0700

    First Common Ancestor. Initial commit.

---
+++ src/main/java/problem/FirstCommonAncestor.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * First Common Ancestor
+ * <p>
+ * Design an algorithm and write code to find the first common ancestor
+ * of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
+ * necessarily a binary search tree.
+ */
+public class FirstCommonAncestor {
+
+    public TreeNode firstCommonAncestor(TreeNode node1, TreeNode node2) {
+        return null;
+    }
+}
