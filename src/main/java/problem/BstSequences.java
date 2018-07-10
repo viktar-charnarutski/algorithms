7c3adca9dbe1d325800a93d1b77c80570ad9a18b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-10 09:55:52 -0700

    First Common Ancestor. Solution.

--- src/main/java/problem/BstSequences.java
+++ src/main/java/problem/BstSequences.java
@@ -1,7 +1,9 @@
 package problem;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * BST Sequences
@@ -13,6 +15,44 @@ import java.util.List;
 public class BstSequences {
 
     public List<List<Integer>> bstSequences(TreeNode root) {
-        return Collections.emptyList();
+        List<List<Integer>> resultSequences = new ArrayList<>();
+
+        List<Integer> currentSequence = new ArrayList<>();
+
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.offer(root);
+        currentSequence.add(root.val);
+
+        createSequence(currentSequence, nodes, resultSequences);
+
+        return resultSequences;
+    }
+
+    private void createSequence(List<Integer> currentSequence, Queue<TreeNode> nodes, List<List<Integer>> resultSequences) {
+        if (nodes.isEmpty()) {
+            resultSequences.add(new ArrayList<>(currentSequence));
+            return;
+        }
+
+        TreeNode curr = nodes.poll();
+        if (curr.left != null) {
+            currentSequence.add(curr.left.val);
+            nodes.offer(curr.left);
+        }
+        if (curr.right != null) {
+            currentSequence.add(curr.right.val);
+            nodes.offer(curr.right);
+        }
+
+        createSequence(currentSequence, nodes, resultSequences);
+
+        if (curr.left != null && curr.right != null) {
+            currentSequence.remove(currentSequence.size() - 1);
+            currentSequence.remove(currentSequence.size() - 1);
+            currentSequence.add(curr.right.val);
+            currentSequence.add(curr.left.val);
+            createSequence(currentSequence, nodes, resultSequences);
+        }
+
     }
 }
