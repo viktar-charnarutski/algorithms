d2869e3b6b02ce3240ccb9b23bdaf1c2a02f33bc
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-09 22:19:42 -0700

    First Common Ancestor. Solution.

--- src/main/java/problem/FirstCommonAncestor.java
+++ src/main/java/problem/FirstCommonAncestor.java
@@ -1,5 +1,7 @@
 package problem;
 
+import java.util.Stack;
+
 /**
  * First Common Ancestor
  * <p>
@@ -10,6 +12,43 @@ package problem;
 public class FirstCommonAncestor {
 
     public TreeNode firstCommonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
+        Stack<TreeNode> path1Stack = getPathAsStack(root, node1);
+        Stack<TreeNode> path2Stack = getPathAsStack(root, node2);
+        return commonNode(path1Stack, path2Stack);
+    }
+
+    private TreeNode commonNode(Stack<TreeNode> path1Stack, Stack<TreeNode> path2Stack) {
+        while (!path1Stack.isEmpty() && !path2Stack.isEmpty()) {
+            TreeNode node1 = path1Stack.pop();
+            TreeNode node2 = path2Stack.pop();
+            if (node1.equals(node2)) {
+                return node1;
+            }
+        }
         return null;
+    }
+
+    Stack<TreeNode> getPathAsStack(TreeNode root, TreeNode node) {
+        Stack<TreeNode> path = new Stack<>();
+        hasPath(root, node, path);
+        return path;
+    }
+
+    private boolean hasPath(TreeNode root, TreeNode node, Stack<TreeNode> path) {
+        if (root == null) return false;
+
+        if (root.equals(node)) {
+            return true;
+        }
+
+        path.push(root);
+
+        if (hasPath(root.left, node, path) || hasPath(root.right, node, path)) {
+            return true;
+        }
+
+        path.pop();
+
+        return false;
     }
 }
