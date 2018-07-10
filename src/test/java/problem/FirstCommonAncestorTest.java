4f9c01cb260c315eec03cbd747f0ce1334f70033
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-09 20:10:12 -0700

    First Common Ancestor. Tests.

---
+++ src/test/java/problem/FirstCommonAncestorTest.java
@@ -0,0 +1,35 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstCommonAncestorTest {
+
+    @Test
+    public void firstCommonAncestor() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n10 = new TreeNode(10);
+        TreeNode n11 = new TreeNode(11);
+        TreeNode n12 = new TreeNode(12);
+        TreeNode n15 = new TreeNode(15);
+
+        n4.left = n2;
+        n4.right = n10;
+
+        n2.left = n1;
+        n2.right = n3;
+
+        n10.left = n9;
+        n10.right = n15;
+
+        n15.right = n11;
+        n9.left = n12;
+
+        assertEquals(n10, new FirstCommonAncestor().firstCommonAncestor(n11, n12));
+    }
+}
