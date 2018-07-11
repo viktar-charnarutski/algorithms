f3a1bc3fbf9f9dd7385f06ca98198a594eae9df2
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-10 22:18:06 -0700

    Minimum Difference of Two Arrays. Solution.

--- src/main/java/problem/ArraysMinimumDifference.java
+++ src/main/java/problem/ArraysMinimumDifference.java
@@ -1,8 +1,10 @@
 package problem;
 
+import java.util.Arrays;
+
 /**
  * Minimum Difference of Two Arrays.
- * <p>
+ *
  * Given two arrays of integers(A and B), now we are going to find an element in array A which is A[i],
  * and another element in array B which is B[j], so that the difference between A[i] and B[j] (|A[i] - B[j]|)
  * is as small as possible, return their smallest difference.
@@ -11,6 +13,30 @@ package problem;
  */
 public class ArraysMinimumDifference {
     public int findMinDiff(int[] arrA, int[] arrB) {
-        return 0;
+        Arrays.sort(arrB);
+        int min = Integer.MAX_VALUE;
+        for (int numA : arrA) {
+            int minDiff = searchForMinDiff(numA, arrB);
+            min = Math.min(min, Math.abs(numA - minDiff));
+        }
+        return min;
+    }
+
+    private int searchForMinDiff(int num, int[] arr) {
+        int start = 0, end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int midElement = arr[mid];
+            if (midElement == num) {
+                start = mid;
+                break;
+            } else if (midElement < num) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return arr[start];
     }
 }
