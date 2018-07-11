a675d55f5c798c683449fd95e8807618cabd3eae
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-11 07:45:08 -0700

    Check Subtree. Initial commit.

---
+++ src/main/java/problem/CheckSubtree.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Check Subtree
+ * <p>
+ * Tl and T2 are two very large binary trees, with Tl much bigger than T2. Create an
+ * algorithm to determine if T2 is a subtree of Tl.
+ * <p>
+ * A tree T2 is a subtree of Tl if there exists a node n in Tl such that the subtree of n is identical to T2.
+ * That is, if you cut off the tree at node n, the two trees would be identical.
+ */
+public class CheckSubtree {
+    public boolean checkSubtree(TreeNode root, TreeNode node) {
+        return false;
+    }
+}
