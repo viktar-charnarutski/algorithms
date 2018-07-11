54e0beb4b24982de8a142f6ce611a8e138f5b622
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-10 17:11:31 -0700

    144. Binary Tree Preorder Traversal. Initial commit.

---
+++ src/main/java/problem/BinaryTreePreorderTraversal.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 144. Binary Tree Preorder Traversal
+ * <p>
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ */
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return Collections.emptyList();
+    }
+}
