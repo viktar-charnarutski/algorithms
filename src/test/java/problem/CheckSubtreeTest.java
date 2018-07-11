27fad2212fc3e2a6f6cbe142cba663f0594bac3c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-11 07:49:02 -0700

    Check Subtree. Tests.

---
+++ src/test/java/problem/CheckSubtreeTest.java
@@ -0,0 +1,32 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CheckSubtreeTest {
+
+    @Test
+    public void checkSubtree() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n9 = new TreeNode(9);
+
+        n1.left = n2;
+        n1.right = n3;
+        n2.right = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n4.left = n8;
+        n4.right = n9;
+
+        assertTrue(new CheckSubtree().checkSubtree(n1, n4));
+    }
+}
