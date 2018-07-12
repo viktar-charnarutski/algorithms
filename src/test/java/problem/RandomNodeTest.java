aad33eec4b3547a26e8480ea0e0eb36085c26777
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-11 22:19:35 -0700

    Random Node. Tests.

--- src/test/java/problem/RandomNodeTest.java
+++ src/test/java/problem/RandomNodeTest.java
@@ -98,4 +98,38 @@ public class RandomNodeTest {
 
         assertEquals(expected, n1.getAllNodesAsList());
     }
+    @Test
+    public void deleteNode() {
+        RandomNode<Integer> n1 = new RandomNode<>(1);
+        RandomNode<Integer> n2 = new RandomNode<>(2);
+        RandomNode<Integer> n3 = new RandomNode<>(3);
+        RandomNode<Integer> n4 = new RandomNode<>(4);
+        RandomNode<Integer> n5 = new RandomNode<>(5);
+        RandomNode<Integer> n6 = new RandomNode<>(6);
+        RandomNode<Integer> n7 = new RandomNode<>(7);
+
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        n2.setLeft(n4);
+        n2.setRight(n5);
+        n3.setLeft(n6);
+        n3.setRight(n7);
+
+        RandomNode<Integer> e1 = new RandomNode<>(1);
+        RandomNode<Integer> e2 = new RandomNode<>(2);
+        RandomNode<Integer> e3 = new RandomNode<>(3);
+        RandomNode<Integer> e4 = new RandomNode<>(4);
+        RandomNode<Integer> e5 = new RandomNode<>(5);
+        RandomNode<Integer> e6 = new RandomNode<>(6);
+        RandomNode<Integer> e7 = new RandomNode<>(7);
+
+        e1.setLeft(e2);
+
+        e2.setLeft(e4);
+        e2.setRight(e5);
+
+        n1.delete(n3);
+
+        assertEquals(e1, n1);
+    }
 }
