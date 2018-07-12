60944e56e9ccca39ae06aa328e55d8dba197a62f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-11 19:30:46 -0700

    Random Node. Initial commit.

---
+++ src/main/java/problem/RandomNode.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * Random Node
+ * <p>
+ * You are implementing a binary tree class from scratch which, in addition to
+ * insert, find, and delete, has a method getRandomNode() which returns a random node
+ * from the tree. All nodes should be equally likely to be chosen.
+ */
+public class RandomNode {
+
+    public TreeNode getRandomNode(TreeNode node) {
+        return null;
+    }
+}
