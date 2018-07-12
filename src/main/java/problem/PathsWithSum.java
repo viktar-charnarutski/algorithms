be3e7ea29478c9c3f0c30dd35fd970697b50a082
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-12 07:16:31 -0700

    Paths with Sum. Initial commit.

---
+++ src/main/java/problem/PathsWithSum.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * Paths with Sum
+ * <p>
+ * You are given a binary tree in which each node contains an integer value (which
+ * might be positive or negative). Design an algorithm to count the number of paths that sum to a
+ * given value. The path does not need to start or end at the root or a leaf, but it must go downwards
+ * (traveling only from parent nodes to child nodes).
+ */
+public class PathsWithSum {
+    public int countPathsWithSum(TreeNode target) {
+        return 0;
+    }
+}
