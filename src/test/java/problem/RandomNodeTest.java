394fed2df5819d7d49e8c1c1bee069f621662b86
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-11 20:17:06 -0700

    Random Node. Tests.

--- src/test/java/problem/RandomNodeTest.java
+++ src/test/java/problem/RandomNodeTest.java
@@ -2,55 +2,100 @@ package problem;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotEquals;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class RandomNodeTest {
 
     @Test
-    public void getRandomNode() {
-        TreeNode n1 = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
-        TreeNode n6 = new TreeNode(6);
-        TreeNode n7 = new TreeNode(7);
-        TreeNode n8 = new TreeNode(8);
-        TreeNode n9 = new TreeNode(9);
+    public void find_existed() {
+        RandomNode<Integer> n1 = new RandomNode<>(1);
+        RandomNode<Integer> n2 = new RandomNode<>(2);
+        RandomNode<Integer> n3 = new RandomNode<>(3);
+        RandomNode<Integer> n4 = new RandomNode<>(4);
+        RandomNode<Integer> n5 = new RandomNode<>(5);
+        RandomNode<Integer> n6 = new RandomNode<>(6);
+        RandomNode<Integer> n7 = new RandomNode<>(7);
 
-        n1.left = n2;
-        n1.right = n3;
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        n2.setLeft(n4);
+        n2.setRight(n5);
+        n3.setLeft(n6);
+        n3.setRight(n7);
 
-        n2.left = n4;
-        n2.right = n5;
+        assertTrue(n1.find(5));
+    }
 
-        n3.left = n6;
-        n3.right = n7;
+    @Test
+    public void find_non_existed() {
+        RandomNode<Integer> n1 = new RandomNode<>(1);
+        RandomNode<Integer> n2 = new RandomNode<>(2);
+        RandomNode<Integer> n3 = new RandomNode<>(3);
+        RandomNode<Integer> n4 = new RandomNode<>(4);
+        RandomNode<Integer> n5 = new RandomNode<>(5);
+        RandomNode<Integer> n6 = new RandomNode<>(6);
+        RandomNode<Integer> n7 = new RandomNode<>(7);
 
-        n4.left = n8;
-        n4.right = n9;
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        n2.setLeft(n4);
+        n2.setRight(n5);
+        n3.setLeft(n6);
+        n3.setRight(n7);
 
-        RandomNode ins = new RandomNode();
-        TreeNode r1 = ins.getRandomNode(n1);
-        TreeNode r2 = ins.getRandomNode(n1);
+        assertFalse(n1.find(7));
+    }
+
+    @Test
+    public void getRandom() {
+        RandomNode<Integer> n1 = new RandomNode<>(1);
+        RandomNode<Integer> n2 = new RandomNode<>(2);
+        RandomNode<Integer> n3 = new RandomNode<>(3);
+        RandomNode<Integer> n4 = new RandomNode<>(4);
+        RandomNode<Integer> n5 = new RandomNode<>(5);
+        RandomNode<Integer> n6 = new RandomNode<>(6);
+        RandomNode<Integer> n7 = new RandomNode<>(7);
+
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        n2.setLeft(n4);
+        n2.setRight(n5);
+        n3.setLeft(n6);
+        n3.setRight(n7);
+
+        RandomNode<Integer> r1 = n1.getRandomNode();
+        RandomNode<Integer> r2 = n1.getRandomNode();
         assertNotEquals(r1, r2);
-        TreeNode r3 = ins.getRandomNode(n1);
-        assertNotEquals(r1, r3);
-        assertNotEquals(r2, r3);
-        TreeNode r4 = ins.getRandomNode(n1);
-        assertNotEquals(r1, r4);
-        assertNotEquals(r2, r4);
-        assertNotEquals(r3, r4);
-        TreeNode r5 = ins.getRandomNode(n1);
-        assertNotEquals(r1, r5);
-        assertNotEquals(r2, r5);
-        assertNotEquals(r3, r5);
-        assertNotEquals(r4, r5);
-        TreeNode r6 = ins.getRandomNode(n1);
-        assertNotEquals(r1, r6);
-        assertNotEquals(r2, r6);
-        assertNotEquals(r3, r6);
-        assertNotEquals(r4, r6);
-        assertNotEquals(r5, r6);
+    }
+
+    @Test
+    public void getAllNodesAsList() {
+        RandomNode<Integer> n1 = new RandomNode<>(1);
+        RandomNode<Integer> n2 = new RandomNode<>(2);
+        RandomNode<Integer> n3 = new RandomNode<>(3);
+        RandomNode<Integer> n4 = new RandomNode<>(4);
+        RandomNode<Integer> n5 = new RandomNode<>(5);
+        RandomNode<Integer> n6 = new RandomNode<>(6);
+        RandomNode<Integer> n7 = new RandomNode<>(7);
+
+        n1.setLeft(n2);
+        n1.setRight(n3);
+        n2.setLeft(n4);
+        n2.setRight(n5);
+        n3.setLeft(n6);
+        n3.setRight(n7);
+
+        ArrayList<RandomNode<Integer>> expected = new ArrayList<>();
+        expected.add(n1);
+        expected.add(n2);
+        expected.add(n3);
+        expected.add(n4);
+        expected.add(n5);
+        expected.add(n6);
+        expected.add(n7);
+
+        assertEquals(expected, n1.getAllNodesAsList());
     }
 }
