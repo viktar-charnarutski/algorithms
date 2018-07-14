c9d6acca95e17d13bcfd0e8d04fb344f666d67b0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-14 11:38:49 -0700

    Robot in a Grid. Initial commit.

---
+++ src/main/java/problem/RobotInGrid.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * Robot in a Grid
+ * <p>
+ * Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+ * <p>
+ * The robot can only move in two directions, right and down, but certain cells are "off limits" such that
+ * the robot cannot step on them.
+ * <p>
+ * Design an algorithm to find a path for the robot from the top left to the bottom right.
+ */
+public class RobotInGrid {
+    public boolean hasPath(int[][] grid) {
+        return false;
+    }
+}
