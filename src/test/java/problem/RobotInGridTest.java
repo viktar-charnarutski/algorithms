7238c84d31951c42fd4660630111bf4cf539caa0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-14 12:06:36 -0700

    Robot in a Grid. Tests.

--- src/test/java/problem/RobotInGridTest.java
+++ src/test/java/problem/RobotInGridTest.java
@@ -2,12 +2,13 @@ package problem;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RobotInGridTest {
 
     @Test
-    public void hasPath() {
+    public void hasPath_true() {
         int[][] grid = new int[4][5];
         grid[0][3] = 1;
         grid[1][4] = 1;
@@ -16,5 +17,18 @@ public class RobotInGridTest {
         grid[3][1] = 1;
 
         assertTrue(new RobotInGrid().hasPath(grid));
+    }
+
+    @Test
+    public void hasPath_false() {
+        int[][] grid = new int[4][5];
+        grid[0][3] = 1;
+        grid[1][4] = 1;
+        grid[2][1] = 1;
+        grid[2][2] = 1;
+        grid[2][3] = 1;
+        grid[3][0] = 1;
+        grid[3][1] = 1;
+        assertFalse(new RobotInGrid().hasPath(grid));
     }
 }
