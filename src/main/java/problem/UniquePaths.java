9190eda13b7f49b54e45dd733db7afdb6c35c297
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-14 19:45:49 -0700

    62. Unique Paths. Solution.

--- src/main/java/problem/UniquePaths.java
+++ src/main/java/problem/UniquePaths.java
@@ -11,6 +11,14 @@ package problem;
  */
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-        return 0;
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+        if (m == 1 && n == 1) {
+            return 1;
+        }
+
+        return uniquePaths(m, n - 1) + uniquePaths(m - 1, n);
     }
 }
