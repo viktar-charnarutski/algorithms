c3e453dc22f19e64e934e5cc128d4474472ffb4f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-14 19:37:39 -0700

    62. Unique Paths. Initial commit.

---
+++ src/main/java/problem/UniquePaths.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * 62. Unique Paths
+ * <p>
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * <p>
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * <p>
+ * How many possible unique paths are there?
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        return 0;
+    }
+}
