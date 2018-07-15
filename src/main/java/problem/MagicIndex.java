4365fad71605ba834d0318bbcdcdee4f20223295
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-14 19:19:27 -0700

    Magic Index. Initial commit.

---
+++ src/main/java/problem/MagicIndex.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * Magic Index
+ * <p>
+ * A magic index in an array A [ 0 ••• n -1] is defined to be an index such that A[ i] = i.
+ * <p>
+ * Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+ */
+public class MagicIndex {
+    public boolean containsMagicIndex(int[] arr) {
+        return false;
+    }
+}
