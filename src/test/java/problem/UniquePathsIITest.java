17e74dc522f382d92901cd473714a9c66b60960d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-15 12:08:10 -0700

    63. Unique Paths II. Tests.

--- src/test/java/problem/UniquePathsIITest.java
+++ src/test/java/problem/UniquePathsIITest.java
@@ -7,18 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class UniquePathsIITest {
 
     @Test
-    public void hasPath_true() {
+    public void hasPath1() {
         int[][] grid = new int[4][5];
         grid[0][3] = 1;
         grid[1][4] = 1;
         grid[2][1] = 1;
         grid[2][3] = 1;
         grid[3][1] = 1;
-        assertEquals(2, new UniquePathsII().uniquePathsWithObstacles(grid));
+        assertEquals(3
+                , new UniquePathsII().uniquePathsWithObstacles(grid));
     }
 
     @Test
-    public void hasPath_false() {
+    public void hasPath2() {
+        int[][] grid = new int[1][1];
+        assertEquals(1, new UniquePathsII().uniquePathsWithObstacles(grid));
+    }
+
+    @Test
+    public void hasPath3() {
         int[][] grid = new int[4][5];
         grid[0][3] = 1;
         grid[1][4] = 1;
