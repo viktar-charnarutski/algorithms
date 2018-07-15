d3d31ad3ab850834d67b78cbaaa9c27e1de10364
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-14 19:14:11 -0700

    Robot in a Grid. Optimized with memoization for failed paths.

--- src/main/java/problem/RobotInGrid.java
+++ src/main/java/problem/RobotInGrid.java
@@ -1,5 +1,8 @@
 package problem;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Robot in a Grid
  * <p>
@@ -12,16 +15,55 @@ package problem;
  */
 public class RobotInGrid {
     public boolean hasPath(int[][] grid) {
-        return check(grid, 0, 1) || check(grid, 1, 0);
+        Set<Point> blockedPoints = new HashSet<>();
+        return check(grid, 0, 1, blockedPoints) || check(grid, 1, 0, blockedPoints);
     }
 
-    private boolean check(int[][] grid, int row, int col) {
-        if (col == grid[0].length || row == grid.length || grid[row][col] == 1) {
+    private boolean check(int[][] grid, int row, int col, Set<Point> blockedPoints) {
+        Point point = new Point(row, col);
+        if (blockedPoints.contains(point) || col == grid[0].length || row == grid.length || grid[row][col] == 1) {
+            blockedPoints.add(point);
             return false;
         }
         if (col == grid[0].length - 1 && row == grid.length - 1 && grid[row][col] == 0) {
             return true;
         }
-        return check(grid, row + 1, col) || check(grid, row, col + 1);
+        return check(grid, row + 1, col, blockedPoints) || check(grid, row, col + 1, blockedPoints);
+    }
+
+    static class Point {
+        private final int rowNum;
+        private final int colNum;
+
+        public Point(int rowNum, int colNum) {
+            this.rowNum = rowNum;
+            this.colNum = colNum;
+        }
+
+        public int getRowNum() {
+            return rowNum;
+        }
+
+        public int getColNum() {
+            return colNum;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point)) return false;
+
+            Point point = (Point) o;
+
+            if (rowNum != point.rowNum) return false;
+            return colNum == point.colNum;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = rowNum;
+            result = 31 * result + colNum;
+            return result;
+        }
     }
 }
