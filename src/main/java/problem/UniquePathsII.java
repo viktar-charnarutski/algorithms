657da2b219f9dd3d12f0ab73d3da83746f5e41d5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-15 11:33:53 -0700

    63. Unique Paths II. Initial commit.

---
+++ src/main/java/problem/UniquePathsII.java
@@ -0,0 +1,20 @@
+package problem;
+
+/**
+ * 63. Unique Paths II
+ * <p>
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * <p>
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * <p>
+ * Now consider if some obstacles are added to the grids. How many unique paths would there be?
+ * <p>
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ * <p>
+ * Note: m and n will be at most 100.
+ */
+public class UniquePathsII {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return 0;
+    }
+}
