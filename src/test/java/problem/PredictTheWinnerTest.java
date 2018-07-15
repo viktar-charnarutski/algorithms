5f2076f0bac6469dc9094ccd95a80cab8805bc29
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-15 12:37:58 -0700

    486. Predict the Winner. Tests.

---
+++ src/test/java/problem/PredictTheWinnerTest.java
@@ -0,0 +1,19 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PredictTheWinnerTest {
+
+    @Test
+    public void predictTheWinner1() {
+        assertFalse(new PredictTheWinner().predictTheWinner(new int[]{1, 5, 2}));
+    }
+
+    @Test
+    public void predictTheWinner2() {
+        assertTrue(new PredictTheWinner().predictTheWinner(new int[]{1, 5, 233, 2}));
+    }
+}
