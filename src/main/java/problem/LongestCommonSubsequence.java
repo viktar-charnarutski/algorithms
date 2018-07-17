d5187dbd6d1e1f2953b1ebbac73785048c54eacb
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-17 08:24:40 -0700

    Longest Common Subsequence. Initial commit.

---
+++ src/main/java/problem/LongestCommonSubsequence.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * Longest Common Subsequence
+ * <p>
+ * Given two sequences, find the length of longest subsequence present in both of them.
+ * <p>
+ * A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
+ * For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”.
+ * <p>
+ * So a string of length n has 2^n different possible subsequences.
+ */
+public class LongestCommonSubsequence {
+    public String longestCommonSubsequence(String strA, String strB) {
+        return "";
+    }
+}
