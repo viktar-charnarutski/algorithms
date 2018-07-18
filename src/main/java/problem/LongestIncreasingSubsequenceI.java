8b48e2a2fbb6a6905edfb751b4f3ab9b16217998
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-18 07:28:23 -0700

    Longest Increasing Subsequence. Initial commit.

---
+++ src/main/java/problem/LongestIncreasingSubsequenceI.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * Longest Increasing Subsequence
+ * <p>
+ * The Longest Increasing Subsequence (LIS) problem is to find the length of the longest subsequence of
+ * a given sequence such that all elements of the subsequence are sorted in increasing order.
+ */
+public class LongestIncreasingSubsequenceI {
+    public int longestIncreasingSubsequence(int[] nums) {
+        return 0;
+    }
+}
