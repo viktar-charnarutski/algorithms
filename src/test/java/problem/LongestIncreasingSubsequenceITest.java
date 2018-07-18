6dc1bf5ae78f825f307e7e61997d7ebd10312b48
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-18 07:31:00 -0700

    Longest Increasing Subsequence. Tests.

--- src/test/java/problem/LongestIncreasingSubsequenceITest.java
+++ src/test/java/problem/LongestIncreasingSubsequenceITest.java
@@ -7,7 +7,22 @@ import static org.junit.Assert.assertEquals;
 public class LongestIncreasingSubsequenceITest {
 
     @Test
-    public void longestIncreasingSubsequence() {
+    public void longestIncreasingSubsequence1() {
         assertEquals(6, new LongestIncreasingSubsequenceI().longestIncreasingSubsequence(new int[]{10, 22, 9, 33, 21, 50, 41, 60, 80}));
+    }
+
+    @Test
+    public void longestIncreasingSubsequence2() {
+        assertEquals(0, new LongestIncreasingSubsequenceI().longestIncreasingSubsequence(new int[]{100, 22, 9, 8, 1}));
+    }
+
+    @Test
+    public void longestIncreasingSubsequence3() {
+        assertEquals(0, new LongestIncreasingSubsequenceI().longestIncreasingSubsequence(new int[]{1, 1, 1, 1, 1, 1}));
+    }
+
+    @Test
+    public void longestIncreasingSubsequence3() {
+        assertEquals(0, new LongestIncreasingSubsequenceI().longestIncreasingSubsequence(new int[]{1, 2, 3, 4, 5, 6}));
     }
 }
