acf4f05afbe3f1f021caf13af9730a634354b99d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-18 17:35:06 -0700

    Edit Distance. Solution.

--- src/main/java/problem/EditDistance.java
+++ src/main/java/problem/EditDistance.java
@@ -13,6 +13,22 @@ package problem;
  */
 public class EditDistance {
     public int editDistance(String str1, String str2) {
-        return 0;
+        return count(str1, str2, 0, 0);
+    }
+
+    private int count(String str1, String str2, int i1, int i2) {
+        if (i1 == str1.length()) {
+            return str2.length() - i2;
+        }
+        if (i2 == str2.length()) {
+            return str1.length() - i1;
+        }
+
+        if (str1.charAt(i1) == str2.charAt(i2)) {
+            return count(str1, str2, i1 + 1, i2 + 1);
+        }
+
+        return Math.min(count(str1, str2, i1 + 1, i2 + 1) + 1,
+                Math.min(count(str1, str2, i1 + 1, i2) + 1, count(str1, str2, i1, i2 + 1) + 1));
     }
 }
