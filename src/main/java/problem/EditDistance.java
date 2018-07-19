428b78ceeb3e575b6e4b9839683ee7656e91fb52
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-18 17:09:17 -0700

    Edit Distance. Initial commit.

---
+++ src/main/java/problem/EditDistance.java
@@ -0,0 +1,18 @@
+package problem;
+
+/**
+ * Edit Distance
+ * <p>
+ * Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1’ into ‘str2’.
+ * <p>
+ * - Insert
+ * - Remove
+ * - Replace
+ * <p>
+ * All of the above operations are of equal cost.
+ */
+public class EditDistance {
+    public int editDistance(String str1, String str2) {
+        return 0;
+    }
+}
