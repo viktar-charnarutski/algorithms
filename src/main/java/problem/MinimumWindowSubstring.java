7d8f6731de753afd111603f1c50a3b8b346d9456
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-18 22:02:45 -0700

    76. Minimum Window Substring. Initial commit.

---
+++ src/main/java/problem/MinimumWindowSubstring.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * 76. Minimum Window Substring
+ * <p>
+ * Given a string S and a string T, find the minimum window in S which will contain all the characters in T.
+ */
+public class MinimumWindowSubstring {
+    public String minWindow(String s, String t) {
+        return null;
+    }
+}
