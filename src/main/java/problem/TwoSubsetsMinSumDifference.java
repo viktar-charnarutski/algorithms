2709457f7c0c23359ba3e97cb118f496f4478922
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-19 21:13:05 -0700

    Two Subsets Minimum Sum Difference. Initial commit.

---
+++ src/main/java/problem/TwoSubsetsMinSumDifference.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Two Subsets Minimum Sum Difference
+ * <p>
+ * Given a set of integers, the task is to divide it into two sets S1 and S2
+ * such that the absolute difference between their sums is minimum.
+ * <p>
+ * If there is a set S with n elements, then if we assume Subset1 has m elements,
+ * Subset2 must have n-m elements and the value of abs(sum(Subset1) – sum(Subset2)) should be minimum.
+ */
+public class TwoSubsetsMinSumDifference {
+    public int twoSubsetsMinSumDifference(int[] nums) {
+        return 0;
+    }
+}
