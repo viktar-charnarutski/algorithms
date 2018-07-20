a00d545bcb5c934d4a22f4b408ae9e06fea84ba8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-20 07:46:39 -0700

    Ways to Cover a Distance. Solution.

--- src/main/java/problem/WaysToCoverDistance.java
+++ src/main/java/problem/WaysToCoverDistance.java
@@ -2,11 +2,15 @@ package problem;
 
 /**
  * Ways to Cover a Distance
- *
+ * <p>
  * Given a distance ‘dist, count total number of ways to cover the distance with 1, 2 and 3 steps.
  */
 public class WaysToCoverDistance {
     public int countWaysToCoverDistance(int n) {
-        return 0;
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        return countWaysToCoverDistance(n - 1) +
+                countWaysToCoverDistance(n - 2) +
+                countWaysToCoverDistance(n - 3);
     }
 }
