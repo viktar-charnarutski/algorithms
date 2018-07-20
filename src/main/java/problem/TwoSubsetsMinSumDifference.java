c0a7cd15bc368b88337655c6da31aa9370d56d50
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-19 21:13:49 -0700

    Two Subsets Minimum Sum Difference. Solution.

--- src/main/java/problem/TwoSubsetsMinSumDifference.java
+++ src/main/java/problem/TwoSubsetsMinSumDifference.java
@@ -1,5 +1,7 @@
 package problem;
 
+import java.util.HashSet;
+
 /**
  * Two Subsets Minimum Sum Difference
  * <p>
@@ -11,6 +13,30 @@ package problem;
  */
 public class TwoSubsetsMinSumDifference {
     public int twoSubsetsMinSumDifference(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) return 0;
+
+        int totalSum = 0;
+        for (int n : nums) {
+            totalSum += n;
+        }
+        HashSet<Integer> sums = new HashSet<>();
+        getAllSums(nums, 0, 0, sums);
+
+        int minSum = Integer.MAX_VALUE;
+        for (int sum1 : sums) {
+            int sum2 = totalSum - sum1;
+            minSum = Math.min(Math.abs(sum1 - sum2), minSum);
+        }
+
+        return minSum;
+    }
+
+    private void getAllSums(int[] nums, int currSum, int i, HashSet<Integer> sums) {
+        if (i == nums.length) {
+            sums.add(currSum);
+            return;
+        }
+        getAllSums(nums, currSum + nums[i], i + 1, sums);
+        getAllSums(nums, currSum, i + 1, sums);
     }
 }
