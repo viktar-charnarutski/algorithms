83d433fc1f626dd329d8fe67b4e48a308f4530a8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-20 07:46:25 -0700

    Ways to Cover a Distance. Tests.

--- src/test/java/problem/WaysToCoverDistanceTest.java
+++ src/test/java/problem/WaysToCoverDistanceTest.java
@@ -9,6 +9,6 @@ public class WaysToCoverDistanceTest {
     @Test
     public void countWaysToCoverDistance() {
         assertEquals(4, new WaysToCoverDistance().countWaysToCoverDistance(3));
-        assertEquals(4, new WaysToCoverDistance().countWaysToCoverDistance(7));
+        assertEquals(7, new WaysToCoverDistance().countWaysToCoverDistance(4));
     }
 }
