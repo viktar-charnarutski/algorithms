153ec0bee1a2d006a9002c6a9e7e45339b90cf10
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-20 19:29:55 -0700

    3. Longest Substring Without Repeating Characters. Tests.

--- src/test/java/problem/LongestSubstringWithoutRepeatingCharactersTest.java
+++ src/test/java/problem/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -7,9 +7,24 @@ import static org.junit.Assert.assertEquals;
 public class LongestSubstringWithoutRepeatingCharactersTest {
 
     @Test
-    public void lengthOfLongestSubstring() {
+    public void lengthOfLongestSubstring1() {
         assertEquals(3, new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("abcabcbb"));
         assertEquals(1, new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("bbbbb"));
         assertEquals(3, new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("pwwkew"));
+    }
+
+    @Test
+    public void lengthOfLongestSubstring_one_char() {
+        assertEquals(1, new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("a"));
+    }
+
+    @Test
+    public void lengthOfLongestSubstring_null() {
+        assertEquals(0, new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(null));
+    }
+
+    @Test
+    public void lengthOfLongestSubstring_empty() {
+        assertEquals(0, new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(""));
     }
 }
