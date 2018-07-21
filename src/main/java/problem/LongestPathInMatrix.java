4293d4bb521d1fc13a8b40a1ce582fbca8d6a910
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-21 11:19:24 -0700

    Longest Path In Matrix. Initial commit.

---
+++ src/main/java/problem/LongestPathInMatrix.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Find the longest path in a matrix with given constraints
+ * <p>
+ * Given a n*n matrix where all numbers are distinct, find the maximum length path (starting from any cell)
+ * such that all cells along the path are in increasing order with a difference of 1.
+ * <p>
+ * We can move in 4 directions from a given cell (i, j), i.e., we can move to (i+1, j) or (i, j+1) or (i-1, j)
+ * or (i, j-1) with the condition that the adjacent cells have a difference of 1.
+ */
+public class LongestPathInMatrix {
+    public int longestPathInMatrix(int[][] grid) {
+        return 0;
+    }
+}
