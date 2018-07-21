37da3397af33597187e8e4858828086a69784529
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-20 19:14:58 -0700

    3. Longest Substring Without Repeating Characters. Initial commit.

---
+++ src/main/java/problem/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+package problem;
+
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * <p>
+ * Given a string, find the length of the longest substring without repeating characters.
+ * <p>
+ * Examples:
+ * <p>
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * <p>
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * <p>
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke"
+ * is a subsequence and not a substring.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
