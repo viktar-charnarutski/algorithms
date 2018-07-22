b197dc2a777f8de796ae83ec6079b7d041040828
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-22 12:24:44 -0700

    Subset Sum. Initial commit.

---
+++ src/main/java/problem/SubsetSum.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * Subset Sum
+ * <p>
+ * Given a set of non-negative integers, and a value sum,
+ * determine if there is a subset of the given set with sum equal to given sum.
+ */
+public class SubsetSum {
+
+    public boolean subsetSum(int[] nums, int sum) {
+        return false;
+    }
+}
