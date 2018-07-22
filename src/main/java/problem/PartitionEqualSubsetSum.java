b5d395f60f0b9ac6496bb1e795f0062b64d2e21b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-22 15:55:29 -0700

    416. Partition Equal Subset Sum. Solution.

--- src/main/java/problem/PartitionEqualSubsetSum.java
+++ src/main/java/problem/PartitionEqualSubsetSum.java
@@ -3,7 +3,8 @@ package problem;
 /**
  * 416. Partition Equal Subset Sum
  * <p>
- * Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+ * Given a non-empty array containing only positive integers, find if the array can be partitioned into two
+ * subsets such that the sum of elements in both subsets is equal.
  * <p>
  * Note:
  * Each of the array element will not exceed 100.
@@ -11,6 +12,17 @@ package problem;
  */
 public class PartitionEqualSubsetSum {
     public boolean canPartition(int[] nums) {
-        return false;
+        if (nums == null || nums.length < 2) return false;
+        int totalSum = 0, currentSum = 0;
+        for (int n : nums) totalSum += n;
+        return canPartition(nums, 0, currentSum, totalSum);
+    }
+
+    private boolean canPartition(int[] nums, int index, int currentSum, int remainedSum) {
+        if (currentSum == remainedSum) return true;
+        if (remainedSum < currentSum || index == nums.length) return false;
+
+        return canPartition(nums, index + 1, currentSum + nums[index], remainedSum - nums[index])
+                || canPartition(nums, index + 1, currentSum, remainedSum);
     }
 }
