7105c29df15b9af6dba9305b0c815a77cbd92beb
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-22 12:25:59 -0700

    Subset Sum. Tests.

---
+++ src/test/java/problem/SubsetSumTest.java
@@ -0,0 +1,19 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SubsetSumTest {
+
+    @Test
+    public void subsetSum_true() {
+        assertTrue(new SubsetSum().subsetSum(new int[]{3, 34, 4, 12, 5, 2}, 9));
+    }
+
+    @Test
+    public void subsetSum_false() {
+        assertFalse(new SubsetSum().subsetSum(new int[]{3, 34, 4, 12, 5, 2}, 10));
+    }
+}
