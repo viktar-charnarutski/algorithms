58a310a260dd2549f368eac07740970e3f688328
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-23 08:02:52 -0700

    416. Partition Equal Subset Sum. Tests.

--- src/test/java/problem/OptimalStrategyTest.java
+++ src/test/java/problem/OptimalStrategyTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class OptimalStrategyTest {
 
     @Test
-    public void maxPossibleAmount() {
+    public void maxPossibleAmount1() {
         assertEquals(14, new OptimalStrategy().maxPossibleAmount(new int[]{4, 3, 10, 1}));
+    }
+
+    @Test
+    public void maxPossibleAmount2() {
+        assertEquals(15, new OptimalStrategy().maxPossibleAmount(new int[]{5, 3, 7, 10}));
     }
 }
