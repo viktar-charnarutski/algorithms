6e1bca34a376fa39fdc284f8129b0e5d00adcd48
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-23 07:39:54 -0700

    Optimal Strategy for a Game. Initial commit.

---
+++ src/main/java/problem/OptimalStrategy.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Optimal Strategy for a Game
+ * <p>
+ * Consider a row of n coins of values v1 . . . vn, where n is even.
+ * We play a game against an opponent by alternating turns. In each turn, a player selects either
+ * the first or last coin from the row, removes it from the row permanently, and receives the value of the coin.
+ * <p>
+ * Determine the maximum possible amount of money we can definitely win if we move first.
+ */
+public class OptimalStrategy {
+    public int maxPossibleAmount(int[] nums) {
+        return 0;
+    }
+}
