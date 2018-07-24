2b2b7e84bdab502cbeef171b12f8c436429cc5ba
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-24 07:56:07 -0700

    213. House Robber II. Tests.

---
+++ src/test/java/problem/HouseRobberIITest.java
@@ -0,0 +1,18 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HouseRobberIITest {
+
+    @Test
+    public void rob1() {
+        assertEquals(4, new HouseRobberII().rob(new int[]{1, 2, 3, 1}));
+    }
+
+    @Test
+    public void rob2() {
+        assertEquals(3, new HouseRobberII().rob(new int[]{2, 3, 2}));
+    }
+}
