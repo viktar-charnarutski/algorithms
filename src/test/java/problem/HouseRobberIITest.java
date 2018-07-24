fda008e38031f6d31949812d95c6167233b51321
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-24 15:35:48 -0700

    213. House Robber II. Tests.

--- src/test/java/problem/HouseRobberIITest.java
+++ src/test/java/problem/HouseRobberIITest.java
@@ -15,4 +15,9 @@ public class HouseRobberIITest {
     public void rob2() {
         assertEquals(3, new HouseRobberII().rob(new int[]{2, 3, 2}));
     }
+
+    @Test
+    public void rob3_one_element() {
+        assertEquals(1, new HouseRobberII().rob(new int[]{1}));
+    }
 }
