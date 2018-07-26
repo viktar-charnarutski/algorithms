82694eda6ae46d186d3de2dedc5f3d982b4e956c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-26 16:31:44 -0700

    832. Flipping an Image. Tests.

--- src/test/java/problem/FlippingAnImageTest.java
+++ src/test/java/problem/FlippingAnImageTest.java
@@ -74,4 +74,53 @@ public class FlippingAnImageTest {
 
         assertTrue(Arrays.deepEquals(expected, new FlippingAnImage().flipAndInvertImage(input)));
     }
+
+    @Test
+    public void flipAndInvertImage_flip_horizontally() {
+        int[][] input = new int[4][4];
+        input[0][0] = 1;
+        input[0][1] = 1;
+        input[0][2] = 0;
+        input[0][3] = 0;
+
+        input[1][0] = 1;
+        input[1][1] = 0;
+        input[1][2] = 0;
+        input[1][3] = 1;
+
+        input[2][0] = 0;
+        input[2][1] = 1;
+        input[2][2] = 1;
+        input[2][3] = 1;
+
+        input[3][0] = 1;
+        input[3][1] = 0;
+        input[3][2] = 1;
+        input[3][3] = 0;
+
+        int[][] expected = new int[4][4];
+        expected[0][0] = 0;
+        expected[0][1] = 0;
+        expected[0][2] = 1;
+        expected[0][3] = 1;
+
+        expected[1][0] = 1;
+        expected[1][1] = 0;
+        expected[1][2] = 0;
+        expected[1][3] = 1;
+
+        expected[2][0] = 1;
+        expected[2][1] = 1;
+        expected[2][2] = 1;
+        expected[2][3] = 0;
+
+        expected[3][0] = 0;
+        expected[3][1] = 1;
+        expected[3][2] = 0;
+        expected[3][3] = 1;
+
+        new FlippingAnImage().flipHorizontally(input);
+
+        assertTrue(Arrays.deepEquals(expected, input));
+    }
 }
