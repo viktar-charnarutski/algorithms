fcb643d519ecbdda3ca2ac54675805a7b5ff4656
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-27 07:19:08 -0700

    139. Word Break. Initial commit.

---
+++ src/main/java/problem/WordBreak.java
@@ -0,0 +1,20 @@
+package problem;
+
+import java.util.List;
+
+/**
+ * 139. Word Break
+ * <p>
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be
+ * segmented into a space-separated sequence of one or more dictionary words.
+ * <p>
+ * Note:
+ * <p>
+ * The same word in the dictionary may be reused multiple times in the segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ */
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+}
