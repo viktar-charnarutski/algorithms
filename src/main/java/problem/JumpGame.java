f4d4cdb15c39dc04c04ffeb68da95e4610d3fab7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-26 17:24:49 -0700

    55. Jump Game. Initial commit.

---
+++ src/main/java/problem/JumpGame.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * 55. Jump Game
+ * <p>
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ * <p>
+ * Each element in the array represents your maximum jump length at that position.
+ * <p>
+ * Determine if you are able to reach the last index.
+ */
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+}
