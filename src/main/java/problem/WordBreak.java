507b0b20f177ab8230e1b030f4826f19566e6d06
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-27 07:57:30 -0700

    139. Word Break. Solution.

--- src/main/java/problem/WordBreak.java
+++ src/main/java/problem/WordBreak.java
@@ -1,6 +1,8 @@
 package problem;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 139. Word Break
@@ -10,11 +12,25 @@ import java.util.List;
  * <p>
  * Note:
  * <p>
- * The same word in the dictionary may be reused multiple times in the segmentation.
- * You may assume the dictionary does not contain duplicate words.
+ * - The same word in the dictionary may be reused multiple times in the segmentation.
+ * - You may assume the dictionary does not contain duplicate words.
  */
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || wordDict.size() == 0) return false;
+        return checkWordParts(s, new HashSet<>(wordDict));
+    }
+
+    private boolean checkWordParts(String s, Set<String> dict) {
+        for (int i = 0; i < s.length(); i++) {
+            String subStr1 = s.substring(0, i + 1);
+            String subStr2 = s.substring(i + 1, s.length());
+            if (dict.contains(subStr1)) {
+                if (subStr2.length() == 0 || dict.contains(subStr2) || checkWordParts(subStr2, dict)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
