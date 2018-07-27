24f7603e4fc6057f759a1d7d582b5baa4c5a8689
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-26 20:54:00 -0700

    55. Jump Game. Tests.

--- src/test/java/problem/JumpGameTest.java
+++ src/test/java/problem/JumpGameTest.java
@@ -8,8 +8,18 @@ import static org.junit.Assert.assertTrue;
 public class JumpGameTest {
 
     @Test
-    public void canJump_true() {
+    public void canJump_true1() {
         assertTrue(new JumpGame().canJump(new int[]{2, 3, 1, 1, 4}));
+    }
+
+    @Test
+    public void canJump_true2() {
+        assertTrue(new JumpGame().canJump(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void canJump_true3() {
+        assertTrue(new JumpGame().canJump(new int[]{2, 0}));
     }
 
     @Test
