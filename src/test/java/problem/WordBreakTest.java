5dc942ec77c6cd11709663a82667a5a4bc7d2129
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-27 07:57:16 -0700

    139. Word Break. Tests.

--- src/test/java/problem/WordBreakTest.java
+++ src/test/java/problem/WordBreakTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class WordBreakTest {
@@ -20,6 +21,22 @@ public class WordBreakTest {
 
     @Test
     public void wordBreak3() {
-        assertTrue(new WordBreak().wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+        assertFalse(new WordBreak().wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+    }
+
+    @Test
+    public void wordBreak4() {
+        assertTrue(new WordBreak().wordBreak("a", Arrays.asList("a")));
+    }
+
+    @Test
+    public void wordBreak5() {
+        assertTrue(new WordBreak().wordBreak("apple", Arrays.asList("apple", "pear", "peach")));
+    }
+
+    @Test
+    public void wordBreak6() {
+        assertFalse(new WordBreak().wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+                Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa")));
     }
 }
