30247be11ddfa75b9c0b2e65c1898ead155131b0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-26 20:54:13 -0700

    832. Flipping an Image. Solution.

--- src/main/java/problem/JumpGame.java
+++ src/main/java/problem/JumpGame.java
@@ -11,6 +11,21 @@ package problem;
  */
 public class JumpGame {
     public boolean canJump(int[] nums) {
+        if (nums == null || nums.length < 2) return true;
+        return canJump(nums, 0);
+    }
+
+    private boolean canJump(int[] nums, int i) {
+        if (i >= nums.length) {
+            return false;
+        }
+        if (i == nums.length - 1) return true;
+
+        if (nums[i] == 0) return false;
+
+        for (int j = i + nums[i]; j > i; j--) {
+            if (canJump(nums, j)) return true;
+        }
         return false;
     }
 }
