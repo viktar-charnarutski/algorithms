22414a6698f49bfd70a6cdcba9ee35d1bd42211b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-27 21:02:45 -0700

    315. Count of Smaller Numbers After Self. Solution.

--- src/main/java/problem/CountOfSmallerNumbersAfterSelf.java
+++ src/main/java/problem/CountOfSmallerNumbersAfterSelf.java
@@ -1,16 +1,29 @@
 package problem;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 315. Count of Smaller Numbers After Self
- *
+ * <p>
  * You are given an integer array nums and you have to return a new counts array.
  * The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
  */
 public class CountOfSmallerNumbersAfterSelf {
     public List<Integer> countSmaller(int[] nums) {
-        return Collections.emptyList();
+        if (nums == null || nums.length == 0)
+            return Collections.emptyList();
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) count++;
+            }
+            res.add(i, count);
+        }
+
+        return res;
     }
 }
