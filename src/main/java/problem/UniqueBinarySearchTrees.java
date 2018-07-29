af6f654c8aaee8e6fe9f3db08ebed4b95898f35c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-29 14:33:42 -0700

    96. Unique Binary Search Trees. Solution.

--- src/main/java/problem/UniqueBinarySearchTrees.java
+++ src/main/java/problem/UniqueBinarySearchTrees.java
@@ -2,11 +2,25 @@ package problem;
 
 /**
  * 96. Unique Binary Search Trees
- *
+ * <p>
  * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
  */
 public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
-        return 0;
+        if (n <= 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
     }
 }
