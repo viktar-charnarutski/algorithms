1b5eeed5be0821fe52794a95233d38121934e07c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-28 21:48:03 -0700

    96. Unique Binary Search Trees. Initial commit.

---
+++ src/main/java/problem/UniqueBinarySearchTrees.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * 96. Unique Binary Search Trees
+ *
+ * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+ */
+public class UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+        return 0;
+    }
+}
