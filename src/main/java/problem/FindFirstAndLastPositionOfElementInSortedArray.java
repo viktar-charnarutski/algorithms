6c2bd243090d99c3ccc5a1fb2641b3f0f38a842f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-30 08:09:05 -0700

    34. Find First and Last Position of Element in Sorted Array. Initial commit.

---
+++ src/main/java/problem/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 34. Find First and Last Position of Element in Sorted Array
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target
+ * value.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * If the target is not found in the array, return [-1, -1].
+ */
+public class FindFirstAndLastPositionOfElementInSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[0];
+    }
+}
