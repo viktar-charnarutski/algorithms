aab9b375c147e7729e63e536545b07a0aada4b6e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-30 07:17:00 -0700

    96. Unique Binary Search Trees. Refactoring.

--- src/main/java/problem/UniqueBinarySearchTrees.java
+++ src/main/java/problem/UniqueBinarySearchTrees.java
@@ -13,9 +13,8 @@ public class UniqueBinarySearchTrees {
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 dp[i] += dp[j - 1] * dp[i - j];
             }
