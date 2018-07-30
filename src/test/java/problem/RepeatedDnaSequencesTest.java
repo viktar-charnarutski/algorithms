d4551d476bff13842488980c79693651f2507a3f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-30 07:50:17 -0700

    187. Repeated DNA Sequences. Tests.

--- src/test/java/problem/RepeatedDnaSequencesTest.java
+++ src/test/java/problem/RepeatedDnaSequencesTest.java
@@ -9,10 +9,16 @@ import static org.junit.Assert.assertEquals;
 public class RepeatedDnaSequencesTest {
 
     @Test
-    public void findRepeatedDnaSequences() {
+    public void findRepeatedDnaSequences1() {
         ArrayList<String> expected = new ArrayList<>();
         expected.add("AAAAACCCCC");
         expected.add("CCCCCAAAAA");
         assertEquals(expected, new RepeatedDnaSequences().findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+    }
+    @Test
+    public void findRepeatedDnaSequences2() {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("AAAAAAAAAA");
+        assertEquals(expected, new RepeatedDnaSequences().findRepeatedDnaSequences("AAAAAAAAAAA"));
     }
 }
