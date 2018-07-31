1292d0b858ba40d49d687447540789da8cc1b483
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-31 07:48:58 -0700

    859. Buddy Strings. Initial commit.

---
+++ src/main/java/problem/BuddyStrings.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * 859. Buddy Strings
+ *
+ * Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the
+ * result equals B.
+ *
+ */
+public class BuddyStrings {
+    public boolean buddyStrings(String A, String B) {
+        return false;
+    }
+}
