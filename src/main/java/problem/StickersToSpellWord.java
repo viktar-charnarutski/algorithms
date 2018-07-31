58c442908c7415004b794f8a4c2aefb53c1dd487
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-31 07:18:39 -0700

    691. Stickers to Spell Word. Initial commit.

---
+++ src/main/java/problem/StickersToSpellWord.java
@@ -0,0 +1,19 @@
+package problem;
+
+/**
+ * 691. Stickers to Spell Word
+ * <p>
+ * We are given N different types of stickers. Each sticker has a lowercase English word on it.
+ * <p>
+ * You would like to spell out the given target string by cutting individual letters from your collection of stickers
+ * and rearranging them.
+ * <p>
+ * You can use each sticker more than once if you want, and you have infinite quantities of each sticker.
+ * <p>
+ * What is the minimum number of stickers that you need to spell out the target? If the task is impossible, return -1.
+ */
+public class StickersToSpellWord {
+    public int minStickers(String[] stickers, String target) {
+        return 0;
+    }
+}
