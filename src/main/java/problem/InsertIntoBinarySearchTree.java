7ad49d1a3e0372dd4ab4c3a5d7a6139835127eb3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-31 16:41:41 -0700

    701. Insert into a Binary Search Tree. initial commit.

---
+++ src/main/java/problem/InsertIntoBinarySearchTree.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 701. Insert into a Binary Search Tree
+ * <p>
+ * Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into
+ * the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in
+ * the original BST.
+ * <p>
+ * Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion.
+ * You can return any of them.
+ */
+public class InsertIntoBinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        return null;
+    }
+}
