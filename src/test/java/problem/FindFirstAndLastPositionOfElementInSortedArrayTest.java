997fd92189d9be9fe65dd64679c9d30a825e863b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-30 21:14:14 -0700

    34. Find First and Last Position of Element in Sorted Array. Tests.

--- src/test/java/problem/FindFirstAndLastPositionOfElementInSortedArrayTest.java
+++ src/test/java/problem/FindFirstAndLastPositionOfElementInSortedArrayTest.java
@@ -3,6 +3,7 @@ package problem;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class FindFirstAndLastPositionOfElementInSortedArrayTest {
 
@@ -16,5 +17,29 @@ public class FindFirstAndLastPositionOfElementInSortedArrayTest {
     public void searchRange2() {
         assertArrayEquals(new int[]{-1, -1},
                 new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6));
+    }
+
+    @Test
+    public void searchRange3() {
+        assertArrayEquals(new int[]{5, 5},
+                new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[]{5, 7, 7, 8, 8, 10}, 10));
+    }
+
+    @Test
+    public void searchRange4() {
+        assertArrayEquals(new int[]{-1, -1},
+                new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[]{0}, 5));
+    }
+
+    @Test
+    public void searchRange5() {
+        assertArrayEquals(new int[]{0, 0},
+                new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[]{5}, 5));
+    }
+
+    @Test
+    public void searchRange_find() {
+        assertEquals(2,
+                new FindFirstAndLastPositionOfElementInSortedArray().findIndex(new int[]{5, 7, 7, 8, 8, 10}, 7));
     }
 }
