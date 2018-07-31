d174cd1342745e0d4e8d784f89c8efd41ec7adf8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-31 07:53:51 -0700

    859. Buddy Strings. Tests.

---
+++ src/test/java/problem/BuddyStringsTest.java
@@ -0,0 +1,34 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BuddyStringsTest {
+
+    @Test
+    public void buddyStrings1() {
+        assertTrue(new BuddyStrings().buddyStrings("ab", "ba"));
+    }
+
+    @Test
+    public void buddyStrings2() {
+        assertTrue(new BuddyStrings().buddyStrings("aa", "aa"));
+    }
+
+    @Test
+    public void buddyStrings3() {
+        assertTrue(new BuddyStrings().buddyStrings("aaaaaaabc", "aaaaaaacb"));
+    }
+
+    @Test
+    public void buddyStrings4() {
+        assertFalse(new BuddyStrings().buddyStrings("", "aaaaaaacb"));
+    }
+
+    @Test
+    public void buddyStrings5() {
+        assertFalse(new BuddyStrings().buddyStrings("ab", "ab"));
+    }
+}
