a0668bc817798ebe3df0ba2072dcfdcfeb22ad8b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-07-31 16:17:13 -0700

    859. Buddy Strings. Solution.

--- src/main/java/problem/BuddyStrings.java
+++ src/main/java/problem/BuddyStrings.java
@@ -2,13 +2,34 @@ package problem;
 
 /**
  * 859. Buddy Strings
- *
+ * <p>
  * Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the
  * result equals B.
- *
  */
 public class BuddyStrings {
     public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+
+        // edge case handling: A == B
+        if (A.equals(B)) {
+            for (int i = 1; i < A.length(); i++) {
+                if (A.charAt(i) == A.charAt(i - 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        int diffInd = -1;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                if (diffInd == -1) {
+                    diffInd = i;
+                } else {
+                    return B.charAt(i) == A.charAt(diffInd) && A.charAt(i) == B.charAt(diffInd);
+                }
+            }
+        }
         return false;
     }
 }
