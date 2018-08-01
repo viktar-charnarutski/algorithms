e6c40e6aace8db9cb6033074a1ab323927e8f694
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-01 08:11:02 -0700

    700. Search in a Binary Search Tree. Solution.

--- src/main/java/problem/SearchInBinarySearchTree.java
+++ src/main/java/problem/SearchInBinarySearchTree.java
@@ -9,6 +9,11 @@ package problem;
  */
 public class SearchInBinarySearchTree {
     public TreeNode searchBST(TreeNode root, int val) {
-        return null;
+        if (root == null || root.val == val) return root;
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
     }
 }
