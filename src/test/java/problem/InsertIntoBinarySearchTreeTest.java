c840eaf323e687c0c2683d2616bcb64ac0794190
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-01 07:26:52 -0700

    701. Insert into a Binary Search Tree. Tests.

---
+++ src/test/java/problem/InsertIntoBinarySearchTreeTest.java
@@ -0,0 +1,35 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InsertIntoBinarySearchTreeTest {
+
+    @Test
+    public void insertIntoBST1() {
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n3 = new TreeNode(3);
+        n4.left = n2;
+        n4.right = n7;
+        n2.left = n1;
+        n2.right = n3;
+
+        TreeNode m4 = new TreeNode(4);
+        TreeNode m2 = new TreeNode(2);
+        TreeNode m7 = new TreeNode(7);
+        TreeNode m1 = new TreeNode(1);
+        TreeNode m3 = new TreeNode(3);
+        TreeNode m5 = new TreeNode(5);
+        m4.left = m2;
+        m4.right = m7;
+        m2.left = m1;
+        m2.right = m3;
+        m7.left = m5;
+
+        assertEquals(m4, new InsertIntoBinarySearchTree().insertIntoBST(n4, 5));
+    }
+}
