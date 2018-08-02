55759194d797b7e2e37f204710ee1b1a4c10782c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-01 19:40:03 -0700

    678. Valid Parenthesis String. Tests.

---
+++ src/test/java/problem/ValidParenthesisStringTest.java
@@ -0,0 +1,44 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ValidParenthesisStringTest {
+
+    @Test
+    public void checkValidString1() {
+        assertTrue(new ValidParenthesisString().checkValidString("()"));
+    }
+
+    @Test
+    public void checkValidString2() {
+        assertTrue(new ValidParenthesisString().checkValidString("(*)"));
+    }
+
+    @Test
+    public void checkValidString3() {
+        assertTrue(new ValidParenthesisString().checkValidString("(*))"));
+    }
+
+    @Test
+    public void checkValidString4() {
+        assertFalse(new ValidParenthesisString().checkValidString("(*)))"));
+    }
+
+    @Test
+    public void checkValidString5() {
+        assertTrue(new ValidParenthesisString().checkValidString("((*))"));
+    }
+
+    @Test
+    public void checkValidString6() {
+        assertFalse(new ValidParenthesisString().checkValidString("())"));
+    }
+
+    @Test
+    public void checkValidString7() {
+        assertFalse(new ValidParenthesisString().checkValidString("((())"));
+    }
+}
