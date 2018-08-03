ce7d79082c610c7c811d051f947cbd34a4e84e16
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-02 17:20:24 -0700

    4. Median of Two Sorted Arrays. Tests.

--- src/test/java/problem/MedianOfTwoSortedArraysTest.java
+++ src/test/java/problem/MedianOfTwoSortedArraysTest.java
@@ -2,17 +2,25 @@ package problem;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertSame;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 public class MedianOfTwoSortedArraysTest {
 
     @Test
     public void findMedianSortedArrays1() {
-        assertSame(2.0, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        assertEquals(2.0, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), 0.0);
     }
 
     @Test
     public void findMedianSortedArrays2() {
-        assertSame(2.5, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        assertEquals(2.5, new MedianOfTwoSortedArrays().findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), 0.0);
+    }
+
+    @Test
+    public void mergeArrays() {
+        assertEquals(Arrays.asList(-3, 1, 2, 3, 4, 5, 6, 7, 8),
+                new MedianOfTwoSortedArrays().mergeArrays(new int[]{1, 2, 3, 4}, new int[]{-3, 3, 4, 5, 6, 7, 8}));
     }
 }
