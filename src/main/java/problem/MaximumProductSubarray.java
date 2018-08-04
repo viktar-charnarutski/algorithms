933379da7c199c0d6bc403a0c75de42ff4ea5b87
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-03 19:43:06 -0700

    152. Maximum Product Subarray. Initial commit.

---
+++ src/main/java/problem/MaximumProductSubarray.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * 152. Maximum Product Subarray
+ *
+ * Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has
+ * the largest product.
+ */
+public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+}
