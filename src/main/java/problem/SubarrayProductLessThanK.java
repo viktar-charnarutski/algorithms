216211e77136a04edc1d4f97075b71444df9d0ce
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-06 16:23:16 -0700

    713. Subarray Product Less Than K. Initial commit.

---
+++ src/main/java/problem/SubarrayProductLessThanK.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * 713. Subarray Product Less Than K
+ * <p>
+ * Your are given an array of positive integers nums.
+ * <p>
+ * Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less
+ * than k.
+ */
+public class SubarrayProductLessThanK {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        return 0;
+    }
+}
