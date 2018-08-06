699a023f06c745fec180b3c3dc869118877b4172
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-06 08:22:08 -0700

    819. Most Common Word. Tests.

--- src/test/java/problem/MostCommonWordTest.java
+++ src/test/java/problem/MostCommonWordTest.java
@@ -14,7 +14,7 @@ public class MostCommonWordTest {
 
     @Test
     public void mostCommonWord2() {
-        assertEquals("ball", new MostCommonWord().mostCommonWord(
+        assertEquals("bob", new MostCommonWord().mostCommonWord(
                 "Bob hit a ball, the hit BALL flew far after it was hit. And then Bob" +
                         " decided to get home. It was too late, and Bob didn't want to be late.", new String[]{"hit"}));
     }
