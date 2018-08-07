26effb4a48d6765b0ae5640137a47c3296351bcc
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-07 08:01:36 -0700

    713. Subarray Product Less Than K. Tests.

--- src/test/java/problem/SubarrayProductLessThanKTest.java
+++ src/test/java/problem/SubarrayProductLessThanKTest.java
@@ -7,8 +7,20 @@ import static org.junit.Assert.assertEquals;
 public class SubarrayProductLessThanKTest {
 
     @Test
-    public void numSubarrayProductLessThanK() {
+    public void numSubarrayProductLessThanK1() {
         assertEquals(8,
                 new SubarrayProductLessThanK().numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100));
+    }
+
+    @Test
+    public void numSubarrayProductLessThanK2() {
+        assertEquals(0,
+                new SubarrayProductLessThanK().numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 1));
+    }
+
+    @Test
+    public void numSubarrayProductLessThanK3() {
+        assertEquals(0,
+                new SubarrayProductLessThanK().numSubarrayProductLessThanK(new int[]{1,2,3}, 0));
     }
 }
