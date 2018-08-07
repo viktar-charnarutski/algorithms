ba33b526d2023b7951bdea1b4f9460cebae1c466
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-07 08:05:19 -0700

    713. Subarray Product Less Than K. Solution.

--- src/main/java/problem/SubarrayProductLessThanK.java
+++ src/main/java/problem/SubarrayProductLessThanK.java
@@ -10,6 +10,15 @@ package problem;
  */
 public class SubarrayProductLessThanK {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        return 0;
+        if (nums == null || nums.length == 0 || k <= 1) return 0;
+        int res = 0, left = 0,  curr = 1;
+        for (int right = 0; right < nums.length; right++) {
+            curr *= nums[right];
+            while (curr >= k) {
+                curr /= nums[left++];
+            }
+            res += right - left + 1;
+        }
+        return res;
     }
 }
