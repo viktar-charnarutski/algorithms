1160088377761d32d38ad6c245e8cfe3fba1b9bc
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-07 20:18:25 -0700

    216. Combination Sum III. Tests.

--- src/test/java/problem/CombinationSumIIITest.java
+++ src/test/java/problem/CombinationSumIIITest.java
@@ -19,4 +19,25 @@ public class CombinationSumIIITest {
         input.add(res1);
         assertEquals(input, new CombinationSumIII().combinationSum3(3, 7));
     }
+
+    @Test
+    public void combinationSum3_2() {
+        List<List<Integer>> input = new ArrayList<>();
+        List<Integer> res1 = new ArrayList<>();
+        res1.add(1);
+        res1.add(2);
+        res1.add(6);
+        input.add(res1);
+        List<Integer> res2 = new ArrayList<>();
+        res2.add(1);
+        res2.add(3);
+        res2.add(5);
+        input.add(res2);
+        List<Integer> res3 = new ArrayList<>();
+        res3.add(2);
+        res3.add(3);
+        res3.add(4);
+        input.add(res3);
+        assertEquals(input, new CombinationSumIII().combinationSum3(3, 9));
+    }
 }
