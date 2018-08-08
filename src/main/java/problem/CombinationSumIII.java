ae8405b2d9230e8a0c8ccdbc224ef7dbc0303b3a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-08 07:37:29 -0700

    216. Combination Sum III. Solution.

--- src/main/java/problem/CombinationSumIII.java
+++ src/main/java/problem/CombinationSumIII.java
@@ -1,13 +1,40 @@
 package problem;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 216. Combination Sum III
  */
 public class CombinationSumIII {
+    private List<List<Integer>> res = new ArrayList<>();
+    private int k, n;
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-        return Collections.emptyList();
+        this.k = k;
+        this.n = n;
+        combinationSum3(1, n, new ArrayList<>());
+        return res;
+    }
+
+    private void combinationSum3(int currNum, int remained, List<Integer> currSet) {
+        if (currNum >= n) {
+            return;
+        }
+        if (currSet.size() == k) {
+            if (remained == 0) {
+                res.add(new ArrayList<>(currSet));
+            }
+            return;
+        }
+        if (currNum > remained) {
+            return;
+        }
+        List<Integer> currSetNoCCurrItem = new ArrayList<>(currSet);
+
+        currSet.add(currNum);
+        combinationSum3(currNum + 1, remained - currNum, currSet);
+
+        combinationSum3(currNum + 1, remained, currSetNoCCurrItem);
     }
 }
