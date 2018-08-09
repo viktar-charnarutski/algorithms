f99f444003fe876e5ce6aa401bb8fed3f7f5f676
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-08 17:02:27 -0700

    98. Validate Binary Search Tree. Tests.

--- src/test/java/problem/ValidateBinarySearchTreeTest.java
+++ src/test/java/problem/ValidateBinarySearchTreeTest.java
@@ -38,4 +38,18 @@ public class ValidateBinarySearchTreeTest {
         n1.left = n1_1;
         assertFalse(new ValidateBinarySearchTree().isValidBST(n1));
     }
+
+    @Test
+    public void isValidBST4() {
+        TreeNode n10 = new TreeNode(10);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n15 = new TreeNode(15);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n20 = new TreeNode(20);
+        n10.left = n5;
+        n10.right = n15;
+        n15.left = n6;
+        n15.right = n20;
+        assertFalse(new ValidateBinarySearchTree().isValidBST(n10));
+    }
 }
