f75603b060682cba75b8f5711584e709aa4bdc5d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-09 08:01:43 -0700

    113. Path Sum II. Initial commit.

---
+++ src/main/java/problem/PathSumII.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 113. Path Sum II
+ * <p>
+ * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+ * <p>
+ * Note: A leaf is a node with no children.
+ */
+public class PathSumII {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        return Collections.emptyList();
+    }
+}
