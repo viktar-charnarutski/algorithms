22c8c35b6a596659b4eb80d0ef8617dde8232013
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-10 07:12:42 -0700

    709. To Lower Case. Initial commit.

---
+++ src/main/java/problem/ToLowerCase.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * 709. To Lower Case
+ * <p>
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ */
+public class ToLowerCase {
+    public String toLowerCase(String str) {
+        return null;
+    }
+}
