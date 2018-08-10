2833cd786b46bc711325d1d4bdd27c175809cdf3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-10 07:45:46 -0700

    705. Design HashSet. Initial commit.

---
+++ src/main/java/problem/DesignHashSet.java
@@ -0,0 +1,36 @@
+package problem;
+
+/**
+ * 705. Design HashSet
+ * <p>
+ * Design a HashSet without using any built-in hash table libraries.
+ * <p>
+ * To be specific, your design should include these functions:
+ * <p>
+ * add(value): Insert a value into the HashSet.
+ * contains(value) : Return whether the value exists in the HashSet or not.
+ * remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
+ */
+public class DesignHashSet {
+    /**
+     * Initialize your data structure here.
+     */
+    public DesignHashSet() {
+
+    }
+
+    public void add(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        return false;
+    }
+}
