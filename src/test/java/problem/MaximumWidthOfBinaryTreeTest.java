87d41638f01ead9b76209481e04322ed2fd11352
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-11 21:04:09 -0700

    662. Maximum Width of Binary Tree. Tests.

---
+++ src/test/java/problem/MaximumWidthOfBinaryTreeTest.java
@@ -0,0 +1,28 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaximumWidthOfBinaryTreeTest {
+
+    @Test
+    public void widthOfBinaryTree1() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+
+        n1.left = n2;
+        n1.right = n3;
+
+        n2.left = n4;
+        n2.right = n5;
+
+        n3.right = n6;
+
+        assertEquals(4, new MaximumWidthOfBinaryTree().widthOfBinaryTree(n1));
+    }
+}
