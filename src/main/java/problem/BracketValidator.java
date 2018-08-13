6963906b1f366cd566e80833f6355e0264000e5c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-12 22:06:17 -0700

    Bracket Validator. Initial commit.

---
+++ src/main/java/problem/BracketValidator.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * Bracket Validator
+ *
+ * You're working with an intern that keeps coming to you with JavaScript code that won't run because the braces,
+ * brackets, and parentheses are off. To save you both some time, you decide to write a braces/brackets/parentheses
+ * validator.
+ */
+public class BracketValidator {
+    public boolean isValid(String parentheses) {
+        return false;
+    }
+}
