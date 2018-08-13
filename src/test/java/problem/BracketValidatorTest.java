927222d1584751aa5b9d186df5937df669516743
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-12 22:08:09 -0700

    Bracket Validator. Tests.

---
+++ src/test/java/problem/BracketValidatorTest.java
@@ -0,0 +1,24 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BracketValidatorTest {
+
+    @Test
+    public void isValid1() {
+        assertTrue(new BracketValidator().isValid("{[]()}"));
+    }
+
+    @Test
+    public void isValid2() {
+        assertFalse(new BracketValidator().isValid("{[(])}"));
+    }
+
+    @Test
+    public void isValid3() {
+        assertTrue(new BracketValidator().isValid("{[}"));
+    }
+}
