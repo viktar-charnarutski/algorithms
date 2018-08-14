d8f573f238c62f090275c36ab639241819be0bc9
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-13 22:00:30 -0700

    721. Accounts Merge. Tests.

--- src/test/java/problem/AccountsMergeTest.java
+++ src/test/java/problem/AccountsMergeTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class AccountsMergeTest {
 
     @Test
-    public void accountsMerge() {
+    public void accountsMerge1() {
         List<List<String>> accounts = new ArrayList<>();
         List<String> account1 = new ArrayList<>();
         account1.add("John");
@@ -37,6 +37,47 @@ public class AccountsMergeTest {
         expected1.add("johnsmith@mail.com");
         expected1.add("john00@mail.com");
         expected1.add("john_newyork@mail.com");
+        expected.add(expected1);
+        List<String> expected2 = new ArrayList<>();
+        expected2.add("John");
+        expected2.add("johnnybravo@mail.com");
+        expected.add(expected2);
+        List<String> expected3 = new ArrayList<>();
+        expected3.add("Mary");
+        expected3.add("mary@mail.com");
+        expected.add(expected3);
+
+        assertEquals(expected, new AccountsMerge().accountsMerge(accounts));
+    }
+
+    @Test
+    public void accountsMerge2() {
+        List<List<String>> accounts = new ArrayList<>();
+        List<String> account1 = new ArrayList<>();
+        account1.add("John");
+        account1.add("johnsmith@mail.com");
+        account1.add("john_newyork@mail.com");
+        accounts.add(account1);
+        List<String> account2 = new ArrayList<>();
+        account2.add("John");
+        account2.add("johnnybravo@mail.com");
+        accounts.add(account2);
+        List<String> account3 = new ArrayList<>();
+        account3.add("John");
+        account3.add("johnsmith@mail.com");
+        account3.add("john00@mail.com");
+        accounts.add(account3);
+        List<String> account4 = new ArrayList<>();
+        account4.add("Mary");
+        account4.add("mary@mail.com");
+        accounts.add(account4);
+
+        List<List<String>> expected = new ArrayList<>();
+        List<String> expected1 = new ArrayList<>();
+        expected1.add("John");
+        expected1.add("john00@mail.com");
+        expected1.add("john_newyork@mail.com");
+        expected1.add("johnsmith@mail.com");
         expected.add(expected1);
         List<String> expected2 = new ArrayList<>();
         expected2.add("John");
