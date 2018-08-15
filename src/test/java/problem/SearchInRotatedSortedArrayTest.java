d61fa2949f76881f298dbe7f116e7b67b9d2a8e4
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-15 08:44:43 -0700

    33. Search in Rotated Sorted Array. Tests.

--- src/test/java/problem/SearchInRotatedSortedArrayTest.java
+++ src/test/java/problem/SearchInRotatedSortedArrayTest.java
@@ -10,4 +10,9 @@ public class SearchInRotatedSortedArrayTest {
     public void search1() {
         assertEquals(4, new SearchInRotatedSortedArray().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
     }
+
+    @Test
+    public void search2() {
+        assertEquals(-1, new SearchInRotatedSortedArray().search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+    }
 }
