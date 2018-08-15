a556587b034871a44519068890b87eaeb0a389e8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-15 09:20:10 -0700

    33. Search in Rotated Sorted Array. Solution.

--- src/main/java/problem/SearchInRotatedSortedArray.java
+++ src/main/java/problem/SearchInRotatedSortedArray.java
@@ -5,7 +5,7 @@ package problem;
  * <p>
  * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
  * <p>
- * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,    6,7,0,1,2]).
  * <p>
  * You are given a target value to search. If found in the array return its index, otherwise return -1.
  * <p>
@@ -15,6 +15,30 @@ package problem;
  */
 public class SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
-        return -1;
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[lo] <= nums[mid]) {
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return nums[lo] == target ? lo : -1;
     }
 }
