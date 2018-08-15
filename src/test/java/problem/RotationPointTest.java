baab28d8f21b57b3f8f3e52480ae7290edcd6964
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-14 18:05:18 -0700

    Rotation Point. Tests.

--- src/test/java/problem/RotationPointTest.java
+++ src/test/java/problem/RotationPointTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class RotationPointTest {
 
     @Test
-    public void findRotationPoint() {
+    public void findRotationPoint1() {
         String[] words = {"ptolemaic",
                 "retrograde",
                 "supplant",
@@ -21,5 +21,23 @@ public class RotationPointTest {
                 "othellolagkage"};
 
         assertEquals(5, new RotationPoint().findRotationPoint(words));
+    }
+
+    @Test
+    public void findRotationPoint2() {
+        String[] words = {
+                "asymptote",
+                "babka",
+                "banoffee",
+                "engender",
+                "karpatkav",
+                "othellolagkage",
+                "ptolemaic",
+                "retrograde",
+                "supplant",
+                "undulate",
+                "xenoepist"};
+
+        assertEquals(0, new RotationPoint().findRotationPoint(words));
     }
 }
