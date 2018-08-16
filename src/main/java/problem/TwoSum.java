b40904fd95db4441203991e123c9bc9951a9e4d2
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-15 23:18:06 -0700

    1. Two Sum. Initial commit.

---
+++ src/main/java/problem/TwoSum.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * 1. Two Sum
+ * <p>
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[2];
+    }
+}
