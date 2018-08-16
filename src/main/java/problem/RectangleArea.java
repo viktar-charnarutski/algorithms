548d45bae627020d279b99eef65fea8ba45d9af6
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-15 19:30:08 -0700

    223. Rectangle Area. Initial commit.

---
+++ src/main/java/problem/RectangleArea.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * 223. Rectangle Area
+ * <p>
+ * Find the total area covered by two rectilinear rectangles in a 2D plane.
+ * <p>
+ * Each rectangle is defined by its bottom left corner and top right corner.
+ */
+public class RectangleArea {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        return 0;
+    }
+}
