047d8887c78461dad800485dcfcb9ac61519010f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-17 08:49:01 -0700

    205. Isomorphic Strings. Initial commit.

---
+++ src/main/java/problem/IsomorphicStrings.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 205. Isomorphic Strings
+ * <p>
+ * Given two strings s and t, determine if they are isomorphic.
+ * <p>
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ * <p>
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character but a character may map to itself.
+ */
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        return false;
+    }
+}
