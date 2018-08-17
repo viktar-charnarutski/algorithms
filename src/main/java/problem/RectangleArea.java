d37662beaf07f3f99658d9aa09ac2c61ab3a6833
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-16 22:03:58 -0700

    223. Rectangle Area. Solution.

--- src/main/java/problem/RectangleArea.java
+++ src/main/java/problem/RectangleArea.java
@@ -9,6 +9,86 @@ package problem;
  */
 public class RectangleArea {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        return 0;
+        Rectangle rec1 = new Rectangle(new Point(A, B), new Point(C, D));
+        Rectangle rec2 = new Rectangle(new Point(E, F), new Point(G, H));
+        return rec1.totalArea(rec2);
+    }
+
+    class Rectangle {
+        private final Point bottomLeft;
+        private final Point topRight;
+
+        public Rectangle(Point bottomLeft, Point topRight) {
+            this.bottomLeft = bottomLeft;
+            this.topRight = topRight;
+        }
+
+        public int area() {
+            return bottomSide() * leftSide();
+        }
+
+        int bottomSide() {
+            return side(topRight.getX(), bottomLeft.getX());
+        }
+
+        int leftSide() {
+            return side(topRight.getY(), bottomLeft.getY());
+        }
+
+        private int side(int p1, int p2) {
+            if (p1 < 0 || p2 < 0) {
+                return Math.abs(p1) + Math.abs(p2);
+            } else {
+                return Math.abs(p1 - p2);
+            }
+        }
+
+        public int intersectionArea(Rectangle otherRec) {
+            int bottomSide;
+            if (this.bottomLeft.getX() < otherRec.bottomLeft.getX()) {
+                bottomSide = side(otherRec.bottomLeft.getX(), this.topRight.getX());
+            } else {
+                bottomSide = side(otherRec.topRight.getX(), this.bottomLeft.getX());
+            }
+
+            int leftSide;
+            if (this.bottomLeft.getY() < otherRec.bottomLeft.getY()) {
+                leftSide = side(this.topRight.getY(), otherRec.bottomLeft.getY());
+            } else {
+                leftSide = side(this.bottomLeft.getY(), otherRec.topRight.getY());
+            }
+            return bottomSide * leftSide;
+        }
+
+        public int totalArea(Rectangle otherRec) {
+            int otherRecArea = otherRec.area();
+            int currArea = area();
+            if (otherRecArea == 0) {
+                return currArea;
+            }
+            if (currArea == 0) {
+                return otherRecArea;
+            }
+            return otherRecArea + currArea - intersectionArea(otherRec);
+        }
+    }
+
+    class Point {
+        private final int x;
+        private final int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
     }
 }
+
