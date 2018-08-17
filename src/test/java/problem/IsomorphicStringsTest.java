7fbfe7827aea427bf69ef5f4fbb08bab159a6866
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-17 08:52:01 -0700

    205. Isomorphic Strings. Tests.

---
+++ src/test/java/problem/IsomorphicStringsTest.java
@@ -0,0 +1,13 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class IsomorphicStringsTest {
+
+    @Test
+    public void isIsomorphic1() {
+        assertTrue(new IsomorphicStrings().isIsomorphic("egg", "add"));
+    }
+}
