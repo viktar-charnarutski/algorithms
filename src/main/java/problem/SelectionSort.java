f0c93471c17aaa7ceed42218fecd802a7e573315
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-18 11:01:05 -0700

    Selection Sort. Solution.

---
+++ src/main/java/problem/SelectionSort.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Selection Sort
+ * <p>
+ * Selection sort offers improved performance over bubble sort, especially for arrays with a large number of elements.
+ * Where bubble sort accumulated the largest elements towards the end of the array, selection sort accumulates the
+ * smallest elements at the beginning of the array.
+ * <p>
+ * Write a method that uses the selection sort algorithm to sort an input array of integers. See the hints and click the
+ * red colored links for additonal details on the algorithm.
+ */
+public class SelectionSort {
+    public void sort(int[] arr) {
+    }
+}
