c40584d47779b1d3d269678048a8f2402f36c90a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-18 11:05:35 -0700

    Selection Sort. Tests.

--- src/test/java/problem/SelectionSortTest.java
+++ src/test/java/problem/SelectionSortTest.java
@@ -8,7 +8,7 @@ public class SelectionSortTest {
 
     @Test
     public void sort1() {
-        int[] input = new int[]{4,12,4,78,7,-1};
+        int[] input = new int[]{4, 12, 4, 78, 7, -1};
         int[] expected = new int[]{-1, 4, 4, 7, 12, 78};
         new SelectionSort().sort(input);
         assertArrayEquals(expected, input);
