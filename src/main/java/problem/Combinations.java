02c11ec06fa935d182b32aea7bdf5e7a686caea7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-19 12:16:56 -0700

    77. Combinations. Initial commit.

---
+++ src/main/java/problem/Combinations.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 77. Combinations
+ *
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ */
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        return Collections.emptyList();
+    }
+}
