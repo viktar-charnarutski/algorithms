40efbc1e9d802c2461645f8f8cf3e249282be617
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-19 11:58:52 -0700

    Selection Sort. Initial commit.

---
+++ src/main/java/problem/InsertStars.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * Insert Stars
+ * <p>
+ * Given a string, recursively compute a new string where the identical adjacent characters in the original string are
+ * separated by a "*".
+ */
+public class InsertStars {
+    public String insertPairStar(String s) {
+        return null;
+    }
+}
