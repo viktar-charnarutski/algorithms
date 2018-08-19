78b74513db2270620ce74aa863cf053746b576eb
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-19 11:30:44 -0700

    Better Fibonacci. Tests.

--- src/test/java/problem/BetterFibonacciTest.java
+++ src/test/java/problem/BetterFibonacciTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class BetterFibonacciTest {
 
     @Test
-    public void betterFibonacci1() {
+    public void betterFibonacci_0() {
         assertEquals(0, new BetterFibonacci().betterFibonacci(0));
+    }
+
+    @Test
+    public void betterFibonacci_1() {
+        assertEquals(1, new BetterFibonacci().betterFibonacci(1));
     }
 }
