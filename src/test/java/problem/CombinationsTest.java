c2fecb7d8b2dac58b212d1de3817b0d9c489906d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-20 18:35:52 -0700

    77. Combinations. Tests.

--- src/test/java/problem/CombinationsTest.java
+++ src/test/java/problem/CombinationsTest.java
@@ -2,25 +2,40 @@ package problem;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class CombinationsTest {
 
     @Test
     public void combine() {
-        int[][] expected = new int[6][2];
-        expected[0][0] = 2;
-        expected[0][1] = 4;
-        expected[1][0] = 3;
-        expected[1][1] = 4;
-        expected[2][0] = 2;
-        expected[2][1] = 3;
-        expected[3][0] = 1;
-        expected[3][1] = 2;
-        expected[4][0] = 1;
-        expected[4][1] = 3;
-        expected[5][0] = 1;
-        expected[5][1] = 4;
-        assertEquals(expected, new Combinations().combine(4, 2));
+        List<List<Integer>> exp = new ArrayList<>();
+        ArrayList<Integer> l1 = new ArrayList<>();
+        l1.add(2);
+        l1.add(4);
+        exp.add(l1);
+        ArrayList<Integer> l2 = new ArrayList<>();
+        l2.add(3);
+        l2.add(4);
+        exp.add(l2);
+        ArrayList<Integer> l3 = new ArrayList<>();
+        l3.add(2);
+        l3.add(3);
+        exp.add(l3);
+        ArrayList<Integer> l4 = new ArrayList<>();
+        l4.add(1);
+        l4.add(2);
+        exp.add(l4);
+        ArrayList<Integer> l5 = new ArrayList<>();
+        l5.add(1);
+        l5.add(3);
+        exp.add(l5);
+        ArrayList<Integer> l6 = new ArrayList<>();
+        l6.add(1);
+        l6.add(4);
+        exp.add(l6);
+        assertEquals(exp, new Combinations().combine(4, 2));
     }
 }
