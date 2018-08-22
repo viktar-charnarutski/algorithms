b24d1b3297560e58d8dfba2d50539c3abe5ccd98
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-22 07:57:29 -0700

    202. Happy Number. Tests.

--- src/test/java/problem/HappyNumberTest.java
+++ src/test/java/problem/HappyNumberTest.java
@@ -2,15 +2,12 @@ package problem;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class HappyNumberTest {
 
     @Test
-    public void isHappy() {
-        HappyNumber ins = new HappyNumber();
-        assertTrue(ins.isHappy(19));
-        assertFalse(ins.isHappy(191));
+    public void isHappy_19() {
+        assertTrue(new HappyNumber().isHappy(19));
     }
 }
