7c14d9b8acff2497b57b15722b0ef94d465a78cd
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-22 07:59:58 -0700

    202. Happy Number. Improving the solution.

--- src/main/java/problem/HappyNumber.java
+++ src/main/java/problem/HappyNumber.java
@@ -16,9 +16,28 @@ import java.util.HashSet;
 public class HappyNumber {
 
     public boolean isHappy(int n) {
-        return isHappy(n, new HashSet<>());
+        int slow = n, fast = n;
+        do {
+            slow = next(slow);
+            fast = next(next(fast));
+        } while (slow != fast);
+
+        return slow == 1;
     }
 
+    private int next(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int r = n % 10;
+            sum += r * r;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public boolean isHappy_Set(int n) {
+        return isHappy(n, new HashSet<>());
+    }
     private boolean isHappy(int n, HashSet<Integer> sums) {
         if (n == 1) return true;
 
