b4489ccd8b9132bad630d20e6c59ad3d32e3a129
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-22 09:10:20 -0700

    18. 4Sum. Tests.

---
+++ src/test/java/problem/FourSumTest.java
@@ -0,0 +1,35 @@
+package problem;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FourSumTest {
+
+    @Test
+    public void fourSum() {
+        List<List<Integer>> expected = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(-1);
+        list1.add(0);
+        list1.add(0);
+        list1.add(1);
+        expected.add(list1);
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(-2);
+        list2.add(-1);
+        list2.add(1);
+        list2.add(2);
+        expected.add(list2);
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(-2);
+        list3.add(0);
+        list3.add(0);
+        list3.add(2);
+        expected.add(list3);
+        assertEquals(expected, new FourSum().fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
+    }
+}
