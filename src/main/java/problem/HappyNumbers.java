44db7380c55215ce1cba8326782d56795d80db09
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-22 07:49:55 -0700

    202. Happy Number. Refactoring.

--- src/main/java/problem/HappyNumbers.java
+++ src/main/java/problem/HappyNumbers.java
@@ -5,9 +5,9 @@ import java.util.Set;
 
 /**
  * 202. Happy Number
- *
+ * <p>
  * Write an algorithm to determine if a number is "happy".
- *
+ * <p>
  * A happy number is a number defined by the following process: Starting with any positive integer,
  * replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1
  * (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process
@@ -18,21 +18,22 @@ public class HappyNumbers {
         if (n == 0 || n == 1) {
             return true;
         }
-        Set<Integer> seen = new HashSet<>();
-        while (true) {
-            if (!seen.add(n)) {
-                return false;
-            }
-            int sum = 0;
-            while (n != 0) {
-                int r = n % 10;
-                sum += r * r;
-                n /= 10;
-            }
-            if (sum == 1) {
-                return true;
-            }
-            n = sum;
+        return isHappyNumber(n, new HashSet<>());
+    }
+
+    private boolean isHappyNumber(int n, Set<Integer> seen) {
+        if (n == 0 || n == 1) {
+            return true;
         }
+        int sum = 0;
+        while (n != 0) {
+            int r = n % 10;
+            sum += r * r;
+            n /= 10;
+        }
+        if (!seen.add(sum)) {
+            return false;
+        }
+        return isHappyNumber(sum, seen);
     }
 }
