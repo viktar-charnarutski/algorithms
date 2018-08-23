318afca1f6e3aa98a3cb03bfd8b94925a7fc483a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-22 19:39:09 -0700

    Max Gain. Initial commit.

---
+++ src/main/java/problem/MaxGain.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * Max Gain
+ * <p>
+ * Given an array of integers, write a method - maxGain - that returns the maximum gain.
+ * Maximum Gain is defined as the maximum difference between 2 elements in a list such that the larger element appears
+ * after the smaller element. If no gain is possible, return 0.
+ */
+public class MaxGain {
+    public static int maxGain(int[] a) {
+        return 0;
+    }
+}
