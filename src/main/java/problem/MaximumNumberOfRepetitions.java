c18c7cd9c2e2a22d9a524bd7fee875ec5647fdee
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-23 07:24:09 -0700

    Maximum Number of Repetitions. Initial commit.

---
+++ src/main/java/problem/MaximumNumberOfRepetitions.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * Maximum Number of Repetitions
+ *
+ * Given an Array of integers, write a method that will return the integer with the maximum number of repetitions.
+ * Your code is expected to run with O(n) time complexity and O(1) space complexity. The elements in the array are
+ * between 0 to size(array) - 1 and the array will not be empty.
+ */
+public class MaximumNumberOfRepetitions {
+    public static int getMaxRepetition(int[] a) {
+        return 0;
+    }
+}
