86a181e6ea11db0fa6b637bf2291e4649c70fbfc
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-23 19:51:48 -0700

    Rotate a Square Image Clockwise. Solution.

--- src/main/java/problem/RotateSquareImage.java
+++ src/main/java/problem/RotateSquareImage.java
@@ -8,6 +8,30 @@ package problem;
  */
 public class RotateSquareImage {
     public static void rotateSquareImageCW(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        transpose(matrix);
+        flipVertical(matrix);
+    }
 
+    private static void transpose(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = row + 1; col < matrix[0].length; col++) {
+                int tmp = matrix[col][row];
+                matrix[col][row] = matrix[row][col];
+                matrix[row][col] = tmp;
+            }
+        }
+    }
+
+    private static void flipVertical(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int col = 0; col < row.length / 2; col++) {
+                int tmp = row[col];
+                row[col] = row[row.length - 1 - col];
+                row[row.length - 1 - col] = tmp;
+            }
+        }
     }
 }
