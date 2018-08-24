35f61b615684f36240b8f4c54305948991437857
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-23 22:47:25 -0700

    Distance of a node from the root. Initial commit.

---
+++ src/main/java/problem/DistanceToTheNode.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * Distance of a node from the root
+ * <p>
+ * Given the root of a Binary Tree  and an integer that represents the data value of a TreeNode present in the tree,
+ * write a method - pathLengthFromRoot that returns the distance between the root and that node. You can assume that
+ * the given key exists in the tree. The distance is defined as the minimum number of nodes that must be traversed to
+ * reach the target node.
+ */
+public class DistanceToTheNode {
+    public int pathLengthFromRoot(TreeNode root, int n1) {
+        return 0;
+    }
+}
