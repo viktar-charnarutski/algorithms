08bb42857eb9e7f0a00f60c484159e448457a0ee
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-25 16:39:35 -0700

    54. Spiral Matrix. Tests.

--- src/test/java/problem/SpiralMatrixTest.java
+++ src/test/java/problem/SpiralMatrixTest.java
@@ -35,4 +35,37 @@ public class SpiralMatrixTest {
 
         assertEquals(expected, new SpiralMatrix().spiralOrder(input));
     }
+
+    @Test
+    public void spiralOrder2() {
+        int[][] input = new int[3][4];
+        input[0][0] = 1;
+        input[0][1] = 2;
+        input[0][2] = 3;
+        input[0][3] = 4;
+        input[1][0] = 5;
+        input[1][1] = 6;
+        input[1][2] = 7;
+        input[1][3] = 8;
+        input[2][0] = 9;
+        input[2][1] = 10;
+        input[2][2] = 11;
+        input[2][3] = 12;
+
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        expected.add(8);
+        expected.add(12);
+        expected.add(11);
+        expected.add(10);
+        expected.add(9);
+        expected.add(5);
+        expected.add(6);
+        expected.add(7);
+
+        assertEquals(expected, new SpiralMatrix().spiralOrder(input));
+    }
 }
