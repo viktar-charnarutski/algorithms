b807ecaf97012f8fde9fe7a6269b9df75a128c15
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-24 17:25:32 -0700

    54. Spiral Matrix. Initial commit.

---
+++ src/main/java/problem/SpiralMatrix.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 54. Spiral Matrix
+ * <p>
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ */
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return Collections.emptyList();
+    }
+}
