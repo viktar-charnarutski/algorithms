02912c9fd2a58879c1a66192ed62e2c5f3acca1b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-26 15:28:46 -0700

    Rotate Linear Array. Solution.

--- src/main/java/problem/RotateLinearArray.java
+++ src/main/java/problem/RotateLinearArray.java
@@ -7,6 +7,25 @@ package problem;
  */
 public class RotateLinearArray {
     public static int[] rotateLeft(int[] arr, int k) {
-        return new int[0];
+        if (arr == null || arr.length <= 1) {
+            return arr;
+        }
+        if (k > arr.length) {
+            k %= arr.length;
+        }
+        rotate(arr, 0, arr.length - 1);
+        rotate(arr, 0, arr.length - k - 1);
+        rotate(arr, arr.length - k, arr.length - 1);
+        return arr;
+    }
+
+    private static void rotate(int[] arr, int start, int end) {
+        while (start < end) {
+            int tmp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = tmp;
+            start++;
+            end--;
+        }
     }
 }
