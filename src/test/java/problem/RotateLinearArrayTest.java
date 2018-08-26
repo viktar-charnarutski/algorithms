9d9405571d3358dc3a1a758523f3fbfae7cc0903
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-26 15:26:02 -0700

    Rotate Linear Array. Tests.

--- src/test/java/problem/RotateLinearArrayTest.java
+++ src/test/java/problem/RotateLinearArrayTest.java
@@ -10,4 +10,9 @@ public class RotateLinearArrayTest {
     public void rotateLeft1() {
         assertArrayEquals(new int[]{3, 4, 5, 1, 2}, RotateLinearArray.rotateLeft(new int[]{1, 2, 3, 4, 5}, 2));
     }
+
+    @Test
+    public void rotateLeft2() {
+        assertArrayEquals(new int[]{2, 3, 4, 5, 6, 7, 8, 9, 1}, RotateLinearArray.rotateLeft(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 10));
+    }
 }
