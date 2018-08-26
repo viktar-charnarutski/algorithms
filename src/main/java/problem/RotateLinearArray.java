773304d0e37bef27da9adcb84497b9f45c2b64a8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-26 15:10:54 -0700

    Rotate Linear Array. Initial commit.

---
+++ src/main/java/problem/RotateLinearArray.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * Rotate Linear Array
+ * <p>
+ * Rotate an array to the left by k positions without using extra space.k can be greater than the size of the array.
+ */
+public class RotateLinearArray {
+    public static int[] rotateLeft(int[] arr, int k) {
+        return new int[0];
+    }
+}
