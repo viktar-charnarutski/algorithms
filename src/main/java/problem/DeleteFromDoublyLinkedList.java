abe508554e53accdd0bf848c7273e47d23ce4fd7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-27 10:35:56 -0700

    Delete the Node at the Specific Position in a Doubly Linked List. Initial commit.

---
+++ src/main/java/problem/DeleteFromDoublyLinkedList.java
@@ -0,0 +1,16 @@
+package problem;
+
+import datastructure.DoublyLinkedNode;
+
+/**
+ * Delete the Node at the Specific Position in a Doubly Linked List
+ * <p>
+ * Given a doubly-linked list, write a method to delete the node at a given position
+ * (starting from 1 as the head position) and return the modified list's head.
+ * Do nothing if the input position is out of range.
+ */
+public class DeleteFromDoublyLinkedList {
+    public DoublyLinkedNode deleteAtPos(DoublyLinkedNode head, int pos) {
+        return null;
+    }
+}
