54492b1620c14b87b4df2cd3f3d641a784b02be0
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-27 10:30:50 -0700

    DoublyLinkedNode class.

---
+++ src/main/java/datastructure/DoublyLinkedNode.java
@@ -0,0 +1,11 @@
+package datastructure;
+
+class DoublyLinkedNode {
+    int data;
+    DoublyLinkedNode next;
+    DoublyLinkedNode prev;
+
+    public DoublyLinkedNode(int data) {
+        this.data = data;
+    }
+}
