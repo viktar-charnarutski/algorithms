6a1bfde12ca9dea2507977806dfe3f5cd348f155
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-27 11:02:44 -0700

    Delete the Node at the Specific Position in a Doubly Linked List. Tests.

--- src/test/java/problem/DeleteFromDoublyLinkedListTest.java
+++ src/test/java/problem/DeleteFromDoublyLinkedListTest.java
@@ -31,4 +31,32 @@ public class DeleteFromDoublyLinkedListTest {
 
         assertEquals(e1, new DeleteFromDoublyLinkedList().deleteAtPos(n1, 3));
     }
+
+    @Test
+    public void deleteAtPos2() {
+        DoublyLinkedNode n1 = new DoublyLinkedNode(1);
+        DoublyLinkedNode n2 = new DoublyLinkedNode(2);
+        DoublyLinkedNode n3 = new DoublyLinkedNode(3);
+        DoublyLinkedNode n4 = new DoublyLinkedNode(4);
+        n1.next = n2;
+        n2.prev = n1;
+        n2.next = n3;
+        n3.prev = n2;
+        n3.next = n4;
+        n4.prev = n3;
+
+
+        DoublyLinkedNode e1 = new DoublyLinkedNode(1);
+        DoublyLinkedNode e2 = new DoublyLinkedNode(2);
+        DoublyLinkedNode e3 = new DoublyLinkedNode(3);
+        DoublyLinkedNode e4 = new DoublyLinkedNode(4);
+        e1.next = e2;
+        e2.prev = e1;
+        e2.next = e3;
+        e3.prev = e2;
+        e3.next = e4;
+        e4.prev = e3;
+
+        assertEquals(e1, new DeleteFromDoublyLinkedList().deleteAtPos(n1, 5));
+    }
 }
