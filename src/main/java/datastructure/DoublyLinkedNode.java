60b60c765d71864410d8195bf599473cc7ddbca5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-27 10:38:50 -0700

    DoublyLinkedNode class.

--- src/main/java/datastructure/DoublyLinkedNode.java
+++ src/main/java/datastructure/DoublyLinkedNode.java
@@ -8,4 +8,24 @@ public class DoublyLinkedNode {
     public DoublyLinkedNode(int data) {
         this.data = data;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DoublyLinkedNode)) return false;
+
+        DoublyLinkedNode that = (DoublyLinkedNode) o;
+
+        if (data != that.data) return false;
+        if (next != null ? !next.equals(that.next) : that.next != null) return false;
+        return prev != null ? prev.equals(that.prev) : that.prev == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = data;
+        result = 31 * result + (next != null ? next.hashCode() : 0);
+        result = 31 * result + (prev != null ? prev.hashCode() : 0);
+        return result;
+    }
 }
