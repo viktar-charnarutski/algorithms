cf35b4f1900cc1af2e831b72d7e186a4c73df38b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-28 14:55:35 -0700

    373. Find K Pairs with Smallest Sums. Initial commit.

---
+++ src/main/java/problem/FindKPairsWithSmallestSums.java
@@ -0,0 +1,19 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 373. Find K Pairs with Smallest Sums
+ * <p>
+ * You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+ * <p>
+ * Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+ * <p>
+ * Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+ */
+public class FindKPairsWithSmallestSums {
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        return Collections.emptyList();
+    }
+}
