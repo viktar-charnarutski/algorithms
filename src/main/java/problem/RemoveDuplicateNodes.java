b8c7e7736bdc16146fbc2a6224dd8247fdb1b2c8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-28 07:41:54 -0700

    Remove Duplicate Nodes. Initial commit.

---
+++ src/main/java/problem/RemoveDuplicateNodes.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * Remove Duplicate Nodes
+ *
+ * Given a singly-linked list, remove duplicates in the list and return head of the list.
+ * Target a worst case space complexity of O(n).
+ */
+public class RemoveDuplicateNodes {
+    public ListNode removeDuplicates(ListNode head) {
+        return null;
+    }
+}
