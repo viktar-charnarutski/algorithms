21a8bd0e13e1d9cdb04d96b2f97194f6f3342eb3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-29 08:12:50 -0700

    297. Binary Tree Serialization. Tests.

--- src/test/java/problem/BinaryTreeSerializationTest.java
+++ src/test/java/problem/BinaryTreeSerializationTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class BinaryTreeSerializationTest {
 
     @Test
-    public void serializeTree() {
-        String expected = "1,2,3,null,null,4,5,";
+    public void serializeTree1() {
+        String expected = "1,2,3,null,null,4,5,null,null,null,null,";
         TreeNode n1 = new TreeNode(1);
         TreeNode n2 = new TreeNode(2);
         TreeNode n3 = new TreeNode(3);
@@ -22,7 +22,7 @@ public class BinaryTreeSerializationTest {
     }
 
     @Test
-    public void restoreTree() {
+    public void restoreTree1() {
         TreeNode n1 = new TreeNode(1);
         TreeNode n2 = new TreeNode(2);
         TreeNode n3 = new TreeNode(3);
@@ -32,6 +32,43 @@ public class BinaryTreeSerializationTest {
         n1.right = n3;
         n3.left = n4;
         n3.right = n5;
-        assertEquals(n1, new BinaryTreeSerialization().restoreTree("1,2,3,null,null,4,5,"));
+        assertEquals(n1, new BinaryTreeSerialization().restoreTree("1,2,3,null,null,4,5,null,null,null,null,"));
+    }
+
+    @Test
+    public void restoreTree2() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        assertEquals(n1, new BinaryTreeSerialization().restoreTree("1,2,3,4,5,6,7,null,null,null,null,null,null,null,null,"));
+    }
+
+    @Test
+    public void serializeTree2() {
+        String expected = "1,2,3,4,5,6,7,null,null,null,null,null,null,null,null,";
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        assertEquals(expected, new BinaryTreeSerialization().serializeTree(n1));
     }
 }
