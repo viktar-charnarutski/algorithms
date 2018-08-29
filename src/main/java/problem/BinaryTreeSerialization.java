75a9125a4434bb6f2608e6e2d3b8eacdab074aa1
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-29 07:41:41 -0700

    Binary Tree Serialization. Initial commit

---
+++ src/main/java/problem/BinaryTreeSerialization.java
@@ -0,0 +1,27 @@
+package problem;
+
+/**
+ * 297. Serialize and Deserialize Binary Tree
+ * <p>
+ * In Computer Science, serialization is the process of converting objects or data structures into a sequence
+ * (or series) of characters that can be stored easily in a file / database table or transmitted across a network.
+ * Serialized objects need to be de-serialized to create a semantically identical clone of the original object, before
+ * being used in programs. You're given the root node of a binary tree - TreeNode root in the method serializeTree.
+ * This method should serialize the binary tree and output a String str, which is then used as an input parameter for
+ * the method restoreTree. restoreTree should create a Binary Tree that is structurally identical to the one you
+ * serialized and return the root node of the tree. Your task is to fill in the logic for these 2 methods.
+ * <p>
+ * Don't worry about passing the serialized String to restoreTree - that will be done automatically when you run your
+ * code. Feel free to use any notation you prefer when serializing the binary tree. The choice of traversal algorithm
+ * is also open - but try and limit the time complexity of both methods to O(n).
+ */
+public class BinaryTreeSerialization {
+    public String serializeTree(TreeNode root) {
+        return null;
+    }
+
+    public TreeNode restoreTree(String str) {
+        return null;
+    }
+
+}
