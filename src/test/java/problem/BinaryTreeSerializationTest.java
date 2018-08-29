679f2ac245d3d12af9113a55ed7f49a21e22eeaf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-29 07:46:50 -0700

    297. Binary Tree Serialization. Tests.

--- src/test/java/problem/BinaryTreeSerializationTest.java
+++ src/test/java/problem/BinaryTreeSerializationTest.java
@@ -21,4 +21,17 @@ public class BinaryTreeSerializationTest {
         assertEquals(expected, new BinaryTreeSerialization().serializeTree(n1));
     }
 
+    @Test
+    public void restoreTree() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        n1.left = n2;
+        n1.right = n3;
+        n3.left = n4;
+        n3.right = n5;
+        assertEquals(n1, new BinaryTreeSerialization().restoreTree("1,2,3,null,null,4,5,"));
+    }
 }
