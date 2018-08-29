c21ccf5747355987b88c7ff2f87b9875d4f22a38
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-29 14:35:40 -0700

    TreeNode. Adding an additional constructor with left, right node parameters.

--- src/main/java/problem/TreeNode.java
+++ src/main/java/problem/TreeNode.java
@@ -8,8 +8,15 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int x) {
-        val = x;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+
     }
 
     @Override
