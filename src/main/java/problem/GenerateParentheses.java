f4982232871fc1cd8240ac762d96df14665d486a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-30 07:39:20 -0700

    22. Generate Parentheses. Initial commit.

---
+++ src/main/java/problem/GenerateParentheses.java
@@ -0,0 +1,14 @@
+package problem;
+
+import java.util.ArrayList;
+
+/**
+ * 22. Generate Parentheses
+ * <p>
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ */
+public class GenerateParentheses {
+    public static ArrayList<String> combParenthesis(int pairs) {
+        return null;
+    }
+}
