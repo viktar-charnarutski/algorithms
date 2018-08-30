ec1e8c242405b74da717993dcb38c4e82e0aa4f4
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-29 19:39:17 -0700

    632. Smallest Range. Initial commit.

---
+++ src/main/java/problem/SmallestRange.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.List;
+
+/**
+ * 632. Smallest Range
+ * <p>
+ * You have k lists of sorted integers in ascending order. Find the smallest range that includes at least one number
+ * from each of the k lists.
+ * <p>
+ * We define the range [a,b] is smaller than range [c,d] if b-a < d-c or a < c if b-a == d-c.
+ */
+public class SmallestRange {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        return new int[0];
+    }
+}
