66f6e840e176bf62acd920ed85bc19da33988029
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-30 07:41:22 -0700

    22. Generate Parentheses. Tests.

---
+++ src/test/java/problem/GenerateParenthesesTest.java
@@ -0,0 +1,22 @@
+package problem;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class GenerateParenthesesTest {
+
+    @Test
+    public void combParenthesis1() {
+        List<String> res = new ArrayList<>();
+        res.add("((()))");
+        res.add("(()())");
+        res.add("(())()");
+        res.add("()(())");
+        res.add("()()()");
+        assertEquals(res, GenerateParentheses.combParenthesis(3));
+    }
+}
