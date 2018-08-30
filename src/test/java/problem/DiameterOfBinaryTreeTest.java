ded65aaf2d2a9afb3e93a443cb84ddb9d86bc029
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-30 07:39:03 -0700

    Diameter of Binary Tree. Tests.

--- src/test/java/problem/DiameterOfBinaryTreeTest.java
+++ src/test/java/problem/DiameterOfBinaryTreeTest.java
@@ -9,7 +9,7 @@ public class DiameterOfBinaryTreeTest {
     private DiameterOfBinaryTree ins = new DiameterOfBinaryTree();
 
     @Test
-    public void diameterOfBinaryTree() {
+    public void diameterOfBinaryTree1() {
         TreeNode n1 = new TreeNode(1);
         TreeNode n2 = new TreeNode(2);
         TreeNode n3 = new TreeNode(3);
@@ -35,5 +35,19 @@ public class DiameterOfBinaryTreeTest {
         n11.right = n12;
 
         assertSame(8, ins.diameterOfBinaryTree(n1));
+    }
+    @Test
+    public void diameterOfBinaryTree2() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+
+        assertSame(3, ins.diameterOfBinaryTree(n1));
     }
 }
