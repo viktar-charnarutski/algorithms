4477312845d5114ddbb34483ff57daa5148b416f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-08-30 08:01:28 -0700

    22. Generate Parentheses. Solution.

--- src/main/java/problem/GenerateParentheses.java
+++ src/main/java/problem/GenerateParentheses.java
@@ -1,6 +1,8 @@
 package problem;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 22. Generate Parentheses
@@ -8,7 +10,25 @@ import java.util.ArrayList;
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  */
 public class GenerateParentheses {
-    public static ArrayList<String> combParenthesis(int pairs) {
-        return null;
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0) {
+            return Collections.emptyList();
+        }
+        List<String> res = new ArrayList<>();
+        generateParenthesis(0, 0, "", res, n);
+        return res;
+    }
+
+    private void generateParenthesis(int left, int right, String curr, List<String> res, int max) {
+        if (curr.length() == max * 2) {
+            res.add(curr);
+            return;
+        }
+        if (left < max) {
+            generateParenthesis(left + 1, right, curr + "(", res, max);
+        }
+        if (right < left) {
+            generateParenthesis(left, right + 1, curr + ")", res, max);
+        }
     }
 }
