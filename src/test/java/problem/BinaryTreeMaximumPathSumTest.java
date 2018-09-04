64025c25c50c531c0ec8b1af1e5b428cfec73305
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-03 20:03:20 -0700

    22. Generate Parentheses. Tests.

--- src/test/java/problem/BinaryTreeMaximumPathSumTest.java
+++ src/test/java/problem/BinaryTreeMaximumPathSumTest.java
@@ -65,4 +65,30 @@ public class BinaryTreeMaximumPathSumTest {
         TreeNode n1 = new TreeNode(-10);
         assertEquals(-10, new BinaryTreeMaximumPathSum().maxPathSum(n1));
     }
+
+    @Test
+    public void maxPathSum5() {
+        TreeNode n1 = new TreeNode(10);
+        TreeNode n2 = new TreeNode(-10);
+        n1.left = n2;
+        assertEquals(10, new BinaryTreeMaximumPathSum().maxPathSum(n1));
+    }
+
+    @Test
+    public void maxPathSum6() {
+        TreeNode n1 = new TreeNode(-10);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(-7);
+        n1.left = n2;
+        n2.right = n3;
+        n3.left = n4;
+        n4.right = n5;
+        n5.left = n6;
+        n6.right = n7;
+        assertEquals(20, new BinaryTreeMaximumPathSum().maxPathSum(n1));
+    }
 }
