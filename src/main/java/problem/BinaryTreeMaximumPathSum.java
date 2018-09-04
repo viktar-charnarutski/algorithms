a0c8ffdd8c4038a3b8c34eebea8ec83bd2fc36b3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-03 19:05:57 -0700

    124. Binary Tree Maximum Path Sum. Initial commit.

---
+++ src/main/java/problem/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * 124. Binary Tree Maximum Path Sum
+ *
+ * Given a non-empty binary tree, find the maximum path sum.
+ *
+ * For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along
+ * the parent-child connections. The path must contain at least one node and does not need to go through the root.
+ */
+public class BinaryTreeMaximumPathSum {
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+}
