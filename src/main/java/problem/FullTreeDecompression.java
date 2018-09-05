88449a425b120a14697fa4388b3fe0a4679f0129
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-04 19:01:54 -0700

    Full Tree Decompression. Initial commit.

---
+++ src/main/java/problem/FullTreeDecompression.java
@@ -0,0 +1,21 @@
+package problem;
+
+/**
+ * Full Tree Decompression
+ * <p>
+ * Given a String that represents a Binary Tree, write a method - decompressTree that decompresses that tree
+ * (reconstructs the tree) and returns the root TreeNode. The compression algorithm included traversing the tree level
+ * by level, from the left to the right. The TreeNode's data values were appended to the String, delimited by commas.
+ * Also, null TreeNodes were denoted by appending an asterisk - *. The input String denotes the structure of a Full
+ * Binary Tree - i.e. a tree that is structurally balanced. However, the reconstructed tree may not be a full tree as
+ * the String included * characters, which represent null TreeNodes.
+ * <p>
+ * Note:
+ * You can assume that if a Binary Tree contains k levels, the compressed String will contain 2k-1 elements - either
+ * numbers or *.
+ */
+public class FullTreeDecompression {
+    public TreeNode decompressTree(String str){
+        return null;
+    }
+}
