753764234995be3a21fc9e067e474e3745a617b7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-04 19:05:32 -0700

    Full Tree Decompression. Tests.

--- src/test/java/problem/FullTreeDecompressionTest.java
+++ src/test/java/problem/FullTreeDecompressionTest.java
@@ -23,4 +23,20 @@ public class FullTreeDecompressionTest {
         n3.right = n7;
         assertEquals(n1, new FullTreeDecompression().decompressTree("1,2,3,4,5,6,7"));
     }
+
+    @Test
+    public void decompressTree2() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n3.left = n6;
+        n3.right = n7;
+        assertEquals(n1, new FullTreeDecompression().decompressTree("1,2,3,4,*,6,7"));
+    }
 }
