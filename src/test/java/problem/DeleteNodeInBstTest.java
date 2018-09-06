cb8f3009b3a9e24f3bf8aed7f6ab7cfb9439a9bd
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-05 23:30:20 -0700

    450. Delete Node in a BST. Tests.

--- src/test/java/problem/DeleteNodeInBstTest.java
+++ src/test/java/problem/DeleteNodeInBstTest.java
@@ -163,4 +163,33 @@ public class DeleteNodeInBstTest {
 
         assertEquals(m5, new DeleteNodeInBst().deleteNode(n5, 6));
     }
+
+    @Test
+    public void deleteNode7() {
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n7 = new TreeNode(7);
+        n5.left = n3;
+        n5.right = n6;
+        n3.left = n2;
+        n3.right = n4;
+        n6.right = n7;
+
+        TreeNode m5 = new TreeNode(5);
+        TreeNode m2 = new TreeNode(2);
+        TreeNode m3 = new TreeNode(3);
+        TreeNode m4 = new TreeNode(4);
+        TreeNode m7 = new TreeNode(7);
+        TreeNode m6 = new TreeNode(6);
+        m5.left = m3;
+        m5.right = m6;
+        m3.left = m2;
+        m3.right = m4;
+        m6.right = m7;
+
+        assertEquals(m5, new DeleteNodeInBst().deleteNode(n5, 0));
+    }
 }
