61c0af7695cdd1b7483ab10fe4b707a63f333cb3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-05 19:01:59 -0700

    450. Delete Node in a BST. Initial commit.

---
+++ src/main/java/problem/DeleteNodeInBst.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 450. Delete Node in a BST
+ * <p>
+ * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+ * <p>
+ * Basically, the deletion can be divided into two stages:
+ * <p>
+ *  - Search for a node to remove.
+ *  - If the node is found, delete the node.
+ */
+public class DeleteNodeInBst {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return null;
+    }
+}
