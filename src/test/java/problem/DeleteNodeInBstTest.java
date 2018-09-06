1ee7e1df214c5047956ae549bfed16bd4563988e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-05 23:16:20 -0700

    22. Generate Parentheses. Tests.

--- src/test/java/problem/DeleteNodeInBstTest.java
+++ src/test/java/problem/DeleteNodeInBstTest.java
@@ -48,8 +48,8 @@ public class DeleteNodeInBstTest {
         n6.right = n7;
 
         TreeNode m5 = new TreeNode(5);
-        TreeNode m6 = new TreeNode(6);
         TreeNode m3 = new TreeNode(3);
+        TreeNode m6 = new TreeNode(6);
         TreeNode m2 = new TreeNode(2);
         TreeNode m4 = new TreeNode(4);
         m5.left = m3;
@@ -62,6 +62,29 @@ public class DeleteNodeInBstTest {
 
     @Test
     public void deleteNode3() {
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n7 = new TreeNode(7);
+        n5.left = n3;
+        n5.right = n6;
+        n3.left = n2;
+        n6.right = n7;
+
+        TreeNode m5 = new TreeNode(5);
+        TreeNode m6 = new TreeNode(6);
+        TreeNode m2 = new TreeNode(2);
+        TreeNode m7 = new TreeNode(7);
+        m5.left = m2;
+        m5.right = m6;
+        m6.right = m7;
+
+        assertEquals(m5, new DeleteNodeInBst().deleteNode(n5, 3));
+    }
+
+    @Test
+    public void deleteNode4() {
         TreeNode n5 = new TreeNode(5);
         TreeNode n3 = new TreeNode(3);
         TreeNode n6 = new TreeNode(6);
