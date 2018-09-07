d4662578070bbea756950f5193674f2977c7fcc3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-06 17:14:03 -0700

    String Permutation. Initial commit.

---
+++ src/main/java/problem/StringPermutation.java
@@ -0,0 +1,24 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * String Permutation
+ * <p>
+ * String permutations are the various possible strings made by the rearrangement of the characters in the original
+ * String.
+ * <p>
+ * For example, the permutations of car are
+ * <p>
+ * car, cra, acr, arc, rac, rca
+ * <p>
+ * Write a recursive method getPermutations() that returns all permutations of an input String in an ArrayList.
+ * Define a helper method if needed. For the sake of simplicity, assume that all characters in the input String are
+ * unique.
+ */
+public class StringPermutation {
+    public static List<String> getPermutations(String s) {
+        return Collections.emptyList();
+    }
+}
