ae730bb90143e0078aac6483fa4bf28db5de6f0b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-07 12:02:32 -0700

    Combinations and Permutations. Solution.

--- src/main/java/problem/CombinationsAndPermutations.java
+++ src/main/java/problem/CombinationsAndPermutations.java
@@ -14,6 +14,22 @@ import java.util.ArrayList;
  */
 public class CombinationsAndPermutations {
     public static ArrayList<String> getCombPerms(String s) {
-        return null;
+        ArrayList<String> res = new ArrayList<>();
+        perm("", res, s);
+        return res;
+    }
+
+    private static void perm(String curr, ArrayList<String> res, String s) {
+        if (curr.length() <= s.length() && !res.contains(curr)) {
+            if (curr.length() != 0) res.add(curr);
+        } else {
+            return;
+        }
+        for (char c : s.toCharArray()) {
+            if (curr.indexOf(c) != -1) {
+                continue;
+            }
+            perm(curr + c , res, s);
+        }
     }
 }
