9718fab2e81f66c7a099e84b62a161a2e9011350
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-07 11:47:39 -0700

    Combinations and Permutations. Initial commit.

---
+++ src/main/java/problem/CombinationsAndPermutations.java
@@ -0,0 +1,19 @@
+package problem;
+
+import java.util.ArrayList;
+
+/**
+ * Combinations and Permutations
+ * <p>
+ * Given a string, list all possible combinations and permutations of its characters.
+ * <p>
+ * Examples:
+ * getCombPerms("a") ==> {"a"}
+ * <p>
+ * getCombPerms("ab") ==> {"a","ab","ba","b"}
+ */
+public class CombinationsAndPermutations {
+    public static ArrayList<String> getCombPerms(String s) {
+        return null;
+    }
+}
