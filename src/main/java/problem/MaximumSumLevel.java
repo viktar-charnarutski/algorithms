5e2ed1f2292d8c8c1c76c41679ca42ce548c43cf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-06 21:29:18 -0700

    Find the Level that has the Maximum Sum. Initial commit.

---
+++ src/main/java/problem/MaximumSumLevel.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * Find the Level that has the Maximum Sum
+ *
+ * Given a binary tree, write a method to return the level that has the maximum sum.
+ * In case the tree is empty, return -1
+ */
+public class MaximumSumLevel {
+    public int findMaxSumLevel(TreeNode root) {
+        return 0;
+    }
+}
