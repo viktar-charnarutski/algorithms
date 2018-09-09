3049d9377a3abe9a7dbc5c836cd8853de54e0095
Viktar Charnarutski <v.charnarutski@gmail.com>
2018-09-08 18:22:54 -0700

    236. Lowest Common Ancestor of a Binary Tree. Initial commit.

---
+++ src/main/java/problem/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 236. Lowest Common Ancestor of a Binary Tree
+ * <p>
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+ * <p>
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q
+ * as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ * <p>
+ * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ */
+public class LowestCommonAncestorOfBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+}
