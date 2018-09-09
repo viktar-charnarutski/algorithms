094c3817ad0a2c05009653e8192aa77bc6b3a038
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-08 18:30:21 -0700

    22. Generate Parentheses. Tests.

--- src/test/java/problem/LowestCommonAncestorOfBinaryTreeTest.java
+++ src/test/java/problem/LowestCommonAncestorOfBinaryTreeTest.java
@@ -71,4 +71,26 @@ public class LowestCommonAncestorOfBinaryTreeTest {
         n7.right = n9;
         assertEquals(n1, new LowestCommonAncestorOfBinaryTree().lowestCommonAncestor(n3, n6, n7));
     }
+
+    @Test
+    public void lowestCommonAncestor4() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n9 = new TreeNode(9);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n6.left = n8;
+        n7.right = n9;
+        assertEquals(n1, new LowestCommonAncestorOfBinaryTree().lowestCommonAncestor(n2, n4, n5));
+    }
 }
