1365f91a94ad949cf9b0eacbf263100638afcbc3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-10 08:13:16 -0700

    93. Restore IP Addresses. Initial commit.

---
+++ src/main/java/problem/RestoreIpAddresses.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 93. Restore IP Addresses
+ * <p>
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ */
+public class RestoreIpAddresses {
+    public List<String> restoreIpAddresses(String s) {
+        return Collections.emptyList();
+    }
+}
