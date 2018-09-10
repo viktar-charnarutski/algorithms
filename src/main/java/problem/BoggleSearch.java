9f325f62ed83c407c8a990c8ed34fa04b267a0c8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-09 21:56:10 -0700

    Boggle Search. Initial commit.

---
+++ src/main/java/problem/BoggleSearch.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * Boggle Search
+ *
+ * You're given a 2D Boggle Board which contains an m x n matrix of chars - char[][] board, and a String - word.
+ * Write a method - boggleSearch that searches the Boggle Board for the presence of the input word. Words on the board
+ * can be constructed with sequentially adjacent letters, where adjacent letters are horizontal or vertical neighbors
+ * (not diagonal). Also, each letter on the Boggle Board must be used only once.
+ */
+public class BoggleSearch {
+    public static boolean boggleSearch(char[][] board, String word){
+        return false;
+    }
+}
