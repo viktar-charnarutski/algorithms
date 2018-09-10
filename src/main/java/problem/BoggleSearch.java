2a3c4855d06c22817940ff257bbdc675b8d7010c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-09 22:03:53 -0700

    Boggle Search. Solution.

--- src/main/java/problem/BoggleSearch.java
+++ src/main/java/problem/BoggleSearch.java
@@ -2,7 +2,7 @@ package problem;
 
 /**
  * Boggle Search
- *
+ * <p>
  * You're given a 2D Boggle Board which contains an m x n matrix of chars - char[][] board, and a String - word.
  * Write a method - boggleSearch that searches the Boggle Board for the presence of the input word. Words on the board
  * can be constructed with sequentially adjacent letters, where adjacent letters are horizontal or vertical neighbors
@@ -10,6 +10,39 @@ package problem;
  */
 public class BoggleSearch {
     public static boolean boggleSearch(char[][] board, String word){
+        if (board == null || board.length == 0) {
+            return false;
+        }
+        if (word == null || word.length() == 0) {
+            return false;
+        }
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                if (boggleSearch(row, col, 0, board, word)) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+
+    private static boolean boggleSearch(int r, int c, int i, char[][] board, String word){
+        if (i == word.length()) {
+            return true;
+        }
+        if (r >= board.length || r < 0 || c >= board[0].length || c < 0
+                || board[r][c] == '*' || board[r][c] != word.charAt(i)) {
+            return false;
+        }
+        board[r][c] = '*';
+
+        boolean res = boggleSearch(r - 1, c, i + 1, board, word)
+                || boggleSearch(r + 1, c, i + 1, board, word)
+                || boggleSearch(r, c - 1, i + 1, board, word)
+                || boggleSearch(r, c + 1, i + 1, board, word);
+
+        board[r][c] = word.charAt(i);
+
+        return res;
     }
 }
