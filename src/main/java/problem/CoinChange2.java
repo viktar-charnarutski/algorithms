14d5eba24970615a363a9c6734acac73c8eb1fa5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-11 15:47:55 -0700

    518. Coin Change 2. Initial commit.

---
+++ src/main/java/problem/CoinChange2.java
@@ -0,0 +1,20 @@
+package problem;
+
+/**
+ * 518. Coin Change 2
+ * <p>
+ * You are given coins of different denominations and a total amount of money. Write a function to compute the number
+ * of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+ * <p>
+ * Note: You can assume that
+ * <p>
+ * 0 <= amount <= 5000
+ * 1 <= coin <= 5000
+ * the number of coins is less than 500
+ * the answer is guaranteed to fit into signed 32-bit integer
+ */
+public class CoinChange2 {
+    public int change(int amount, int[] coins) {
+        return 0;
+    }
+}
