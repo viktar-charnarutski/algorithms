4f0d7936612783661eb19d2f6aa2a5f7f13b86d6
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-12 07:42:33 -0700

    Interval class. Equals and hashCode.

--- src/main/java/problem/Interval.java
+++ src/main/java/problem/Interval.java
@@ -13,4 +13,22 @@ public class Interval {
         start = s;
         end = e;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Interval)) return false;
+
+        Interval interval = (Interval) o;
+
+        if (start != interval.start) return false;
+        return end == interval.end;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start;
+        result = 31 * result + end;
+        return result;
+    }
 }
