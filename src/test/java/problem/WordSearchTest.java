82c4aab73d5f11400a68b1550d1e1e01bfbb538b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-13 15:54:22 -0700

    79. Word Search. Tests.

--- src/test/java/problem/WordSearchTest.java
+++ src/test/java/problem/WordSearchTest.java
@@ -24,4 +24,21 @@ public class WordSearchTest {
         assertTrue(new WordSearch().exist(board, "ABCCED"));
     }
 
+    @Test
+    public void exist2() {
+        char[][] board = new char[3][4];
+        board[0][0] = 'A';
+        board[0][1] = 'B';
+        board[0][2] = 'C';
+        board[0][3] = 'D';
+        board[1][0] = 'S';
+        board[1][1] = 'F';
+        board[1][2] = 'C';
+        board[1][3] = 'S';
+        board[2][0] = 'A';
+        board[2][1] = 'D';
+        board[2][2] = 'E';
+        board[2][3] = 'E';
+        assertFalse(new WordSearch().exist(board, "ABCB"));
+    }
 }
