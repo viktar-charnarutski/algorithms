8150ab50aea2e83a6ca466869261856450b988d7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-14 17:38:32 -0700

    90. Subsets II. Initial commit.

---
+++ src/main/java/problem/SubsetsII.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 90. Subsets II
+ * <p>
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+ * <p>
+ * Note: The solution set must not contain duplicate subsets.
+ */
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        return Collections.emptyList();
+    }
+}
