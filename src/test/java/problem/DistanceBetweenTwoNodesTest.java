b12db7af3ed70113b1d1e02b618faa61204d6bcf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-15 00:18:28 -0700

    Distance between two nodes in a Binary Tree. Tests.

---
+++ src/test/java/problem/DistanceBetweenTwoNodesTest.java
@@ -0,0 +1,39 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DistanceBetweenTwoNodesTest {
+
+    @Test
+    public void getNodeDistance() {
+        TreeNode root = new TreeNode(0);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node10 = new TreeNode(10);
+        root.right = node1;
+        root.left = node2;
+
+        node1.right = node3;
+        node1.left = node4;
+
+        node2.right = node5;
+        node2.left = node6;
+
+        node3.right = node7;
+        node3.left = node8;
+
+        node4.right = node9;
+        node4.left = node10;
+
+        assertEquals(5, new DistanceBetweenTwoNodes().getNodeDistance(root, 6, 10));
+    }
+}
