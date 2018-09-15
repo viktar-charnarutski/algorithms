300c408c61b7e1d8e74f4b1eb6c04fe0f3d44df7
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-15 00:15:58 -0700

    Distance between two nodes in a Binary Tree. Initial commit.

---
+++ src/main/java/problem/DistanceBetweenTwoNodes.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * Distance between two nodes in a Binary Tree
+ *
+ * Given the root of a Binary Tree  and 2 integers that represent the data values of any two TreeNodes present in the
+ * tree, write a method - getNodeDistance that returns the distance between the nodes. You can assume that the given
+ * keys exist in the tree. The distance between two nodes is defined as the minimum number of edges that must be
+ * traversed to travel between the two nodes.
+ */
+public class DistanceBetweenTwoNodes {
+    public int getNodeDistance(TreeNode root, int n1, int n2) {
+        return 0;
+    }
+}
