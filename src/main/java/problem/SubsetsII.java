8918ef3935b621f3436e2776424f70f1e49a6c7e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-14 18:03:52 -0700

    90. Subsets II. Solution.

--- src/main/java/problem/SubsetsII.java
+++ src/main/java/problem/SubsetsII.java
@@ -1,5 +1,7 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +14,24 @@ import java.util.List;
  */
 public class SubsetsII {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        return Collections.emptyList();
+        if (nums == null || nums.length == 0) {
+            return Collections.emptyList();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        subsetsWithDup(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void subsetsWithDup(int[] nums, int ind, ArrayList<Integer> curr, List<List<Integer>> res) {
+        res.add(new ArrayList<>(curr));
+        for (int i = ind; i < nums.length; i++) {
+            if (i > ind && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            curr.add(nums[i]);
+            subsetsWithDup(nums, i + 1, curr, res);
+            curr.remove(curr.size() - 1);
+        }
     }
 }
