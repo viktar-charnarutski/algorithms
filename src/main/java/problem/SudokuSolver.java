15125a0eafb1b4bcc385e04adb941bf1b1e69a0f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-15 11:59:18 -0700

    37. Sudoku Solver. Initial commit.

---
+++ src/main/java/problem/SudokuSolver.java
@@ -0,0 +1,19 @@
+package problem;
+
+/**
+ * 37. Sudoku Solver
+ * <p>
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * <p>
+ * A sudoku solution must satisfy all of the following rules:
+ * <p>
+ * Each of the digits 1-9 must occur exactly once in each row.
+ * Each of the digits 1-9 must occur exactly once in each column.
+ * Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+ * Empty cells are indicated by the character '.'.
+ */
+public class SudokuSolver {
+    public void solveSudoku(char[][] board) {
+
+    }
+}
