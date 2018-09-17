8aec9c2d9a28caa9aafcc1b471dc3c5577f43d45
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-16 17:21:42 -0700

    224. Basic Calculator. Tests.

--- src/test/java/problem/BasicCalculatorTest.java
+++ src/test/java/problem/BasicCalculatorTest.java
@@ -10,4 +10,9 @@ public class BasicCalculatorTest {
     public void calculate1() {
         assertEquals(5, new BasicCalculator().calculate("2+3"));
     }
+
+    @Test
+    public void calculate2() {
+        assertEquals(3, new BasicCalculator().calculate("2-1 + 2 "));
+    }
 }
