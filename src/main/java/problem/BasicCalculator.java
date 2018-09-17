5df88c02a91bb556b1e874dc686ab84956853f78
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-16 17:20:24 -0700

    224. Basic Calculator. Initial commit.

---
+++ src/main/java/problem/BasicCalculator.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * 224. Basic Calculator
+ * <p>
+ * Implement a basic calculator to evaluate a simple expression string.
+ * <p>
+ * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -,
+ * non-negative integers and empty spaces .
+ */
+public class BasicCalculator {
+    public int calculate(String s) {
+        return 0;
+    }
+}
