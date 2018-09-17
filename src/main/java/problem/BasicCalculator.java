17fd5da5823324e1bcdb0869e166918b3206622d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-16 18:42:55 -0700

    224. Basic Calculator. Solution.

--- src/main/java/problem/BasicCalculator.java
+++ src/main/java/problem/BasicCalculator.java
@@ -1,5 +1,7 @@
 package problem;
 
+import java.util.Stack;
+
 /**
  * 224. Basic Calculator
  * <p>
@@ -10,6 +12,36 @@ package problem;
  */
 public class BasicCalculator {
     public int calculate(String s) {
-        return 0;
+        if (s == null || s.length() == 0) return 0;
+        int sum = 0, sign = 1, num = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = 10 * num + ((int) c - '0');
+            } else if (c == '+') {
+                sum += sign * num;
+                sign = 1;
+                num = 0;
+            } else if (c == '-') {
+                sum += sign * num;
+                sign = -1;
+                num = 0;
+            } else if (c == '(') {
+                stack.push(sum);
+                stack.push(sign);
+                sum = 0;
+                sign = 1;
+            } else if (c == ')') {
+                sum += sign * num;
+                num = 0;
+                sum *= stack.pop();
+                sum += stack.pop();
+            }
+        }
+        if (num != 0) {
+            sum += sign * num;
+        }
+        return sum;
     }
 }
