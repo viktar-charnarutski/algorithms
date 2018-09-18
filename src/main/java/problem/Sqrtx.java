025d9dd4a2636a7158e68d6cb82081fc5f9280c1
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-18 07:52:54 -0700

    69. Sqrt(x). Initial commit.

---
+++ src/main/java/problem/Sqrtx.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 69. Sqrt(x)
+ * <p>
+ * Implement int sqrt(int x).
+ * <p>
+ * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+ * <p>
+ * Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is
+ * returned.
+ */
+public class Sqrtx {
+    public int mySqrt(int x) {
+        return 0;
+    }
+}
