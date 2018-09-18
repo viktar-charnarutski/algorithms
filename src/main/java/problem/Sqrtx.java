4a4b9f779b3c1f5c0d7cb873402f67a252b79437
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-18 08:10:45 -0700

    69. Sqrt(x). Solution.

--- src/main/java/problem/Sqrtx.java
+++ src/main/java/problem/Sqrtx.java
@@ -12,6 +12,17 @@ package problem;
  */
 public class Sqrtx {
     public int mySqrt(int x) {
-        return 0;
+        if (0 == x) return 0;
+        int left = 1, right = x, ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 }
