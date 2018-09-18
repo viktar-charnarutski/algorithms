60e212febe66ee5ca26cea642846b385b8eb0cbf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-18 07:53:52 -0700

    69. Sqrt(x). Tests.

---
+++ src/test/java/problem/SqrtxTest.java
@@ -0,0 +1,13 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqrtxTest {
+
+    @Test
+    public void mySqrt1() {
+        assertEquals(3, new Sqrtx().mySqrt(9));
+    }
+}
