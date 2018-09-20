23b368a5ba9e13deaeb6807a0f913108045d4a07
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-20 15:48:19 -0700

    150. Evaluate Reverse Polish Notation. Initial commit.

---
+++ src/main/java/problem/EvaluateReversePolishNotation.java
@@ -0,0 +1,20 @@
+package problem;
+
+/**
+ * 150. Evaluate Reverse Polish Notation
+ *
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ *
+ * Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+ *
+ * Note:
+ *
+ * Division between two integers should truncate toward zero.
+ * The given RPN expression is always valid. That means the expression would always evaluate to a result and there
+ * won't be any divide by zero operation.
+ */
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        return 0;
+    }
+}
