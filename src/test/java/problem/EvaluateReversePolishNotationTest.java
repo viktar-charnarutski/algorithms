6b114ce6f7016a1fbde59a7774323448dd30e546
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-20 15:52:13 -0700

    150. Evaluate Reverse Polish Notation. Tests.

--- src/test/java/problem/EvaluateReversePolishNotationTest.java
+++ src/test/java/problem/EvaluateReversePolishNotationTest.java
@@ -10,4 +10,9 @@ public class EvaluateReversePolishNotationTest {
     public void evalRPN1() {
         assertEquals(9, new EvaluateReversePolishNotation().evalRPN(new String[]{"2", "1", "+", "3", "*"}));
     }
+
+    @Test
+    public void evalRPN2() {
+        assertEquals(6, new EvaluateReversePolishNotation().evalRPN(new String[]{"4", "13", "5", "/", "+"}));
+    }
 }
