f3788d5373edb8de5b6303688fb62cfb72dd9d1d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-19 23:01:09 -0700

    221. Maximal Square. Initial commit.

---
+++ src/main/java/problem/MaximalSquare.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * 221. Maximal Square
+ * <p>
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+ */
+public class MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        return 0;
+    }
+}
