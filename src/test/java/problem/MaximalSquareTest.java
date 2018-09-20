b6d0debd09d93ba824a43cd1741b76bd65a69536
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-19 23:15:01 -0700

    221. Maximal Square. Tests.

--- src/test/java/problem/MaximalSquareTest.java
+++ src/test/java/problem/MaximalSquareTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class MaximalSquareTest {
 
     @Test
-    public void maximalSquare() {
+    public void maximalSquare1() {
         char[][] input = {
                 {'1', '0', '1', '0', '0'},
                 {'1', '0', '1', '1', '1'},
@@ -15,5 +15,17 @@ public class MaximalSquareTest {
                 {'1', '0', '0', '1', '0'},
         };
         assertEquals(4, new MaximalSquare().maximalSquare(input));
+    }
+
+    @Test
+    public void maximalSquare2() {
+        char[][] input = {
+                {'0', '0', '0', '1'},
+                {'1', '1', '0', '1'},
+                {'1', '1', '1', '1'},
+                {'0', '1', '1', '1'},
+                {'0', '1', '1', '1'}
+        };
+        assertEquals(9, new MaximalSquare().maximalSquare(input));
     }
 }
