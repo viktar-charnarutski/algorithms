9aa234adaa835d0048b5ac1fe33683dfad369e34
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-21 07:24:04 -0700

    Pancake Sort. Tests.

--- src/test/java/problem/PancakeSortTest.java
+++ src/test/java/problem/PancakeSortTest.java
@@ -43,17 +43,17 @@ public class PancakeSortTest {
 
     @Test
     public void flip1() {
-        int[] input = new int[]{3, 2, 1, 0, 4, 5, 6};
-        int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6};
-        new PancakeSort().flip(input, 3);
+        int[] input = new int[]{0, 1, 2, 3, 4, 5, 6};
+        int[] expected = new int[]{3, 2, 1, 0, 4, 5, 6};
+        new PancakeSort().flip(input, 4);
         assertArrayEquals(expected, input);
     }
 
     @Test
     public void flip2() {
-        int[] input = new int[]{6, 5, 4, 3, 2, 1, 0};
-        int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6};
-        new PancakeSort().flip(input, 6);
+        int[] input = new int[]{0, 1, 2, 3, 4, 5, 6};
+        int[] expected = new int[]{6, 5, 4, 3, 2, 1, 0};
+        new PancakeSort().flip(input, 7);
         assertArrayEquals(expected, input);
     }
 
