7f7612ab39197f7f11463441fd88f64e9d4d8622
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-21 07:10:23 -0700

    Pancake Sort. Initial commit.

---
+++ src/main/java/problem/PancakeSort.java
@@ -0,0 +1,20 @@
+package problem;
+
+/**
+ * Pancake Sort
+ * <p>
+ * Given an array of integers arr:
+ * <p>
+ * Write a function flip(arr, k) that reverses the order of the first k elements in the array arr.
+ * Write a function pancakeSort(arr) that sorts and returns the input array. You are allowed to use only the function
+ * flip you wrote in the first step in order to make changes in the array.
+ */
+public class PancakeSort {
+    public int[] pancakeSort(int[] arr) {
+        return null;
+    }
+
+    public void flip(int[] arr, int k) {
+
+    }
+}
