296b23cf3997da06546f757256fdb31981e0560a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-21 07:13:09 -0700

    Pancake Sort. Tests.

---
+++ src/test/java/problem/PancakeSortTest.java
@@ -0,0 +1,15 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PancakeSortTest {
+
+    @Test
+    public void pancakeSort1() {
+        int[] input = new int[]{6, 0, 2, 1, 4, 3, 5};
+        int[] expected = new int[]{0, 1, 2, 3, 4, 5, 6};
+        assertArrayEquals(expected, new PancakeSort().pancakeSort(input));
+    }
+}
