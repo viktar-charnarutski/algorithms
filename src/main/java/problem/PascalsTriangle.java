76d5dc7d97cf3aec97fa7aa154bd95e600f9be03
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-21 18:29:15 -0700

    118. Pascal's Triangle. Initial commit.

---
+++ src/main/java/problem/PascalsTriangle.java
@@ -0,0 +1,15 @@
+package problem;
+
+import java.util.List;
+
+/**
+ * 118. Pascal's Triangle
+ * <p>
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ */
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        return null;
+    }
+}
