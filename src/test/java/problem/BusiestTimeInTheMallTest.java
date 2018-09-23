75e011d3cc2ae0721d0fdc227e393365d4a044e6
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-23 12:39:05 -0700

    Busiest Time in The Mall. Tests.

---
+++ src/test/java/problem/BusiestTimeInTheMallTest.java
@@ -0,0 +1,24 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BusiestTimeInTheMallTest {
+
+    @Test
+    public void findBusiestPeriod1() {
+        int[][] data = {
+                {1487799425, 14, 1},
+                {1487799425, 4, 0},
+                {1487799425, 2, 0},
+                {1487800378, 10, 1},
+                {1487801478, 18, 0},
+                {1487801478, 18, 1},
+                {1487901013, 1, 0},
+                {1487901211, 7, 1},
+                {1487901211, 7, 0}
+        };
+        assertEquals(1487801478, new BusiestTimeInTheMall().findBusiestPeriod(data));
+    }
+}
