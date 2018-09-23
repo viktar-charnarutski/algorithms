d8eb0b2e873f92601dc2d2a25886b26050d8995a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-23 12:37:14 -0700

    Busiest Time in The Mall. Initial commit.

---
+++ src/main/java/problem/BusiestTimeInTheMall.java
@@ -0,0 +1,26 @@
+package problem;
+
+/**
+ * Busiest Time in The Mall
+ * <p>
+ * The Westfield Mall management is trying to figure out what the busiest moment at the mall was last year.
+ * You’re given data extracted from the mall’s door detectors. Each data point is represented as an integer array whose
+ * size is 3. The values at indices 0, 1 and 2 are the timestamp, the count of visitors, and whether the visitors
+ * entered or exited the mall (0 for exit and 1 for entrance), respectively. Here’s an example of a data point:
+ * [ 1440084737, 4, 0 ].
+ * <p>
+ * Note that time is given in a Unix format called Epoch, which is a nonnegative integer holding the number of seconds
+ * that have elapsed since 00:00:00 UTC, Thursday, 1 January 1970.
+ * <p>
+ * Given an array, data, of data points, write a function findBusiestPeriod that returns the time at which the mall
+ * reached its busiest moment last year. The return value is the timestamp, e.g. 1480640292. Note that if there is more
+ * than one period with the same visitor peak, return the earliest one.
+ * <p>
+ * Assume that the array data is sorted in an ascending order by the timestamp. Explain your solution and analyze its
+ * time and space complexities.
+ */
+public class BusiestTimeInTheMall {
+    public int findBusiestPeriod(int[][] data) {
+        return 0;
+    }
+}
