a466edac29025824cd5cb570b56cc53660e8b480
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-23 18:40:36 -0700

    Word Count Engine. Initial commit.

---
+++ src/main/java/problem/WordCountEngine.java
@@ -0,0 +1,21 @@
+package problem;
+
+/**
+ * Word Count Engine
+ * <p>
+ * Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all
+ * unique words in it and their number of occurrences, sorted by the number of occurrences in a descending order.
+ * If two or more words have the same count, they should be sorted according to their order in the original sentence.
+ * Assume that all letters are in english alphabet. You function should be case-insensitive, so for instance, the words
+ * “Perfect” and “perfect” should be considered the same word.
+ * <p>
+ * The engine should strip out punctuation (even in the middle of a word) and use whitespaces to separate words.
+ * <p>
+ * Analyze the time and space complexities of your solution. Try to optimize for time while keeping a polynomial space
+ * complexity.
+ */
+public class WordCountEngine {
+    static String[][] wordCountEngine(String document) {
+        return null;
+    }
+}
