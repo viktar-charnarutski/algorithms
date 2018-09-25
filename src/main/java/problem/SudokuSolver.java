afa0868deb0df9e052f69ad1c53284167432c05a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-24 18:51:33 -0700

    37. Sudoku Solver. Solution.

--- src/main/java/problem/SudokuSolver.java
+++ src/main/java/problem/SudokuSolver.java
@@ -1,5 +1,8 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 37. Sudoku Solver
  * <p>
@@ -14,6 +17,56 @@ package problem;
  */
 public class SudokuSolver {
     public void solveSudoku(char[][] board) {
+        if (board == null || board.length < 9 || board[0].length < 9) {
+            throw new IllegalArgumentException();
+        }
+        solve(board);
+    }
 
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] == '.') {
+                    List<Integer> variants = variants(row, col, board);
+                    if (variants.isEmpty()) {
+                        return false;
+                    }
+                    for (int var : variants) {
+                        board[row][col] = (char) var;
+                        if (solve(board)) {
+                            return true;
+                        }
+                        board[row][col] = '.';
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> variants(int row, int col, char[][] board) {
+        List<Integer> variants = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            if (isValid((char) i, row, col, board)) {
+                variants.add(i);
+            }
+        }
+        return variants;
+    }
+
+    private boolean isValid(char num, int row, int col, char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+            if (board[row][i] == num) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
     }
 }
