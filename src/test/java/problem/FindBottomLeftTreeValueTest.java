731f490099156f5659ce2ba8d20240e2148ccc6b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-25 16:26:10 -0700

    513. Find Bottom Left Tree Value. Tests.

--- src/test/java/problem/FindBottomLeftTreeValueTest.java
+++ src/test/java/problem/FindBottomLeftTreeValueTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class FindBottomLeftTreeValueTest {
 
     @Test
-    public void findBottomLeftValue() {
+    public void findBottomLeftValue1() {
         TreeNode n1 = new TreeNode(1);
         TreeNode n2 = new TreeNode(2);
         TreeNode n3 = new TreeNode(3);
@@ -25,6 +25,19 @@ public class FindBottomLeftTreeValueTest {
 
         FindBottomLeftTreeValue ins = new FindBottomLeftTreeValue();
         assertEquals(7, ins.findBottomLeftValue(n1));
+    }
+
+    @Test
+    public void findBottomLeftValue2() {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+
+        n1.left = n2;
+        n1.right = n3;
+
+        FindBottomLeftTreeValue ins = new FindBottomLeftTreeValue();
+        assertEquals(2, ins.findBottomLeftValue(n1));
     }
 
     @Test
