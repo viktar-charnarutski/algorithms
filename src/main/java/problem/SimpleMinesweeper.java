0b1ee41585721bbd53d612482554d0736bf4ce53
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-26 15:19:59 -0700

    Simple Minesweeper. Solution.

--- src/main/java/problem/SimpleMinesweeper.java
+++ src/main/java/problem/SimpleMinesweeper.java
@@ -1,5 +1,7 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,7 +24,37 @@ import java.util.List;
  * {[3,1]}
  */
 public class SimpleMinesweeper {
+    private static int VISITED = -1;
+
     public List<List<int[]>> findBombClusters(int[][] grid) {
-        return Collections.emptyList();
+        List<List<int[]>> res = new ArrayList<>();
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                List<int[]> curr = new ArrayList<>();
+                findBombClusters(row, col, grid, curr);
+                if(!curr.isEmpty()) {
+                    res.add(curr);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void findBombClusters(int row, int col, int[][] grid, List<int[]> curr) {
+        // TODO: move the check to a separate function
+        if (row >= grid.length || row < 0
+                || col >= grid[0].length || col < 0
+                || grid[row][col] == 0
+                || grid[row][col] == VISITED) {
+            return;
+        }
+        int[] cell = new int[]{row, col};
+        curr.add(cell);
+        grid[row][col] = VISITED;
+
+        findBombClusters(row + 1, col, grid, curr);
+        findBombClusters(row - 1, col, grid, curr);
+        findBombClusters(row, col + 1, grid, curr);
+        findBombClusters(row, col - 1, grid, curr);
     }
 }
