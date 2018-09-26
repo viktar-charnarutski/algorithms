90ba62f90548a6a880aad8c1a582edb92b3925c4
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-26 07:45:34 -0700

    18. 4Sum. Solution.

--- src/main/java/problem/FourSum.java
+++ src/main/java/problem/FourSum.java
@@ -1,20 +1,50 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 18. 4Sum
- *
+ * <p>
  * Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such
  * that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
- *
+ * <p>
  * Note:
- *
+ * <p>
  * The solution set must not contain duplicate quadruplets.
  */
 public class FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        return Collections.emptyList();
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                int currSum = nums[i] + nums[j];
+                int remSum = target - currSum;
+                int start = j + 1, end = nums.length - 1;
+                while (start < end) {
+                    if (nums[start] + nums[end] == remSum) {
+                        List<Integer> currRes = new ArrayList<>();
+                        currRes.add(nums[i]);
+                        currRes.add(nums[j]);
+                        currRes.add(nums[start]);
+                        currRes.add(nums[end]);
+                        res.add(currRes);
+                        break;
+                    }
+                    if (nums[start] + nums[end] > remSum) {
+                        end--;
+                    } else {
+                        start++;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
