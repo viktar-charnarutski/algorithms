4ae7f1ca126c44084b5706195dea6c44a4b2ce0f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-29 14:05:59 -0700

    10. Regular Expression Matching. Initial commit.

---
+++ src/main/java/problem/RegularExpressionMatching.java
@@ -0,0 +1,21 @@
+package problem;
+
+/**
+ * 10. Regular Expression Matching
+ * <p>
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * <p>
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * Note:
+ * <p>
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
