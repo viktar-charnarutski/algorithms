da0e6c5455678ac9bda99e3384d6da880f8936ad
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-29 14:07:07 -0700

    10. Regular Expression Matching. Tests.

--- src/test/java/problem/RegularExpressionMatchingTest.java
+++ src/test/java/problem/RegularExpressionMatchingTest.java
@@ -10,4 +10,9 @@ public class RegularExpressionMatchingTest {
     public void isMatch1() {
         assertTrue(new RegularExpressionMatching().isMatch("aa", "a"));
     }
+
+    @Test
+    public void isMatch2() {
+        assertTrue(new RegularExpressionMatching().isMatch("aa", "a*"));
+    }
 }
