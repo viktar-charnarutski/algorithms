eec8a9a8571a4054c80ae745488fa4957b5ade8b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-28 19:17:24 -0700

    905. Sort Array By Parity. Solution.

--- src/main/java/problem/SortArrayByParity.java
+++ src/main/java/problem/SortArrayByParity.java
@@ -10,6 +10,24 @@ package problem;
  */
 public class SortArrayByParity {
     public int[] sortArrayByParity(int[] A) {
+        if (A == null || A.length <= 1) {
+            return A;
+        }
+        int i = 0;
+        while (i < A.length) {
+            if (A[i] % 2 != 0) {
+                int j = i + 1;
+                while (j < A.length && A[j] % 2 != 0) {
+                    j++;
+                }
+                if (j < A.length) {
+                    int tmp = A[i];
+                    A[i] = A[j];
+                    A[j] = tmp;
+                }
+            }
+            i++;
+        }
         return A;
     }
 }
