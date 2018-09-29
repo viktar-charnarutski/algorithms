4e9a8285393dc52ea89be298e76df6f019b74c4b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-28 19:11:48 -0700

    905. Sort Array By Parity. Initial commit.

---
+++ src/main/java/problem/SortArrayByParity.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * 905. Sort Array By Parity
+ * <p>
+ * Given an array A of non-negative integers, return an array consisting of all the even elements of A,
+ * followed by all the odd elements of A.
+ * <p>
+ * You may return any answer array that satisfies this condition.
+ */
+public class SortArrayByParity {
+    public int[] sortArrayByParity(int[] A) {
+        return A;
+    }
+}
