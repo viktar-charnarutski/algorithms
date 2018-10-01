846f8b27360aa9fd3597de6aa7c4f07f85392866
viktar-charnarutski <v.charnarutski@gmail.com>
2018-09-30 22:35:27 -0700

    10. Regular Expression Matching. Solution.

--- src/main/java/problem/RegularExpressionMatching.java
+++ src/main/java/problem/RegularExpressionMatching.java
@@ -16,6 +16,38 @@ package problem;
  */
 public class RegularExpressionMatching {
     public boolean isMatch(String s, String p) {
+        return isMatch(s, p, 0, 0);
+    }
+
+    private boolean isMatch(String s, String p, int si, int pi) {
+        if (si >= s.length()) {
+            if (pi >= p.length()) {
+                return true;
+            } else {
+                if (pi + 1 < p.length() && p.charAt(pi + 1) == '*') {
+                    return isMatch(s, p, si, pi + 2);
+                } else {
+                    return false;
+                }
+            }
+        } else if (pi >= p.length()) {
+            return false;
+        }
+
+        // abc, ab*c || abcd, ab*xcd
+        if (pi + 1 < p.length() && p.charAt(pi + 1) == '*') {
+            if (p.charAt(pi) == '.' || s.charAt(si) == p.charAt(pi)) {
+                return isMatch(s, p, si, pi + 2) || isMatch(s, p, si + 1, pi);
+            } else {
+                return isMatch(s, p, si, pi + 2);
+            }
+        }
+
+        // abc, abc || abc, a.c
+        if (s.charAt(si) == p.charAt(pi) || p.charAt(pi) == '.' || p.charAt(pi) == '*') {
+            return isMatch(s, p, si + 1, pi + 1);
+        }
+
         return false;
     }
 }
