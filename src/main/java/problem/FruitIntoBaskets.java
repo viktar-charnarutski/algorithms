672eb0a603e456202af918283d35890e45b3137c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-01 10:37:53 -0700

    904. Fruit Into Baskets. Solution.

--- src/main/java/problem/FruitIntoBaskets.java
+++ src/main/java/problem/FruitIntoBaskets.java
@@ -1,5 +1,8 @@
 package problem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 904. Fruit Into Baskets
  * <p>
@@ -19,6 +22,19 @@ package problem;
  */
 public class FruitIntoBaskets {
     public int totalFruit(int[] tree) {
-        return -1;
+        Map<Integer, Integer> count = new HashMap<>();
+        int total = 0;
+        for (int start = 0, end = 0; end < tree.length; end++) {
+            count.put(tree[end], count.getOrDefault(tree[end], 0) + 1);
+            while (count.size() > 2) {
+                count.put(tree[start], count.get(tree[start]) - 1);
+                if (count.get(tree[start]) == 0) {
+                    count.remove(tree[start]);
+                }
+                start++;
+            }
+            total = Math.max(end - start + 1, total);
+        }
+        return total;
     }
 }
