fc906eda4cca23f1afcfaf9b8c6097c2e8939f48
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-02 07:59:43 -0700

    906. Super Palindromes. Initial commit.

---
+++ src/main/java/problem/SuperPalindromes.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * 906. Super Palindromes
+ * <p>
+ * Let's say a positive integer is a superpalindrome if it is a palindrome, and it is also the square of a palindrome.
+ * <p>
+ * Now, given two positive integers L and R (represented as strings), return the number of superpalindromes in the
+ * inclusive range [L, R].
+ */
+public class SuperPalindromes {
+    public int superpalindromesInRange(String L, String R) {
+        return -1;
+    }
+}
