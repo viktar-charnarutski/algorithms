376ac12dc16c678d7d529d1f06a400ce970f1658
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-02 07:47:43 -0700

    907. Sum of Subarray Minimums. Solution.

--- src/main/java/problem/SumOfSubarrayMinimums.java
+++ src/main/java/problem/SumOfSubarrayMinimums.java
@@ -1,5 +1,10 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * 907. Sum of Subarray Minimums
  * <p>
@@ -9,6 +14,36 @@ package problem;
  */
 public class SumOfSubarrayMinimums {
     public int sumSubarrayMins(int[] A) {
-        return -1;
+        if (A == null || A.length == 0) return 0;
+        int res = 0;
+
+        int[] left = new int[A.length];
+        Stack<int[]> stack = new Stack<>();
+
+        for (int i = 0; i < A.length; i++) {
+            int count = 1;
+            while (!stack.isEmpty() && stack.peek()[0] > A[i]) {
+                count += stack.pop()[1];
+            }
+            stack.push(new int[]{A[i], count});
+            left[i] = count;
+        }
+
+        int[] right = new int[A.length];
+        stack.clear();
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            int count = 1;
+            while (!stack.isEmpty() && stack.peek()[0] >= A[i]) {
+                count += stack.pop()[1];
+            }
+            stack.push(new int[]{A[i], count});
+            right[i] = count;
+        }
+        for (int i = 0; i < left.length; i++) {
+            res += A[i] * left[i] * right[i];
+            res %= (int) 1e9 + 7;
+        }
+        return res;
     }
 }
