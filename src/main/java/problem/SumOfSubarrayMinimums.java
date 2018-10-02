06a9218fffaa65354b3aa89e6bacba2f50cc31a5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-01 18:18:10 -0700

    907. Sum of Subarray Minimums. Initial commit.

---
+++ src/main/java/problem/SumOfSubarrayMinimums.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * 907. Sum of Subarray Minimums
+ * <p>
+ * Given an array of integers A, find the sum of min(B), where B ranges over every (contiguous) subarray of A.
+ * <p>
+ * Since the answer may be large, return the answer modulo 10^9 + 7.
+ */
+public class SumOfSubarrayMinimums {
+    public int sumSubarrayMins(int[] A) {
+        return -1;
+    }
+}
