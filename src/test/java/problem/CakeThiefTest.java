3db8189595c70fc5a410d6fcfb0c2bbd75e29b9d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-02 23:31:20 -0700

    Cake Thief. Tests.

--- src/test/java/problem/CakeThiefTest.java
+++ src/test/java/problem/CakeThiefTest.java
@@ -2,17 +2,30 @@ package problem;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class CakeThiefTest {
 
     @Test
-    public void maxDuffelBagValue() {
+    public void maxDuffelBagValue1() {
         CakeThief.CakeType[] cakes = {
+                new CakeThief.CakeType(2, 15),
                 new CakeThief.CakeType(7, 160),
-                new CakeThief.CakeType(3, 90),
-                new CakeThief.CakeType(2, 15)
+                new CakeThief.CakeType(3, 90)
         };
-        assertEquals(555, CakeThief.maxDuffelBagValue(cakes, 20));
+        long actual = CakeThief.maxDuffelBagValue(cakes, 20);
+        assertEquals(555, actual);
+    }
+
+    @Test
+    public void maxDuffelBagValue2() {
+        CakeThief.CakeType[] cakes = {
+                new CakeThief.CakeType(3, 40),
+                new CakeThief.CakeType(5, 70)
+        };
+        long actual = CakeThief.maxDuffelBagValue(cakes, 8);
+        assertEquals(110, actual);
     }
 }
