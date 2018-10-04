1bbe9af26595241085eb1e9e557b3174b6542952
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-04 15:49:35 -0700

    263. Ugly Number. Initial commit.

---
+++ src/main/java/problem/UglyNumber.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * 263. Ugly Number
+ *
+ * Write a program to check whether a given number is an ugly number.
+ *
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        return false;
+    }
+}
