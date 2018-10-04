d2e1e9135c8c2ccf89505f1fa1601f916cb4fcbc
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-04 16:18:27 -0700

    Fraudulent Activity Notifications. Initial commit.

---
+++ src/main/java/problem/FraudulentActivityNotifications.java
@@ -0,0 +1,18 @@
+package problem;
+
+/**
+ * Fraudulent Activity Notifications
+ * <p>
+ * HackerLand National Bank has a simple policy for warning clients about possible fraudulent account activity.
+ * If the amount spent by a client on a particular day is greater than or equal to 2x the client's median spending for a
+ * trailing number of days, they send the client a notification about potential fraud. The bank doesn't send the client
+ * any notifications until they have at least that trailing number of prior days' transaction data.
+ * <p>
+ * Given the number of trailing days d and a client's total daily expenditures for a period of n days, find and print the
+ * number of times the client will receive a notification over all n days.
+ */
+public class FraudulentActivityNotifications {
+    static int activityNotifications(int[] expenditure, int d) {
+        return -1;
+    }
+}
