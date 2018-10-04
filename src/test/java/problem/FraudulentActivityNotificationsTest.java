6678bce0720a97f429ac802e972ee0110a8e25c9
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-04 16:24:30 -0700

    Fraudulent Activity Notifications. Tests.

--- src/test/java/problem/FraudulentActivityNotificationsTest.java
+++ src/test/java/problem/FraudulentActivityNotificationsTest.java
@@ -10,4 +10,9 @@ public class FraudulentActivityNotificationsTest {
     public void activityNotifications() {
         assertEquals(2, FraudulentActivityNotifications.activityNotifications(new int[]{2, 3, 4, 2, 3, 6, 8, 4, 5}, 5));
     }
+
+    @Test
+    public void median() {
+        assertEquals(3, FraudulentActivityNotifications.median(new int[]{2, 3, 4, 2, 3, 6, 8, 4, 5}, 0, 5));
+    }
 }
