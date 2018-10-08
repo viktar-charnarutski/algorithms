c04b52a8b7d95ed3d46ed4940126737e96c2c45a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-08 08:06:16 -0700

    Lily's Homework. Initial commit.

---
+++ src/main/java/problem/LilysHomework.java
@@ -0,0 +1,19 @@
+package problem;
+
+/**
+ * Lily's Homework
+ * <p>
+ * Whenever George asks Lily to hang out, she's busy doing homework. George wants to help her finish it faster, but he's
+ * in over his head! Can you help George understand Lily's homework so she can hang out with him?
+ * <p>
+ * Consider an array of n distinct integers. George can swap any two elements of the array any number of times. An
+ * array is beautiful if the sum of |arr[i] - arr[i - 1]| among 0 < i < n is minimal.
+ * <p>
+ * Given the array arr, determine and return the minimum number of swaps that should be performed in order to make the
+ * array beautiful.
+ */
+public class LilysHomework {
+    static int lilysHomework(int[] arr) {
+        return -1;
+    }
+}
