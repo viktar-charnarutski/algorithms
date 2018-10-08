a7515d511b2960ed12da69be7cf9fe174c78326a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-08 08:08:18 -0700

    Lily's Homework. Tests.

--- src/test/java/problem/LilysHomeworkTest.java
+++ src/test/java/problem/LilysHomeworkTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.*;
 public class LilysHomeworkTest {
 
     @Test
-    public void lilysHomework() {
+    public void lilysHomework1() {
+        assertEquals(2, LilysHomework.lilysHomework(new int[]{7, 15, 12, 3}));
+    }
+
+    @Test
+    public void lilysHomework2() {
         assertEquals(2, LilysHomework.lilysHomework(new int[]{2, 5, 3, 1}));
     }
 }
