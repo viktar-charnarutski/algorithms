4acfbab4cf2e55f0d1cd0b4b2725bbdb20bb7307
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-09 18:48:36 -0700

    Pseudo-Isomorphic Substrings. Tests.

--- src/test/java/problem/PseudoIsomorphicSubstringsTest.java
+++ src/test/java/problem/PseudoIsomorphicSubstringsTest.java
@@ -2,12 +2,26 @@ package problem;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class PseudoIsomorphicSubstringsTest {
 
     @Test
-    public void pseudoIsomorphicSubstrings() {
-        assertArrayEquals(new int[]{1, 2, 4, 6, 9, 12, 15}, PseudoIsomorphicSubstrings.pseudoIsomorphicSubstrings("abbabab"));
+    public void pseudoIsomorphicSubstrings1() {
+        assertArrayEquals(new int[]{1, 2, 3}, PseudoIsomorphicSubstrings.pseudoIsomorphicSubstrings("abc"));
+    }
+
+    @Test
+    public void pseudoIsomorphicSubstrings2() {
+        int[] res = PseudoIsomorphicSubstrings.pseudoIsomorphicSubstrings("abbabab");
+        System.out.println(Arrays.toString(res));
+        assertArrayEquals(new int[]{1, 2, 4, 6, 9, 12, 15}, res);
+    }
+
+    @Test
+    public void hash() {
+        assertEquals("0:3:5-1:2:4:6-1:2:4:6-0:3:5-1:2:4:6-0:3:5-1:2:4:6-", PseudoIsomorphicSubstrings.hash("abbabab"));
     }
 }
