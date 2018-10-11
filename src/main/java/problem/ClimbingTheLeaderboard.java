5f771da97097c02d2805a4a87797ec782d21b9a3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-11 16:33:47 -0700

    Climbing the Leaderboard. Initial commit.

---
+++ src/main/java/problem/ClimbingTheLeaderboard.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * Climbing the Leaderboard
+ * <p>
+ * Alice is playing an arcade game and wants to climb to the top of the leaderboard and wants to track her ranking.
+ * The game uses Dense Ranking, so its leaderboard works like this:
+ * <p>
+ * The player with the highest score is ranked number 1 on the leaderboard.
+ * Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately
+ * following ranking number.
+ */
+public class ClimbingTheLeaderboard {
+    static int[] climbingLeaderboard(int[] scores, int[] alice) {
+        return new int[0];
+    }
+}
