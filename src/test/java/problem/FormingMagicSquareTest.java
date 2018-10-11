607cd7d7e12711e3b067594383fab0dfffbd593b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-10 19:24:56 -0700

    Forming a Magic Square. Tests.

--- src/test/java/problem/FormingMagicSquareTest.java
+++ src/test/java/problem/FormingMagicSquareTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.*;
 public class FormingMagicSquareTest {
 
     @Test
-    public void formingMagicSquare() {
+    public void formingMagicSquare1() {
         assertEquals(7, FormingMagicSquare.formingMagicSquare(new int[][]{{5, 3, 4}, {1, 5, 8}, {6, 4, 2}}));
+    }
+
+    @Test
+    public void formingMagicSquare2() {
+        assertEquals(7, FormingMagicSquare.formingMagicSquare(new int[][]{{4, 9, 2}, {3, 5, 7}, {8, 1, 5}}));
     }
 }
