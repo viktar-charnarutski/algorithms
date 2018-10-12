e80c37f4213b33c9b4f4fec20f6090749eaaeeac
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-12 10:12:28 -0700

    Climbing the Leaderboard. Tests.

--- src/test/java/problem/ClimbingTheLeaderboardTest.java
+++ src/test/java/problem/ClimbingTheLeaderboardTest.java
@@ -7,8 +7,13 @@ import static org.junit.Assert.*;
 public class ClimbingTheLeaderboardTest {
 
     @Test
-    public void climbingLeaderboard() {
+    public void climbingLeaderboard1() {
         assertArrayEquals(new int[]{6, 4, 2, 1},
                 ClimbingTheLeaderboard.climbingLeaderboard(new int[]{100, 100, 50, 40, 40, 20, 10}, new int[]{5, 25, 50, 120}));
+    }
+    @Test
+    public void climbingLeaderboard2() {
+        assertArrayEquals(new int[]{1},
+                ClimbingTheLeaderboard.climbingLeaderboard(new int[0], new int[]{5, 25, 50, 120}));
     }
 }
