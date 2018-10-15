8152078feef8c343ce7d4ed5a9977849abd02d2e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-15 14:49:49 -0700

    Non-Divisible Subset. Initial commit.

---
+++ src/main/java/problem/NonDivisibleSubset.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * Non-Divisible Subset
+ * <p>
+ * Given a set of distinct integers, print the size of a maximal subset of S where the sum of any 2 numbers in S' is not
+ * evenly divisible by k.
+ */
+public class NonDivisibleSubset {
+    static int nonDivisibleSubset(int k, int[] S) {
+        return 0;
+    }
+}
