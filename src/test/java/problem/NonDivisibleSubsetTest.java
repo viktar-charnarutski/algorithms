883554e36c23205237c15da6df4b1dc07ec0670a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-15 15:28:59 -0700

    Non-Divisible Subset. Tests.

--- src/test/java/problem/NonDivisibleSubsetTest.java
+++ src/test/java/problem/NonDivisibleSubsetTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.*;
 public class NonDivisibleSubsetTest {
 
     @Test
-    public void nonDivisibleSubset() {
-        assertEquals(3, NonDivisibleSubset.nonDivisibleSubset(3, new int[] {1, 7, 2, 4}));
+    public void nonDivisibleSubset1() {
+        assertEquals(3, NonDivisibleSubset.nonDivisibleSubset(3, new int[]{1, 7, 2, 4}));
+    }
+
+    @Test
+    public void nonDivisibleSubset2() {
+        assertEquals(11, NonDivisibleSubset.nonDivisibleSubset(7, new int[]{278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436}));
     }
 }
