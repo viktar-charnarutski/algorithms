1635c28c0bf36256e7ca76641fa221a79068185b
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-15 15:29:16 -0700

    Non-Divisible Subset. Solution.

--- src/main/java/problem/NonDivisibleSubset.java
+++ src/main/java/problem/NonDivisibleSubset.java
@@ -8,6 +8,17 @@ package problem;
  */
 public class NonDivisibleSubset {
     static int nonDivisibleSubset(int k, int[] S) {
-        return 0;
+        int maxLength = 0;
+        for (int i = 0; i < S.length; i++) {
+            int currSum = S[i], currLength = 0;
+            for (int j = i + 1; j < S.length; j++) {
+                if (currSum + S[j] % k != 0) {
+                    currSum += S[j];
+                    currLength++;
+                }
+            }
+            maxLength = Math.max(currLength, maxLength);
+        }
+        return maxLength;
     }
 }
