30c2bdf9a4919483ec19110dcdfbca9e898f8030
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-15 20:16:29 -0700

    Non-Divisible Subset. Solution.

--- src/main/java/problem/NonDivisibleSubset.java
+++ src/main/java/problem/NonDivisibleSubset.java
@@ -8,14 +8,23 @@ package problem;
  */
 public class NonDivisibleSubset {
     static int nonDivisibleSubset(int k, int[] S) {
-        int[] rems = new int[k + 1];
+        int[] rems = new int[k];
         for (int value : S) {
             rems[value % k]++;
         }
-        int maxLength = 1;
-        for (int i = 1; i < rems.length / 2; i++) {
+
+        // Handle edge-cases:
+        // 1. if there are several numbers dividable by k (remain is 0), we can take only one of them.
+        int maxLength = rems[0] > 0 ? 1 : 0;
+        // 2. if k-number is even, we can take only one number with k / 2 as a remain.
+        maxLength += k % 2 == 0 && rems[k / 2] > 0 ? 1 : 0;
+
+        // Iterate only the first half of the rems array.
+        int n = k % 2 == 0 ? k / 2 : k / 2 + 1;
+        for (int i = 1; i < n; i++) {
             maxLength += Math.max(rems[i], rems[k - i]);
         }
+
         return maxLength;
     }
 }
