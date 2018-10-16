453de6e795e98d1541ee723448dc4715b72c860e
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-16 15:53:21 -0700

    Bigger is Greater. Initial commit.

---
+++ src/main/java/problem/BiggerIsGreater.java
@@ -0,0 +1,18 @@
+package problem;
+
+/**
+ * Bigger is Greater
+ * <p>
+ * Lexicographical order is often known as alphabetical order when dealing with strings. A string is greater than
+ * another string if it comes later in a lexicographically sorted list.
+ * <p>
+ * Given a word, create a new word by swapping some or all of its characters. This new word must meet two criteria:
+ * <p>
+ * 1. It must be greater than the original word
+ * 2. It must be the smallest word that meets the first condition
+ */
+public class BiggerIsGreater {
+    static String biggerIsGreater(String w) {
+        return "";
+    }
+}
