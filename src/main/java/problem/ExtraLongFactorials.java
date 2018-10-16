212d1c3ff830f5b30214a559bd161a42f9ce41e8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-16 07:50:37 -0700

    Extra Long Factorials. Initial commit.

---
+++ src/main/java/problem/ExtraLongFactorials.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * Extra Long Factorials
+ * <p>
+ * The factorial of the integer n, written n!, is defined as:
+ * <p>
+ * f(n) = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+ *
+ * Calculate and print the factorial of a given integer.
+ */
+public class ExtraLongFactorials {
+    static void extraLongFactorials(int n) {
+
+    }
+}
