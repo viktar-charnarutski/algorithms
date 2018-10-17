9829198e472c8582d84deeb05e657d242229b93c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-17 08:06:11 -0700

    The Grid Search. Tests.

--- src/test/java/problem/TheGridSearchTest.java
+++ src/test/java/problem/TheGridSearchTest.java
@@ -20,4 +20,25 @@ public class TheGridSearchTest {
 
         assertEquals("YES", TheGridSearch.gridSearch(grid, pattern));
     }
+
+    @Test
+    public void gridSearch2() {
+        String[] grid = {"7283455864",
+                "6731158619",
+                "8988242643",
+                "3830589324",
+                "2229505813",
+                "5633845374",
+                "6473530293",
+                "7053106601",
+                "0834282956",
+                "4607924137" };
+
+        String[] pattern = {"9505",
+                "3845",
+                "3530" };
+
+        assertEquals("YES", TheGridSearch.gridSearch(grid, pattern));
+    }
+
 }
