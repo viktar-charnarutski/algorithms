1fec87ff42814361fdb23d88d95fa8106e2409a5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-17 07:59:36 -0700

    The Grid Search. Initial commit.

---
+++ src/main/java/problem/TheGridSearch.java
@@ -0,0 +1,28 @@
+package problem;
+
+/**
+ * The Grid Search.
+ * <p>
+ * Given a 2D array of digits or grid, try to find the occurrence of a given 2D pattern of digits. For example,
+ * consider the following grid:
+ * <p>
+ * 1234567890
+ * 0987654321
+ * 1111111111
+ * 1111111111
+ * 2222222222
+ * <p>
+ * Assume we need to look for the following 2D pattern array:
+ * <p>
+ * 876543
+ * 111111
+ * 111111
+ * <p>
+ * The 2D pattern begins at the second row and the third column of the grid. The pattern is said to be present in the
+ * grid.
+ */
+public class TheGridSearch {
+    static String gridSearch(String[] G, String[] P) {
+        return "";
+    }
+}
