a3f534bb2778d9de16bfd73a89eeef310a8384b5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-17 12:21:37 -0700

    264. Ugly Number II. Initial commit.

---
+++ src/main/java/problem/UglyNumberII.java
@@ -0,0 +1,14 @@
+package problem;
+
+/**
+ * 264. Ugly Number II
+ * <p>
+ * Write a program to find the n-th ugly number.
+ * <p>
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ */
+public class UglyNumberII {
+    public int nthUglyNumber(int n) {
+        return -1;
+    }
+}
