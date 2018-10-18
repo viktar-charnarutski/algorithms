e1919da3d2bce74e2a3b3e89b48e56aa50a6d5ae
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-17 18:31:13 -0700

    264. Ugly Number II. Tests.

--- src/test/java/problem/UglyNumberIITest.java
+++ src/test/java/problem/UglyNumberIITest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.*;
 public class UglyNumberIITest {
 
     @Test
-    public void nthUglyNumber() {
+    public void nthUglyNumber1() {
         assertEquals(12, new UglyNumberII().nthUglyNumber(10));
+    }
+
+    @Test
+    public void nthUglyNumber2() {
+        assertEquals(15, new UglyNumberII().nthUglyNumber(11));
     }
 }
