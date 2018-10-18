f8de6deeba534060e534f936d180864a99df4887
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-18 16:00:22 -0700

    890. Find and Replace Pattern. Tests.

--- src/test/java/problem/FindAndReplacePatternTest.java
+++ src/test/java/problem/FindAndReplacePatternTest.java
@@ -9,7 +9,13 @@ import static org.junit.Assert.*;
 public class FindAndReplacePatternTest {
 
     @Test
-    public void findAndReplacePattern() {
+    public void findAndReplacePattern1() {
         assertEquals(Arrays.asList("mee", "aqq"), new FindAndReplacePattern().findAndReplacePattern(new String[]{"abc", "deq", "mee", "aqq", "dkd", "ccc" }, "abb"));
+    }
+
+    @Test
+    public void findAndReplacePattern2() {
+        assertEquals(Arrays.asList("abab", "dede"), new FindAndReplacePattern().findAndReplacePattern(new String[]{"badc", "abab", "dddd", "dede", "yyxx" }, "baba"))
+        ;
     }
 }
