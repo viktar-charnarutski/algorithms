99a4219392d0dd89d235f99d7c4cf1ed5ef5727a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-17 19:08:32 -0700

    890. Find and Replace Pattern. Initial commit.

---
+++ src/main/java/problem/FindAndReplacePattern.java
@@ -0,0 +1,25 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 890. Find and Replace Pattern
+ * <p>
+ * You have a list of words and a pattern, and you want to know which words in words matches the pattern.
+ * <p>
+ * A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the
+ * pattern with p(x), we get the desired word.
+ * <p>
+ * (Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter,
+ * and no two letters map to the same letter.)
+ * <p>
+ * Return a list of the words in words that match the given pattern.
+ * <p>
+ * You may return the answer in any order.
+ */
+public class FindAndReplacePattern {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        return Collections.emptyList();
+    }
+}
