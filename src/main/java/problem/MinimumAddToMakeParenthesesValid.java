4ba7cee923f327ca45f6080ec7205fe48f0ee621
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-17 18:52:52 -0700

    921. Minimum Add to Make Parentheses Valid. Initial commit.

---
+++ src/main/java/problem/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,20 @@
+package problem;
+
+/**
+ * 921. Minimum Add to Make Parentheses Valid
+ * <p>
+ * Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any
+ * positions ) so that the resulting parentheses string is valid.
+ * <p>
+ * Formally, a parentheses string is valid if and only if:
+ * <p>
+ * It is the empty string, or
+ * It can be written as AB (A concatenated with B), where A and B are valid strings, or
+ * It can be written as (A), where A is a valid string.
+ * Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+ */
+public class MinimumAddToMakeParenthesesValid {
+    public int minAddToMakeValid(String S) {
+        return 0;
+    }
+}
