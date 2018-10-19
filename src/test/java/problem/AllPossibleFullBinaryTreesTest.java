aad905d2bf96aa10eb368e1c5ea09be7be6f34f4
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-19 08:02:58 -0700

    894. All Possible Full Binary Trees. Tests.

--- src/test/java/problem/AllPossibleFullBinaryTreesTest.java
+++ src/test/java/problem/AllPossibleFullBinaryTreesTest.java
@@ -7,12 +7,18 @@ import static org.junit.Assert.*;
 public class AllPossibleFullBinaryTreesTest {
 
     @Test
-    public void allPossibleFBT() {
+    public void allPossibleFBT1() {
         TreeNode n1 = new TreeNode(0);
         TreeNode n2 = new TreeNode(0);
         TreeNode n3 = new TreeNode(0);
         n1.left = n2;
         n1.right = n3;
         assertEquals(n1, new AllPossibleFullBinaryTrees().allPossibleFBT(3));
+    }
+
+    @Test
+    public void allPossibleFBT2() {
+        TreeNode n1 = new TreeNode(0);
+        assertEquals(n1, new AllPossibleFullBinaryTrees().allPossibleFBT(1));
     }
 }
