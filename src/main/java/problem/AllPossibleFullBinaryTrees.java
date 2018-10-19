0a303f200c5ebe464bcc8cc0293c38b861fc0ba2
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-19 07:59:59 -0700

    894. All Possible Full Binary Trees. Initial commit.

---
+++ src/main/java/problem/AllPossibleFullBinaryTrees.java
@@ -0,0 +1,22 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 894. All Possible Full Binary Trees
+ * <p>
+ * A full binary tree is a binary tree where each node has exactly 0 or 2 children.
+ * <p>
+ * Return a list of all possible full binary trees with N nodes.  Each element of the answer is the root node of one
+ * possible tree.
+ * <p>
+ * Each node of each tree in the answer must have node.val = 0.
+ * <p>
+ * You may return the final list of trees in any order.
+ */
+public class AllPossibleFullBinaryTrees {
+    public List<TreeNode> allPossibleFBT(int N) {
+        return Collections.emptyList();
+    }
+}
