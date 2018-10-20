39ef66bc825779f4d1c8956b5f67a50a9ab87cb5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-19 18:37:03 -0700

    338. Counting Bits. Initial commit.

---
+++ src/main/java/problem/CountingBits.java
@@ -0,0 +1,22 @@
+package problem;
+
+/**
+ * 338. Counting Bits
+ * <p>
+ * Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's
+ * in their binary representation and return them as an array.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 2
+ * Output: [0,1,1]
+ * Example 2:
+ * <p>
+ * Input: 5
+ * Output: [0,1,1,2,1,2]
+ */
+public class CountingBits {
+    public int[] countBits(int num) {
+        return new int[0];
+    }
+}
