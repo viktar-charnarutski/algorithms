5b1af5bcad4302a0544e45918f09618bd3bbdc06
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-20 16:41:20 -0700

    406. Queue Reconstruction by Height. Tests.

--- src/test/java/problem/QueueReconstructionByHeightTest.java
+++ src/test/java/problem/QueueReconstructionByHeightTest.java
@@ -11,7 +11,10 @@ public class QueueReconstructionByHeightTest {
     @Test
     public void reconstructQueue() {
         int[][] people = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        int[][] expected = {{5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}};
         int[][] result = new QueueReconstructionByHeight().reconstructQueue(people);
-        assertTrue(Arrays.deepEquals(people, result));
+        for (int i = 0; i < people.length; i++) {
+            assertArrayEquals(expected[i], result[i]);
+        }
     }
 }
