40135970b863dda1fa7984c1f93815ada14afb24
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-19 18:38:35 -0700

    338. Counting Bits. Tests.

--- src/test/java/problem/CountingBitsTest.java
+++ src/test/java/problem/CountingBitsTest.java
@@ -8,6 +8,11 @@ public class CountingBitsTest {
 
     @Test
     public void countBits1() {
-        assertArrayEquals(new int[]{0,1,1}, new CountingBits().countBits(2));
+        assertArrayEquals(new int[]{0, 1, 1}, new CountingBits().countBits(2));
+    }
+
+    @Test
+    public void countBits2() {
+        assertArrayEquals(new int[]{0, 1, 1, 2, 1, 2}, new CountingBits().countBits(5));
     }
 }
