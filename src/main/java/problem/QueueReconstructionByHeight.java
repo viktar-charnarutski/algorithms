f8778111326922fcffd930a76d9c24ca6a75b21c
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-20 16:30:18 -0700

    406. Queue Reconstruction by Height. Initial commit.

---
+++ src/main/java/problem/QueueReconstructionByHeight.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 406. Queue Reconstruction by Height
+ * <p>
+ * Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k),
+ * where h is the height of the person and k is the number of people in front of this person who have a height greater
+ * than or equal to h. Write an algorithm to reconstruct the queue.
+ * <p>
+ * Note:
+ * The number of people is less than 1,100.
+ */
+public class QueueReconstructionByHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        return new int[0][0];
+    }
+}
