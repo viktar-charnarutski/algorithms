ac49eae00c3a4fb0f961812f8234e4ef4839cf95
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-21 15:46:04 -0700

    856. Score of Parentheses. Initial commit.

---
+++ src/main/java/problem/ScoreOfParentheses.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * 856. Score of Parentheses
+ * <p>
+ * Given a balanced parentheses string S, compute the score of the string based on the following rule:
+ * <p>
+ * () has score 1
+ * AB has score A + B, where A and B are balanced parentheses strings.
+ * (A) has score 2 * A, where A is a balanced parentheses string.
+ */
+public class ScoreOfParentheses {
+    public int scoreOfParentheses(String S) {
+        return 0;
+    }
+}
