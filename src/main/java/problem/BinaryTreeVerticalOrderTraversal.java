8b269cd34a4aeac86f8aa4ac86a537e4719b4dcf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-22 07:49:02 -0700

    Binary Tree Vertical Order Traversal. Initial commit.

---
+++ src/main/java/problem/BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,16 @@
+package problem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Binary Tree Vertical Order Traversal
+ * <p>
+ * Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by
+ * column).
+ */
+public class BinaryTreeVerticalOrderTraversal {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        return Collections.emptyList();
+    }
+}
