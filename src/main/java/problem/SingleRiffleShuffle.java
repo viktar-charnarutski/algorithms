67ffa1412f5b67ef809a0a9a3a9a888b03c915dd
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-23 19:07:56 -0700

    Single Riffle Shuffle. Initial commit.

---
+++ src/main/java/problem/SingleRiffleShuffle.java
@@ -0,0 +1,12 @@
+package problem;
+
+/**
+ * Single Riffle Shuffle.
+ * <p>
+ * I figured out how to get rich: online poker.
+ */
+public class SingleRiffleShuffle {
+    public static boolean isSingleRiffle(int[] half1, int[] half2, int[] shuffledDeck) {
+        return false;
+    }
+}
