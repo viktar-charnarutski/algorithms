3c70246b1b8054587a62ffc651033df04b7a8fef
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-24 08:13:45 -0700

    925. Long Pressed Name. Initial commit.

---
+++ src/main/java/problem/LongPressedName.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * 925. Long Pressed Name
+ * <p>
+ * Your friend is typing his name into a keyboard.  Sometimes, when typing a character c, the key might get long
+ * pressed, and the character will be typed 1 or more times.
+ * <p>
+ * You examine the typed characters of the keyboard.  Return True if it is possible that it was your friends name, with
+ * some characters (possibly none) being long pressed.
+ */
+public class LongPressedName {
+    public boolean isLongPressedName(String name, String typed) {
+        return false;
+    }
+}
