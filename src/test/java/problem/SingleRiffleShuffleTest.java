11da8183229d73c2ba3dcbb9bf3dd2aa311ea824
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-23 19:17:38 -0700

    Single Riffle Shuffle. Tests.

--- src/test/java/problem/SingleRiffleShuffleTest.java
+++ src/test/java/problem/SingleRiffleShuffleTest.java
@@ -7,10 +7,18 @@ import static org.junit.Assert.*;
 public class SingleRiffleShuffleTest {
 
     @Test
-    public void isSingleRiffle() {
+    public void isSingleRiffle1() {
         int[] half1 = {3, 4, 5, 6};
         int[] half2 = {7, 8, 9, 10};
         int[] shuffledDeck = {3, 4, 7, 8, 9, 5, 10, 6};
         assertTrue(SingleRiffleShuffle.isSingleRiffle(half1, half2, shuffledDeck));
+    }
+
+    @Test
+    public void isSingleRiffle2() {
+        int[] half1 = {3, 4, 5, 6};
+        int[] half2 = {7, 8, 9, 10};
+        int[] shuffledDeck = {4, 3, 7, 8, 9, 5, 10, 6};
+        assertFalse(SingleRiffleShuffle.isSingleRiffle(half1, half2, shuffledDeck));
     }
 }
