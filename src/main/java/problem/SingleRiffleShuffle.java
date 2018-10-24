fb4594dbb5d66506930b16bc96ee005b28486504
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-23 19:16:38 -0700

    Single Riffle Shuffle. Solution.

--- src/main/java/problem/SingleRiffleShuffle.java
+++ src/main/java/problem/SingleRiffleShuffle.java
@@ -1,5 +1,7 @@
 package problem;
 
+import java.util.Arrays;
+
 /**
  * Single Riffle Shuffle.
  * <p>
@@ -7,6 +9,17 @@ package problem;
  */
 public class SingleRiffleShuffle {
     public static boolean isSingleRiffle(int[] half1, int[] half2, int[] shuffledDeck) {
-        return false;
+        if (shuffledDeck.length == 0) return true;
+        if (half1.length > 0 && half1[0] == shuffledDeck[0]) {
+            return isSingleRiffle(slicedArray(half1), half2, slicedArray(shuffledDeck));
+        } else if (half2.length > 0 && half2[0] == shuffledDeck[0]) {
+            return isSingleRiffle(half1, slicedArray(half2), slicedArray(shuffledDeck));
+        } else {
+            return false;
+        }
+    }
+
+    private static int[] slicedArray(int[] deck) {
+        return Arrays.copyOfRange(deck, 1, deck.length);
     }
 }
