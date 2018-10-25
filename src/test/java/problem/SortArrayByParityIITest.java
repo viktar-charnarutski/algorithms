74d4a6f446ef6442f9138f6fc7610559a0a594f6
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-25 08:04:30 -0700

    922. Sort Array By Parity II. Tests.

--- src/test/java/problem/SortArrayByParityIITest.java
+++ src/test/java/problem/SortArrayByParityIITest.java
@@ -8,6 +8,6 @@ public class SortArrayByParityIITest {
 
     @Test
     public void sortArrayByParityII() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, new SortArrayByParityII().sortArrayByParityII(new int[]{1, 3, 5, 2, 4, 6}));
+        assertArrayEquals(new int[]{2, 3, 4, 5, 6}, new SortArrayByParityII().sortArrayByParityII(new int[]{3, 5, 2, 4, 6}));
     }
 }
