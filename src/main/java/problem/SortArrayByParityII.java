c85e86a764564db856061825bea9336845269e18
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-25 07:56:11 -0700

    922. Sort Array By Parity II. Initial commit.

---
+++ src/main/java/problem/SortArrayByParityII.java
@@ -0,0 +1,16 @@
+package problem;
+
+/**
+ * 922. Sort Array By Parity II
+ * <p>
+ * Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
+ * <p>
+ * Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+ * <p>
+ * You may return any answer array that satisfies this condition.
+ */
+public class SortArrayByParityII {
+    public int[] sortArrayByParityII(int[] A) {
+        return new int[A.length];
+    }
+}
