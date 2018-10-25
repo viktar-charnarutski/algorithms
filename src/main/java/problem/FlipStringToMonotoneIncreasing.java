a6edabf630949f12eeee67bafc42275f28545da5
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-24 18:42:33 -0700

    926. Flip String to Monotone Increasing. Initial commit.

---
+++ src/main/java/problem/FlipStringToMonotoneIncreasing.java
@@ -0,0 +1,17 @@
+package problem;
+
+/**
+ * 926. Flip String to Monotone Increasing
+ * <p>
+ * A string of '0's and '1's is monotone increasing if it consists of some number of '0's (possibly 0), followed by
+ * some number of '1's (also possibly 0.)
+ * <p>
+ * We are given a string S of '0's and '1's, and we may flip any '0' to a '1' or a '1' to a '0'.
+ * <p>
+ * Return the minimum number of flips to make S monotone increasing.
+ */
+public class FlipStringToMonotoneIncreasing {
+    public int minFlipsMonoIncr(String S) {
+        return 0;
+    }
+}
