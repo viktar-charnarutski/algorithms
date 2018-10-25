30a661a64d1744b59ad631dff83fdac96cc9475d
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-25 08:04:52 -0700

    922. Sort Array By Parity II. Solution.

--- src/main/java/problem/SortArrayByParityII.java
+++ src/main/java/problem/SortArrayByParityII.java
@@ -11,6 +11,17 @@ package problem;
  */
 public class SortArrayByParityII {
     public int[] sortArrayByParityII(int[] A) {
-        return new int[A.length];
+        int[] res = new int[A.length];
+        int evenIndex = 0, oddIndex = 1;
+        for (int i = 0; i < A.length; i++) {
+            if ((A[i] & 1) == 0) {
+                res[evenIndex] = A[i];
+                evenIndex += 2;
+            } else {
+                res[oddIndex] = A[i];
+                oddIndex += 2;
+            }
+        }
+        return res;
     }
 }
