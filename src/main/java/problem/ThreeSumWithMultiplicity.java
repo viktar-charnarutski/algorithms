40f39f5d3e850256850d5a84f4bc5ed5e9011fb3
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-26 08:12:55 -0700

    923. 3Sum With Multiplicity. Initial commit.

---
+++ src/main/java/problem/ThreeSumWithMultiplicity.java
@@ -0,0 +1,15 @@
+package problem;
+
+/**
+ * 923. 3Sum With Multiplicity
+ * <p>
+ * Given an integer array A, and an integer target, return the number of tuples i, j, k  such that i < j < k and
+ * A[i] + A[j] + A[k] == target.
+ * <p>
+ * As the answer can be very large, return it modulo 10^9 + 7.
+ */
+public class ThreeSumWithMultiplicity {
+    public int threeSumMulti(int[] A, int target) {
+        return 0;
+    }
+}
