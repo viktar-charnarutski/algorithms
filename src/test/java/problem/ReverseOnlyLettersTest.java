2b00b604ff2ad2acada5b95fd06d9ef304422bc8
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-28 12:53:30 -0700

    917. Reverse Only Letters. Tests.

--- src/test/java/problem/ReverseOnlyLettersTest.java
+++ src/test/java/problem/ReverseOnlyLettersTest.java
@@ -10,4 +10,9 @@ public class ReverseOnlyLettersTest {
     public void reverseOnlyLetters1() {
         assertEquals("dc-ba", new ReverseOnlyLetters().reverseOnlyLetters("ab-cd"));
     }
+
+    @Test
+    public void reverseOnlyLetters2() {
+        assertEquals("j-Ih-gfE-dCba", new ReverseOnlyLetters().reverseOnlyLetters("a-bC-dEf-ghIj"));
+    }
 }
