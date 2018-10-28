c3ea15940e73558d717a84ad9333b817fabaa162
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-28 12:54:48 -0700

    917. Reverse Only Letters. Solution.

--- src/main/java/problem/ReverseOnlyLetters.java
+++ src/main/java/problem/ReverseOnlyLetters.java
@@ -8,6 +8,22 @@ package problem;
  */
 public class ReverseOnlyLetters {
     public String reverseOnlyLetters(String S) {
-        return S;
+        char[] chars = S.toCharArray();
+        int start = 0, end = S.length() - 1;
+        while (start < end) {
+            while (start < end && !Character.isLetter(chars[start])) {
+                start++;
+            }
+            while (start < end && !Character.isLetter(chars[end])) {
+                end--;
+            }
+            char c = chars[start];
+            chars[start] = chars[end];
+            chars[end] = c;
+
+            start++;
+            end--;
+        }
+        return new String(chars);
     }
 }
