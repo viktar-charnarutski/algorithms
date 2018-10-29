74bf96a6967b6f9ab865bdd72b49ad57a0085486
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-29 08:35:23 -0700

    914. X of a Kind in a Deck of Cards. Tests.

--- src/test/java/problem/XOfKindInDeckOfCardsTest.java
+++ src/test/java/problem/XOfKindInDeckOfCardsTest.java
@@ -10,4 +10,9 @@ public class XOfKindInDeckOfCardsTest {
     public void hasGroupsSizeX1() {
         assertTrue(new XOfKindInDeckOfCards().hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1}));
     }
+
+    @Test
+    public void hasGroupsSizeX2() {
+        assertFalse(new XOfKindInDeckOfCards().hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2, 3, 3}));
+    }
 }
