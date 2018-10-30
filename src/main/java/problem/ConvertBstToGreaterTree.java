c0135ae30a079daa4a00e6e362c9e77df88548cf
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-30 08:09:59 -0700

    538. Convert BST to Greater Tree. Initial commit.

---
+++ src/main/java/problem/ConvertBstToGreaterTree.java
@@ -0,0 +1,13 @@
+package problem;
+
+/**
+ * 538. Convert BST to Greater Tree
+ *
+ * Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to
+ * the original key plus sum of all keys greater than the original key in BST.
+ */
+public class ConvertBstToGreaterTree {
+    public TreeNode convertBST(TreeNode root) {
+        return root;
+    }
+}
