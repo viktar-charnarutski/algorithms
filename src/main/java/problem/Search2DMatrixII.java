cfc47e79b3aca6f3b52137862f7c0548c72a595a
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-30 16:27:28 -0700

    240. Search a 2D Matrix II. Initial commit.

---
+++ src/main/java/problem/Search2DMatrixII.java
@@ -0,0 +1,29 @@
+package problem;
+
+/**
+ * 240. Search a 2D Matrix II
+ * <p>
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+ * <p>
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * Example:
+ * <p>
+ * Consider the following matrix:
+ * <p>
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * Given target = 5, return true.
+ * <p>
+ * Given target = 20, return false.
+ */
+public class Search2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return false;
+    }
+}
