c4e1bf7f7bfdcaaeb9ba7409ba20038219ca5b02
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-29 19:07:51 -0700

    43. Multiply Strings. Initial commit.

---
+++ src/main/java/problem/MultiplyStrings.java
@@ -0,0 +1,28 @@
+package problem;
+
+/**
+ * 43. Multiply Strings
+ * <p>
+ * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also
+ * represented as a string.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: num1 = "2", num2 = "3"
+ * Output: "6"
+ * Example 2:
+ * <p>
+ * Input: num1 = "123", num2 = "456"
+ * Output: "56088"
+ * Note:
+ * <p>
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contain only digits 0-9.
+ * Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ */
+public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        return "";
+    }
+}
