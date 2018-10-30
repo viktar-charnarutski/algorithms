aa2495bb933b456bfe5193183552945be0a69467
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-30 16:48:53 -0700

    240. Search a 2D Matrix II. Solution.

--- src/main/java/problem/Search2DMatrixII.java
+++ src/main/java/problem/Search2DMatrixII.java
@@ -24,6 +24,17 @@ package problem;
  */
 public class Search2DMatrixII {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix[0].length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (target > matrix[row][col]) {
+                row++;
+            } else {
+                col--;
+            }
+        }
         return false;
     }
 }
