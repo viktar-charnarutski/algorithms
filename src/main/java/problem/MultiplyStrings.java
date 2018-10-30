c8834087fdb291f2e32c0b0fac7a1572ddaaef43
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-29 19:16:45 -0700

    43. Multiply Strings. Solution.

--- src/main/java/problem/MultiplyStrings.java
+++ src/main/java/problem/MultiplyStrings.java
@@ -23,6 +23,31 @@ package problem;
  */
 public class MultiplyStrings {
     public String multiply(String num1, String num2) {
-        return "";
+        int[] prods = new int[num1.length() + num2.length()];
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                prods[i + j + 1] += n1 * n2;
+            }
+        }
+
+        int carry = 0;
+        for (int i = prods.length - 1; i >= 0 ; i--) {
+            int tmp = (prods[i] + carry) % 10;
+            carry = (prods[i] + carry) / 10;
+            prods[i] = tmp;
+        }
+
+        StringBuilder res = new StringBuilder(carry);
+        for(int n : prods) {
+            res.append(n);
+        }
+
+        while (res.length() > 0 && res.charAt(0) == '0') {
+            res.deleteCharAt(0);
+        }
+        return res.length() > 0 ? res.toString() : "0";
     }
 }
