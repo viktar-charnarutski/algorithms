b63d682355177d35522ee7acaed05541385ccfb1
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-31 08:18:41 -0700

    74. Search a 2D Matrix. Solution.

--- src/main/java/problem/SearchA2DMatrix.java
+++ src/main/java/problem/SearchA2DMatrix.java
@@ -13,15 +13,17 @@ public class SearchA2DMatrix {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
-        int row = 0, col = matrix[0].length - 1;
-        while (row < matrix.length && col >= 0) {
-            if (matrix[row][col] == target) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int lo = 0, hi = rows * cols - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target == matrix[mid / cols][mid % cols]) {
                 return true;
             }
-            if (target < matrix[row][col] || row == matrix.length - 1) {
-                col--;
+            if (target < matrix[mid / cols][mid % cols]) {
+                hi = mid - 1;
             } else {
-                row++;
+                lo = mid + 1;
             }
         }
         return false;
