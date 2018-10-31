888b1f0b7f4833645acbe6c6ef04c5d12ae7a02f
viktar-charnarutski <v.charnarutski@gmail.com>
2018-10-31 07:47:46 -0700

    74. Search a 2D Matrix. Tests.

---
+++ src/test/java/problem/SearchA2DMatrixTest.java
@@ -0,0 +1,20 @@
+package problem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchA2DMatrixTest {
+
+    @Test
+    public void searchMatrix1() {
+        int[][] matrix = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}
+        };
+        assertTrue(new SearchA2DMatrix().searchMatrix(matrix, 18));
+    }
+}
